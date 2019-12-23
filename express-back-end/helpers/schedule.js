937582698e93655fddfdbd02a95f55ebbaf38421
Nga Nguyen <ngasdragon@gmail.com>
2019-12-22 23:02:43 -0800

    Create itinerary array

--- express-back-end/helpers/schedule.js
+++ express-back-end/helpers/schedule.js
@@ -17,16 +17,55 @@ const data = [
   { name: "Third Beach",
     lat: 49.30338193650776,
     long: -123.1563403102008
+  },
+  { name: "False Creek Seawall",
+    lat: 49.27273179887483,
+    long: -123.13379848089629
+  },
+  { name: "Earnest Ice Cream",
+    lat: 49.26896525441082,
+    long: -123.10261255924644
+  },
+  { name: "David Lam Park",
+    lat: 49.2724665529135,
+    long: -123.12386556302295
   }
 ];
- 
+
+const days = [1, 2];
+
+// const transitTime =  {
+//   duration: 
+// }
+let itineraries = [];
 let vectors = new Array();
 for (let i = 0 ; i < data.length ; i++) {
   vectors[i] = [ data[i]['lat'] , data[i]['long']];
 }
 
 // K refers to number of days
-kmeans.clusterize(vectors, {k: 2}, (err,res) => {
+const cluster = kmeans.clusterize(vectors, {k: days.length}, (err,res) => {
   if (err) console.error(err);
-  else console.log('%o',res);
+  else {
+    // console.log('%o',res);
+    return res;
+  }
 });
+
+//Loop through the result from kmeans function to get the array of attractions
+
+cluster.groups.forEach(element => {
+  for (let i in element.clusterInd) {
+    element.clusterInd[i] = data[element.clusterInd[i]]
+  }
+  itineraries.push(element.clusterInd);
+});
+console.log(itineraries)
+
+// //API request for transit time
+
+// for (let itinerary of itineraries) {
+//   for (let attraction of itinerary) {
+//     axios
+//   }
+// }
