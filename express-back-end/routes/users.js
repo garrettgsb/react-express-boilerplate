acdc7a85291308191d73d22bd508ff1ceb69ff89
stella-zb <upperdaystudio@gmail.com>
2019-12-25 22:18:13 -0800

    initial setup for all GET routes

--- express-back-end/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req,res) => {
-  res.send('Home Page');
-  console.log("Home Page");
-});
-
-module.exports = router
