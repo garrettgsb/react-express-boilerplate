04410d549e18a8810979a0f0d9a0860f96d9a498
Michelle Siregar <michellesiregar@gmail.com>
2019-12-28 12:38:48 -0800

    styled citylist component

--- react-front-end/src/components/mytrips/City.tsx
+++ react-front-end/src/components/mytrips/City.tsx
@@ -5,6 +5,7 @@ type CityProps = { name: string, img: string, start: string, end: string }
 
 const Name = styled.h2`
   text-align: center;
+  color: #fff;
 `;
 
 const Date = styled.div`
@@ -18,7 +19,7 @@ export const City = ({name, img, start, end}: CityProps) => {
   const Container = styled.div`
     padding: 10px 10px 0px 10px;
     margin: 15px;
-    background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(0, 0, 0, 0.9)), url(${img});
+    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${img});
     background-size: 100%;
   `;
 
--- react-front-end/src/components/mytrips/CityList.tsx
+++ react-front-end/src/components/mytrips/CityList.tsx
@@ -1,16 +1,25 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
 import { City } from './City';
 
 type CityListProps = { city: string }
 
+const Cities = styled.ul`
+  padding-left: 0px;
+`;
+
+const CityItem = styled.li`
+  list-style-type: none;
+`;
+
 export const CityList = ({city}: CityListProps) => {
   return (
-    <ul>
+    <Cities>
       {/* {cities.map(city => 
         <City name={city.city} />
       )} */}
-      <City name="Vancouver" img="https://vancouver.ca/images/cov/feature/about-vancouver-landing-size.jpg" start="start" end="end"/>
-      <City name="Seattle" img="test.png" start="start" end="end"/>
-    </ul>
+      <CityItem><City name="Vancouver" img="https://vancouver.ca/images/cov/feature/about-vancouver-landing-size.jpg" start="start" end="end"/></CityItem>
+      <CityItem><City name="Seattle" img="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Seattle_Kerry_Park_Skyline.jpg/1200px-Seattle_Kerry_Park_Skyline.jpg" start="start" end="end"/></CityItem>
+    </Cities>
   )
 }
