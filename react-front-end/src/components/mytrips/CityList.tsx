ab524d935ef6abcab28e04b92f359f09c09e525d
Michelle Siregar <michellesiregar@gmail.com>
2019-12-28 11:29:38 -0800

    install styled-components

--- react-front-end/src/components/mytrips/CityList.tsx
+++ react-front-end/src/components/mytrips/CityList.tsx
@@ -2,10 +2,13 @@ import React, { FunctionComponent } from 'react';
 import { City } from './City';
 
 type CityListProps = { city: string }
-export const CityList = ({city}: CityListProps) =>
+export const CityList = ({city}: CityListProps) => {
+  return (
     <ul>
       {/* {cities.map(city => 
         <City name={city.city} />
       )} */}
       <City name="Vancouver" img="test.png" start="test1" end="test2"/>
     </ul>
+  )
+}
