febcf27e8951a3f9f98c86481eb025ac5ecc858c
Michelle Siregar <michellesiregar@gmail.com>
2019-12-28 12:26:40 -0800

    style city component

--- react-front-end/src/components/mytrips/City.tsx
+++ react-front-end/src/components/mytrips/City.tsx
@@ -1,14 +1,35 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
 
 type CityProps = { name: string, img: string, start: string, end: string }
 
+const Name = styled.h2`
+  text-align: center;
+`;
+
+const Date = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  color: #fff;
+`;
+
 export const City = ({name, img, start, end}: CityProps) => {
+  const Container = styled.div`
+    padding: 10px 10px 0px 10px;
+    margin: 15px;
+    background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.5), rgba(0, 0, 0, 0.9)), url(${img});
+    background-size: 100%;
+  `;
+
   return (
-    <div>
-      <h3>{name}</h3>
-      <p>{start}</p>
-      <p>{end}</p>
-    </div>
+    <Container>
+      <Name>{name}</Name>
+      <Date>
+        <p>{start}</p>
+        <p>{end}</p>
+      </Date>
+    </Container>
   )
 }
 
--- react-front-end/src/components/mytrips/CityList.tsx
+++ react-front-end/src/components/mytrips/CityList.tsx
@@ -2,13 +2,15 @@ import React, { FunctionComponent } from 'react';
 import { City } from './City';
 
 type CityListProps = { city: string }
+
 export const CityList = ({city}: CityListProps) => {
   return (
     <ul>
       {/* {cities.map(city => 
         <City name={city.city} />
       )} */}
-      <City name="Vancouver" img="test.png" start="test1" end="test2"/>
+      <City name="Vancouver" img="https://vancouver.ca/images/cov/feature/about-vancouver-landing-size.jpg" start="start" end="end"/>
+      <City name="Seattle" img="test.png" start="start" end="end"/>
     </ul>
   )
 }
