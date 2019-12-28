6160d5e0b8091401f8e0b90b8d659f97c079d630
stella-zb <upperdaystudio@gmail.com>
2019-12-28 15:35:22 -0800

    initial styled component for login in form

---
+++ react-front-end/src/components/auth/Auth.components.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AuthForm = styled.form`
+  display: flex;
+  align-item: center;
+  flex-direction: column;
+  width: 50%;
+  margin: 0 auto;
+`
