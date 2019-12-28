d2f6115260dcecf71890f1a92970c60e1fa6f201
Nga Nguyen <ngasdragon@gmail.com>
2019-12-28 12:37:01 -0800

    rename searchbox & initialize DatePick.tsx

---
+++ react-front-end/src/components/explore/SearchBox.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from 'styled-components';
+
+const Input = styled.input`
+  border-radius: 5px
+  height: 30px
+  weight: auto
+  color: red
+`;
+
+type SearchProps = { message: string };
+
+export class Search extends React.Component<SearchProps, {}> {
+  
+  render() {
+    return (
+      <div className="SearchBar">
+        <label className="search-label" htmlFor="search-input">
+          <Input
+              type="text"
+              value=""
+              id="search-input"
+              placeholder="Please enter your destination"
+          />
+        </label>
+      </div>
+    )
+  }
+}
