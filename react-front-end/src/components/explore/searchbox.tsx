5162ee20350165d71f2de6f1f62aa30f0cc728d0
Nga Nguyen <ngasdragon@gmail.com>
2019-12-28 11:53:56 -0800

    add styled components

--- react-front-end/src/components/explore/searchbox.tsx
+++ react-front-end/src/components/explore/searchbox.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import styled from 'styled-components';
 // export interface HelloProps { compiler: string; framework: string; }
 
 
 type HelloProps = { compiler: string; framework: string; message: string}
 
 export class Hello extends React.Component<HelloProps, {}> {
+  
   render() {
       return <h1>Hello from {this.props.compiler} and {this.props.framework} and {this.props.message}!</h1>;
   }
