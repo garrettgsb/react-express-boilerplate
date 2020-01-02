1867fb771d6761abaa1d4c0c77f45a434069b3cd
Nga Nguyen <ngasdragon@gmail.com>
2020-01-02 12:04:37 -0800

    refactor files and components

--- react-front-end/src/components/Explore/SearchBox.tsx
+++ react-front-end/src/components/Explore/SearchBox.tsx
@@ -1,7 +1,9 @@
-import React, {FC, useState} from "react";
+import React, {FC, useState, Fragment} from "react";
 import styled from 'styled-components';
 import axios from "axios";
 import PropTypes from 'prop-types';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Input = styled.input`
   border-radius: 5px
@@ -12,32 +14,72 @@ const Input = styled.input`
 
 interface SearchProps {
   handleInputChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  selected?: string | null,
+  date?: Date | null
 };
 
-export const SearchBar: FC<SearchProps> = ({handleInputChange}) => {
+export const SearchBar: FC<SearchProps> = ({handleInputChange, handleSubmit}) => {
   
+  //user city input
   const [search, setSearch] = useState('');
+
+  //user date input
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(new Date());
+
   handleInputChange = (e) => {
     setSearch(e.target.value)
-  }
-  axios.defaults.baseURL = 'https://maps.googleapis.com'
+  };
+
+  handleSubmit = (e) => {
+    console.log(search);
+    console.log(startDate);
+    console.log(endDate);
+  };
+
+  //API call for city
+
+  axios.defaults.baseURL = 'https://maps.googleapis.com';
   axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
   axios.get(`/maps/api/place/findplacefromtext/json?input=${search}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${process.env.GOOGLE_API_KEY}`)
-  .then(results => console.log(results))
+  .then(results => console.log(results));
 
   return (
-    <div className="SearchBar">
-      <label className="search-label" htmlFor="search-input">
-        <Input
-            type="text"
-            id="search-input"
-            placeholder="Please enter your destination"
-            onChange ={handleInputChange}
-            value = {search}
+    <Fragment>
+      <div className="SearchBar">
+        <label className="search-label" htmlFor="search-input">
+          <Input
+              type="text"
+              id="search-input"
+              placeholder="Please enter your destination"
+              onChange ={handleInputChange}
+              value = {search}
+          />
+        </label>
+      </div>
+      <div className="DatePicker">
+        <DatePicker
+          selected={startDate}
+          onChange={date => setStartDate(date)}
+          showTimeSelect
+          timeFormat="HH:mm"
+          timeIntervals={15}
+          timeCaption="time"
+          dateFormat="MMMM d, yyyy h:mm aa"
         />
-      </label>
-    </div>
+        <DatePicker
+          selected={endDate}
+          onChange={date => setEndDate(date)}
+          showTimeSelect
+          timeFormat="HH:mm"
+          timeIntervals={15}
+          timeCaption="time"
+          dateFormat="MMMM d, yyyy h:mm aa"
+        />
+      </div>
+      <button onClick={handleSubmit}>Search</button>
+    </Fragment>
   );
-  
 };
 
--- react-front-end/src/components/Explore/index.tsx
+++ react-front-end/src/components/Explore/index.tsx
@@ -2,14 +2,11 @@ import React, { Fragment } from "react";
 import styled from 'styled-components';
 
 import { SearchBar } from "./SearchBox";
-import { DatePick } from "./DatePicker";
 import { DestRec } from "./DestRecommended";
-import { Button } from "./Button"
 type ExploreProps = {
   city: string,
   selected?: string | null,
   topRecommended: string
-  // onClick: (e:React.MouseEvent) => void
   // handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
@@ -17,8 +14,6 @@ export const Explore: React.FC<ExploreProps> = () => {
     return (
       <Fragment>
         <SearchBar/>
-        <DatePick/>
-        <Button />
         <DestRec city="Van" topRecommended="Vancouver"/>
       </Fragment>
     )
