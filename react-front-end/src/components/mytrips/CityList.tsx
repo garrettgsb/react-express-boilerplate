7aecc1a060c4a84f30c3d25d9e0c75fb0445d138
Michelle Siregar <michellesiregar@gmail.com>
2020-01-02 18:33:35 -0800

    connect citylist to backend db

--- react-front-end/src/components/mytrips/CityList.tsx
+++ react-front-end/src/components/mytrips/CityList.tsx
@@ -1,10 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { City } from './City';
+import moment from 'moment';
 
-type PropTypes = { cities: string }
+type PropTypes = { cities: Array<any> }
 
 const Cities = styled.ul`
   padding-left: 0px;
@@ -21,24 +22,15 @@ const Title = styled.h1`
 
 export const CityList = ({cities}: PropTypes) => {
 
-  const [trips, setTrips] = useState([]);
-
-  useEffect(() => {
-    axios.get('/api/trips')
-    .then((res) => {
-      setTrips(res.data)
-    })
-    .then(() => {
-      console.log(trips)
-    })
-  }, [])
-
   return (
     <>
     <Title>My Trips</Title>
     <Cities>
-        <CityItem to='/trips/vancouver'><City name="Vancouver" img="https://vancouver.ca/images/cov/feature/about-vancouver-landing-size.jpg" start="start" end="end"/></CityItem>
-        <CityItem to='/trips/seattle'><City name="Seattle" img="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Seattle_Kerry_Park_Skyline.jpg/1200px-Seattle_Kerry_Park_Skyline.jpg" start="start" end="end"/></CityItem>
+      {cities.map(city =>
+        <CityItem to="/trips/vancouver">
+          <City name={city.city} img={city.city_img} start={moment(city.start_time).format('MMM DD, YYYY')} end={moment(city.end_time).format('MMM DD, YYYY')} />
+        </CityItem>
+        )}
     </Cities>
     </>
   )
