9e047ef728848926170bb0ecf904bd022aac38dd
stella-zb <upperdaystudio@gmail.com>
2020-01-07 11:46:31 -0800

    axios request to update the user state, set conditional render if user logged in

--- react-front-end/src/components/profile/profile.tsx
+++ react-front-end/src/components/profile/profile.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from 'axios';
+import { UserInfo, MainInfo, Button } from "./profile.components";
+import { Redirect } from "react-router-dom";
+
+interface User {
+  id?: number;
+  first_name?: string;
+  last_name?: string;
+  email?: string;
+  facebook?: string;
+}
 
 export const Profile = () => {
+
+  // const userID = window.localStorage.getItem('userID');
+  const [user, setUser] = useState({} as User);
+
+  useEffect(() => {
+    // Axios.get(`/profile/${userID}`)
+    Axios.get(`/profile`)
+      .then((res) => {
+        // console.log(res.data)
+        setUser(res.data)
+      })
+      .catch(err => console.log(err));
+  }, [])
+
+  const logout = () => {
+  }
+
   return (
-    <form>
-      <p>Full Name</p>
-      <p>Email</p>
-      <p>Facebook</p>
-      <button type="submit">Login Out</button>
-    </form>
+    !user ? <Redirect to='/' /> :
+      <UserInfo onSubmit={logout}>
+        <MainInfo>
+          <p>Hello! {user.first_name}</p>
+          <p>{user.email}</p>
+          <p>{user.facebook}</p>
+          <Button type="submit">Login Out</Button>
+        </MainInfo>
+      </UserInfo>
   )
 };
