d92bc2c66e276f0d6887d10258bda88ea47a61a4
stella-zb <upperdaystudio@gmail.com>
2020-01-07 11:44:36 -0800

    style component for profile page

---
+++ react-front-end/src/components/profile/profile.components.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const UserInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin: 0 auto;
+  width: 100%;
+  height: 600px;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+export const MainInfo = styled.form`
+  width: 50%;
+`
+
+export const Button = styled.button`
+  color: white;
+  background-color: #00C3AB;
+  width: 50%;
+  padding: 10px 20px;
+  cursor: pointer;
+  border: none;
+  margin: 10px 0;
+`
