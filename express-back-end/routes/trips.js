b6d01dbe5b565e62cdae291db4436d6d721d1349
stella-zb <upperdaystudio@gmail.com>
2020-01-08 15:07:14 -0800

    create algorithm for /trip/:id post router: take in kmean function to cluster the dataset, do cacluation for start_time, end_time for the dataset,  db query insert into database

--- express-back-end/routes/trips.js
+++ express-back-end/routes/trips.js
@@ -30,12 +30,52 @@ module.exports = (db) => {
       })
   })
 
-  router.get('/api/direction', (req, res) => {
+  router.post('/:id', (req, res) => {
     console.log('req data', req)
 
-    axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=${GOOGLE_KEY}`)
-      .then(res => console.log('res after google api direction call', res))
+    // pass req.body to kmean function to cluster the locations in order
+
+    // loop through each day cluster in clusters array,
+    // loop through each location object in cluster array,
+    // check if current and next element is object,
+    // if so, take the first two location objects in cluster array to call google direction api to get travel duration between these two location, then shift/remove the first item in array
+    axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=Stanley+Park&destination=David+Lam+Park&mode=transit&key=${GOOGLE_KEY}`)
+      // traval_duration = response.routes[0].legs[0].duration.text, total travel_duration bewteen locations
+      // direction_steps = response.routes[0].legs[0].steps, if direction_steps contain more than one object, set travel mode to bus, or else it will be walking
+      .then(response => console.log('res after google api direction call', response))
       .catch(err => console.log('res after google api direction call', err))
+    // or else, skip go to the next item
+
+    // SO end_data we received will be [[obj, number, obj, number, obj...],[obj, number, obj, number, obj...], ...] 
+
+    // Default start_time for each day will be 12am
+    // Default start_time for first location in timeslot will be 12am + 9hour (9am in the morning)
+    // const day_start = trip_start + (9 * 3600) + (86400 * dayIndex)
+    // let start_time = day_start
+
+    // loop through end_data
+
+    // loop start:
+
+    // if item === Number
+    // start_time = start_time
+    // end_time = start_time + number
+    // or else
+    // start_time = start_time
+    // end_time = start_time + visit_duration (default as 1 hour if api not working)
+
+    // db insert
+    // if travel_mode === null, insert into db table timeslots with start_time, end_time, itinerary_id, attraction_id, travel_mode with travel_mode value to be null 
+    // else, insert into db table timeslots with start_time, end_time, itinerary_id, attraction_id, travel_mode with attraction_id value to be null
+    db.query(`
+    `, [])
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+
+    // update start_time = end_time
+
+    // loop to next item
+
   })
 
   return router;
