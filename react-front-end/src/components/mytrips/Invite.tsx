34c608a4259a5873ed0cc78633e314837d389722
Michelle Siregar <michellesiregar@gmail.com>
2020-01-09 23:41:11 -0800

    complete successful db request for invite

--- react-front-end/src/components/mytrips/Invite.tsx
+++ react-front-end/src/components/mytrips/Invite.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export const Invite = () => {
   const tripId:string = location.pathname.slice((location.pathname.lastIndexOf('/') - 1), -7);
+  const [email, getEmail] = useState<string>('');
+
+  const inviteFriend = () => {
+    axios.post(`/api/trips/${tripId}/invite`, {
+      user: email
+    })
+    .then((res) => console.log(res.data))
+  }
+
 
   return (
-    <p>Invite to {tripId}</p>
+    <div>
+      <h1>Invite</h1>
+      <form>
+        <input type="text" value={email} onChange={(e) => getEmail(e.target.value)} placeholder="Email" />
+        <button onClick={inviteFriend}>Submit</button>
+      </form>
+    </div>
   )
 }
