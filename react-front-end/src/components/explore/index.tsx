b2221abbf159eec1f214eafd875d4ccda5ddecfc
Nga Nguyen <ngasdragon@gmail.com>
2020-01-10 17:19:13 -0800

    initialize filter component

--- react-front-end/src/components/explore/index.tsx
+++ react-front-end/src/components/explore/index.tsx
@@ -5,6 +5,8 @@ import styled from 'styled-components';
 import { SearchBar } from "./SearchBox";
 import { DestRec } from "./DestRecommended";
 import { Swipe } from "./swipe";
+import { Filter } from "./Filter";
+
 type ExploreProps = {
   cityName: string | null,
   selected?: string | null,
@@ -20,17 +22,16 @@ export const Explore: React.FC<ExploreProps> = () => {
   
     return (
       <Router>
-        
         <Switch>
           <Route exact path='/explore'>
             <SearchBar />
             <DestRec cityName="Van" topRecommended="Vancouver"/>
           </Route>
           <Route exact path={`/explore/:itinerariesId`} render={({match}) => (
-          <Swipe itinerariesId ={match.params.itinerariesId}/>)}/>
-          
-            {/* <Swipe/> */}
-          {/* </Route> */}
+            <Swipe itinerariesId ={match.params.itinerariesId}/>)}/>
+          <Route>
+            <Filter/>
+          </Route>
         </Switch>
 
       </Router>
