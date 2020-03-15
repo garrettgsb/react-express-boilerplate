c7cc3e9200e76453ad30f974f3e374851cfad1a8
Hoang Dinh <hoangdinh90@gmail.com>
2020-03-15 15:24:39 -0700

    prepped files to start work on component

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -11,14 +11,13 @@ class App extends Component {
   }
 
   fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+    axios.get('/api/users') // You can simply make your requests to "/api/whatever you want"
     .then((response) => {
       // handle success
       console.log(response.data) // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
       this.setState({
-        message: response.data.message
+        message: `Hello ${response.data.users[0].name}`
       });
     }) 
   }
