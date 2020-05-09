3cdbef004e8eb345661d05253dfd3d20122d67c9
Allen <headhuntar@gmail.com>
2020-05-09 12:46:44 -0700

    wrote queries for db

--- express-back-end/lib/bookingQueries.js
+++ express-back-end/lib/bookingQueries.js
@@ -1,1 +1,40 @@
 const db = require('./db');
+
+const getBooking = (id) => {
+  return db.query(`
+  SELECT *
+  FROM bookings
+  where id = $1;
+  `, [id])
+    .then(res => {
+      return res.rows;
+    });
+};
+
+module.exports.getBooking = getBooking;
+
+const getBookingsByTour = (tourId) => {
+  return db.query(`
+  SELECT *
+  FROM bookings
+  where tour_id = $1
+  `, [tourId])
+    .then(res => {
+      return res.rows;
+    });
+};
+
+module.exports.getBookingsByTour = getBookingsByTour;
+
+const getBookingsByUser = (userId) => {
+  return db.query(`
+  SELECT *
+  FROM bookings
+  where user_id = $1
+  `, [userId])
+    .then(res => {
+      return res.rows;
+    });
+};
+
+module.exports.getBookingsByUser = getBookingsByUser;
--- express-back-end/lib/notificationQueries.js
+++ express-back-end/lib/notificationQueries.js
@@ -1,1 +1,27 @@
 const db = require('./db');
+
+const getNotification = (id) => {
+  return db.query(`
+  SELECT *
+  FROM notifications
+  where id = $1;
+  `, [id])
+    .then(res => {
+      return res.rows;
+    });
+};
+
+module.exports.getNotification = getNotification;
+
+const getNotificationsByUser = (userId) => {
+  return db.query(`
+  SELECT *
+  FROM notifications
+  where user_id = $1
+  `, [userId])
+    .then(res => {
+      return res.rows;
+    });
+};
+
+module.exports.getNotificationssByUser = getNotificationsByUser;
