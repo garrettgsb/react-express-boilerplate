3ef5b83ef581469e6516548582032c15c99eadfb
Kevin Chu <kvnchu2@gmail.com>
2021-02-12 11:14:37 -0800

    add form for data fetch

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,38 +1,29 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React  from 'react';
+import useApplicationData from './useApplicationData';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+export default function Application(props) {
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+  const { setUser, setURL, fetchData, state } = useApplicationData();
+  
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
-
-  render() {
     return (
       <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
+        <img src={ state.avatar } alt="nothing"></img>
+
+
+        <form autoComplete="off" onSubmit={setURL}>
+          <input name="name" type="text" value={state.user || ""} onChange={(e) => setUser(e)}/>
+          <button type="submit">Submit</button>
+        </form>
+
+
+          
+      
+        <button onClick={fetchData} >
           Fetch Data
         </button>        
       </div>
     );
-  }
 }
-
-export default App;
+            
