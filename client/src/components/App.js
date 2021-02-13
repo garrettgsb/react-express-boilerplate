783dbb74086bcab1a2fd3a7109cc9369536cb138
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-12 19:56:50 -0500

    converting og App component from class based to functional

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,40 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
 // test
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+function App() {
 
-  fetchData = () => {
+  const [message, setMessage] =  useState('Click the button to load data!')
+ 
+  const fetchData = () => {
     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
     .then((response) => {
       // handle success
       console.log(response.data) // The entire response from the Rails API
 
       console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
+      setMessage(response.data.message);
     }) 
   }
 
-  render() {
     return (
       <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
+        <h1>{ message }</h1>
+        <button onClick={fetchData} >
           Fetch Data
         </button>        
       </div>
     );
-  }
+  
 }
 
 export default App;
