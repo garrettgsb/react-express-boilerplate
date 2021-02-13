b5590eb774fe619378ddcc9ef6e1d73da42e6d3a
Stewart <stewart.mckinlay@gmail.com>
2021-02-12 17:50:44 -0700

    Install morgan

--- server/server.js
+++ server/server.js
@@ -1,24 +1,27 @@
-const Express = require("express");
-const App = Express();
+const express = require("express");
 const BodyParser = require("body-parser");
+const morgan = require("morgan")
+const app = express();
 const PORT = 8080;
 
 // Express Configuration
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static("public"));
+app.use(BodyParser.urlencoded({ extended: false }));
+app.use(BodyParser.json());
+app.use(express.static("public"));
+app.use(morgan('dev'));
+
 
 // Sample GET route
-App.get("/api/data", (req, res) =>
+app.get("/api/data", (req, res) =>
   res.json({
     message: "Seems to work!",
   })
 );
 
 //PORT setup
-App.listen(PORT, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(
-    `Express seems to be listening on port ${PORT} so that's pretty good 👍`
+    `Server is listening on port ${PORT}`
   );
 });
