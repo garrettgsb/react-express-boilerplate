06e06323f606f4f34e1c40d0c0a5a5e61c7cb9e8
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2021-02-13 14:11:13 -0800

    Add stores route

--- server/src/routes/stores.js
+++ server/src/routes/stores.js
@@ -1,13 +1,30 @@
 const router = require("express").Router();
 
-// BROWSE GET /stores/  "Get all stores"
-// READ GET /stores/:id  "Get Store Details"
-
 module.exports = (db) => {
-router.get("", (req, res) => {
-  db.query(``)
-  .then()
-  .catch()
-})
-return router;
-}
+  // BROWSE GET /stores/  "Get all stores"
+  router.get("/stores/", (req, res) => {
+    db.query(`SELECT * FROM stores;`)
+      .then((result) => {
+        res.json(result.rows);
+      })
+      .catch((err) => {
+        res.status(400).json({ error: err.message });
+      });
+  });
+
+  // READ GET /stores/:id  "Get Store Details"
+  router.get("/stores/:id", (req, res) => {
+    const queryParams = [req.params.id];
+    db.query(`SELECT * FROM stores WHERE id = $1`, queryParams)
+      .then((result) => {
+        result.rows.length
+          ? res.json(result.rows)
+          : res.json({
+              message: `no stores found with id: ${req.params.id}`,
+            });
+      })
+      .catch((err) => res.status(401).json({ error: err.message }));
+  });
+
+  return router;
+};
