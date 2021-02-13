8f90170226ec9a16228bf55cc3a65a8c02efee9f
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-13 11:25:37 -0500

    creating lifetimebeans and tier components

--- client/src/components/Customer/CurrentBeans.js
+++ client/src/components/Customer/CurrentBeans.js
@@ -1,10 +1,10 @@
 function CurrentBeans(props) {
-  const beanCount = props.currentBeans;
+  const currentBeans = props.currentBeans;
 
   return(
     <div>
       <p>Current beans:</p>
-      <h3>{beanCount}</h3>
+      <h3>{currentBeans}</h3>
     </div>
   )
 }
--- client/src/components/Customer/UserInfo.js
+++ client/src/components/Customer/UserInfo.js
@@ -1,13 +1,17 @@
 import Logo from './Logo'
 import WelcomeMessage from './WelcomeMessage'
 import CurrentBeans from './CurrentBeans'
+import LifetimeBeans from './LifetimeBeans'
+import Tier from './Tier'
 
 function UserInfo() {
   return(
     <div>
       <Logo />
       <WelcomeMessage name='Andy'/>
+      <Tier tier='Super Bean' />
       <CurrentBeans currentBeans='230' />
+      <LifetimeBeans lifetimeBeans='560' />
     </div>
   )
 }
