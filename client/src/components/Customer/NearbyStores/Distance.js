21fa4ea774e7d70a1fb3a9aeb43c7e76516c79ec
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-13 14:26:22 -0500

    adding flexbox to nearby stores component

--- client/src/components/Customer/NearbyStores/Distance.js
+++ client/src/components/Customer/NearbyStores/Distance.js
@@ -14,12 +14,12 @@ function Distance() {
       value={distance}
       onChange={handleChange}
     >
-    <MenuItem value={5}>5</MenuItem>
-    <MenuItem value={10}>10</MenuItem>
-    <MenuItem value={20}>20</MenuItem>
-    <MenuItem value={30}>30</MenuItem>
-    <MenuItem value={50}>50</MenuItem>
-    <MenuItem value={100}>100</MenuItem>
+    <MenuItem value={5}>5 km</MenuItem>
+    <MenuItem value={10}>10 km</MenuItem>
+    <MenuItem value={20}>20 km</MenuItem>
+    <MenuItem value={30}>30 km</MenuItem>
+    <MenuItem value={50}>50 km</MenuItem>
+    <MenuItem value={100}>100 km</MenuItem>
 
     </Select>
   )
--- client/src/components/Customer/NearbyStores/index.js
+++ client/src/components/Customer/NearbyStores/index.js
@@ -4,11 +4,13 @@ import StoresGrid from "./StoresGrid"
 
 function NearbyStores() {
   return(
-    <>
+    <div className='nearby-stores-container'>
+      <div className='nearby-stores-header'>
     <h3>Nearby Stores</h3>
     <Distance />
+      </div>
     <StoresGrid />
-    </>
+    </div>
   )
 }
 
