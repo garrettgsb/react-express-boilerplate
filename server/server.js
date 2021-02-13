3dfb9274a2b2cf2b603f86eedb2dd5bb28e1d04f
Stewart <stewart.mckinlay@gmail.com>
2021-02-13 09:41:21 -0700

    Fix bugs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+// const menu =require('./src/routes/menu');
+// const order = require('./src/routes/order');
+// const stores = require('./src/routes/stores');
+// const users = require('./src/routes/users');
+
 const express = require("express");
 const BodyParser = require("body-parser");
 const morgan = require("morgan")
@@ -5,8 +10,7 @@ const db = require('./src/db/config')
 
 
 const app = express();
-const PORT = 8080;
-
+const PORT = process.env.PORT || 8080;
 
 
 // Express Configuration
@@ -16,20 +20,25 @@ app.use(express.static("public"));
 app.use(morgan('dev'));
 
 
-// Sample GET route
-app.get("/api/data", (req, res) =>
-  res.json({
-    message: "Seems to work!",
-  })
-);
-
+// Routes
 app.get("/", (req, response) => {
   db.query('SELECT * FROM users;')
   .then((res)=>{response.send(res.rows)})
   .catch((err) => {console.log("error", err)})
 });
 
-//PORT setup
+// app.use("/api", menu(db));
+// app.use("/api", order(db));
+// app.use("/api", stores(db));
+// app.use("/api", users(db));
+
+
+// app.get("/api/data", (req, res) =>
+//   res.json({
+//     message: "Seems to work!",
+//   })
+// );
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(
