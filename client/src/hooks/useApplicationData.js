60321a09fe09c317fb836d7f2b5bb0ce10562f5b
Stewart <stewart.mckinlay@gmail.com>
2021-02-15 13:09:19 -0700

    Geocoords in app data

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -8,25 +8,37 @@ export default function useApplicationData() {
     stores: [],
     menus: [],
     orders: [],
+    myCoords: {latitude: 49.281338241296815, longitude: -123.11492992211487}
   });
   
-  const myCoords = {latitude: 49.281338241296815, longitude: -123.11492992211487}
+  
+// const getLocation = () => {
+//   const success = (position) => {
+//    return {latitude: position.latitude, longitude: position.longitude}
+//   }
+//   const error = () =>{
+//     return {latitude: 49.281338241296815, longitude: -123.11492992211487};
+//   }
+//   return navigator.geolocation.getCurrentPosition(success, error, {timeout:5000})
+// };
 
-  // fix orders
+
   useEffect(() => {
     Promise.all([
-      axios.get("/api/users/1"),
+      // getLocation(),
       axios.get("/api/stores"),
+      axios.get("/api/users/7"),
       axios.get("/api/menu/1"),
-      axios.get("/api/users/1"),
+      // axios.get("/api/orders/7"),
     ])
       .then((all) => {
         setState((prev) => ({
           ...prev,
-          users: all[0].data,
-          stores: distance(myCoords, all[1].data),
+          // myCoords: {...prev.myCoords.latitude = all[0].latitude, ...prev.myCoords.longitude = all[0].longitude},
+          stores: distance(state.myCoords, all[0].data),
+          users: all[1].data,
           menus: all[2].data,
-          orders: all[3].data,
+          // orders: all[3].data,
         }));
       })
       .catch((err) =>
