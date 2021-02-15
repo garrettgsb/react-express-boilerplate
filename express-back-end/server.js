fd5b66bf295e02e7e6e99cbb653f8ec484fbb449
a-ebra <a-ebra@users.noreply.github.com>
2021-02-15 16:35:51 -0700

    updated server with cookies and favourites route

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -2,11 +2,21 @@ const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
+const cookieSession = require("cookie-session");
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
+
+// cookies
+app.use(cookieSession({
+  name: "session",
+  keys: ["user_id"]
+}))
+
+// Resource route for favourites:
+app.use("/favourites", favourites(db));
 
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
