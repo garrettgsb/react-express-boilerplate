e81e0c6c03b8dcbc4cc35252090a785608c540cd
Stewart <stewart.mckinlay@gmail.com>
2021-02-16 10:48:52 -0700

    Implement context for getting user data

--- client/src/components/Customer/UserInfo/index.js
+++ client/src/components/Customer/UserInfo/index.js
@@ -4,20 +4,27 @@ import WelcomeMessage from './WelcomeMessage'
 import CurrentBeans from './CurrentBeans'
 import LifetimeBeans from './LifetimeBeans'
 import Tier from './Tier'
+import { useContext } from 'react'
+import {appContext} from '../../appContext'
 
 function UserInfo() {
+
+  const {state} = useContext(appContext) 
+  console.log(state)
+  const userData = state.user[0]
+
   return(
     <div className='user-info-container'>
       <div className='user-info-left-container'>
         <div className='logo-welcome-container'>
         <Logo />
-        <WelcomeMessage name='Andy'/>
+        <WelcomeMessage name={userData.username}/>
         </div>
-        <Tier tier='Super Bean' />
+        <Tier tier={userData.tier} />
       </div>
       <div className='beans-container'>
-        <CurrentBeans currentBeans='230' />
-        <LifetimeBeans lifetimeBeans='560' />
+        <CurrentBeans currentBeans={userData.current_beans} />
+        <LifetimeBeans lifetimeBeans={userData.lifetime_beans} />
       </div>
     </div>
   )
