7c4a22bc479c8334fbc866c57899eef9f996c742
Kevin Chu <kvnchu2@gmail.com>
2021-02-15 18:28:21 -0800

    add sort by most recently updated for repositories, change date to be bigger font on timeline

--- react-front-end/src/components/OppositeTimeline.js
+++ react-front-end/src/components/OppositeTimeline.js
@@ -9,74 +9,76 @@ export default function OppositeTimeline(props) {
 
 
   const repositoryArray = props.repositories;
-  const repositoriesObject = repositoryArray.map((repository) => {
+  
+  const repositoriesObject = repositoryArray.sort((a, b) => new Date(b["updated_at"]) - new Date(a["updated_at"])).map((repository) => {
     const monthConversion = () => {
-      if (repository["created_at"].split("T")[0].split("-")[1] === "01") {
+      if (repository["updated_at"].split("T")[0].split("-")[1] === "01") {
         return "January";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "02"
+        repository["updated_at"].split("T")[0].split("-")[1] === "02"
       ) {
         return "February";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "03"
+        repository["updated_at"].split("T")[0].split("-")[1] === "03"
       ) {
         return "March";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "04"
+        repository["updated_at"].split("T")[0].split("-")[1] === "04"
       ) {
         return "April";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "05"
+        repository["updated_at"].split("T")[0].split("-")[1] === "05"
       ) {
         return "May";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "06"
+        repository["updated_at"].split("T")[0].split("-")[1] === "06"
       ) {
         return "June";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "07"
+        repository["updated_at"].split("T")[0].split("-")[1] === "07"
       ) {
         return "July";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "08"
+        repository["updated_at"].split("T")[0].split("-")[1] === "08"
       ) {
         return "August";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "09"
+        repository["updated_at"].split("T")[0].split("-")[1] === "09"
       ) {
         return "September";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "10"
+        repository["updated_at"].split("T")[0].split("-")[1] === "10"
       ) {
         return "October";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "11"
+        repository["updated_at"].split("T")[0].split("-")[1] === "11"
       ) {
         return "November";
       } else if (
-        repository["created_at"].split("T")[0].split("-")[1] === "12"
+        repository["updated_at"].split("T")[0].split("-")[1] === "12"
       ) {
         return "December";
       }
     };
 
-    const day = repository["created_at"].split("T")[0].split("-")[2];
-    const year = repository["created_at"].split("T")[0].split("-")[0];
+    const day = repository["updated_at"].split("T")[0].split("-")[2];
+    const year = repository["updated_at"].split("T")[0].split("-")[0];
     const month = monthConversion();
 
-    const time = repository["created_at"]
-      .split("T")[1]
-      .split("Z")[0]
-      .split("")
-      .slice(0, 5);
+    // const time = repository["updated_at"]
+    //   .split("T")[1]
+    //   .split("Z")[0]
+    //   .split("")
+    //   .slice(0, 5);
 
     return (
       <div class="timeline-row">
         <div class="timeline-time">
-          {time}
-          <small>
-            {month} {day} {year}
+          <small id="date-size">
+            {month} {day}, {year}
           </small>
+            
+          
         </div>
         <div class="timeline-dot fb-bg"></div>
         <div class="timeline-content">
