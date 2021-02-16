fee509503122d3e5118600b33fd7fa922bc39308
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-15 21:02:52 -0500

    rendering cart info and able to change item quantity

--- client/src/components/Customer/Cart/index.js
+++ client/src/components/Customer/Cart/index.js
@@ -1,23 +1,85 @@
-
+import { useState, useEffect } from 'react'
 import { Modal } from '@material-ui/core'
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+
 import './styles.scss'
 
 function Cart(props) {
 
-  const cartData = (
+  const cartData = [
+    {
+      menuItemId: 1,
+      itemName: 'latte',
+      price: 2.50,
+      quantity: 1
+    },
+    {
+      menuItemId: 2,
+      itemName: 'hot chocolate',
+      price: 2.00,
+      quantity: 2
+    },
+    {
+      menuItemId: 1,
+      itemName: 'latte',
+      price: 2.50,
+      quantity: 1
+    }
+  ]
+
+  const [cartState, setCartSate] = useState(cartData)
+  
+
+  const total = cartState.reduce((a, b) => {
+    console.log(cartState)
+    return a + (b.price * b.quantity)
+  }, 0)
+
+  const cart = (
     <div className='cart-data'>
-      <h2>I am the cart data</h2>
+      <h2>Your cart</h2>
+      <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+      {cartData.map((item, index) => {
+        return (
+          <>
+          <p>{cartState[index].itemName}</p>
+          <RemoveIcon onClick={(event) => setCartSate((prev) => {
+            const cartCopy = [...prev]
+            cartCopy[index].quantity -= 1
+            return cartCopy 
+            }
+             )} />
+          <input 
+            value={cartState[index].quantity}
+            onChange={(event) => setCartSate((prev) => {
+              return [...prev, prev[index].quantity = event.target.value] 
+            }
+             )}
+          />
+          <AddIcon onClick={(event) => setCartSate((prev) => {
+               const cartCopy = [...prev]
+               cartCopy[index].quantity += 1
+               return cartCopy  
+            }
+             )} />
+          <p>${item.price * cartState[index].quantity}</p>
+          </>
+          )
+
+      })}
+      <p>TOTAL: ${total}</p>
+      </form>
+
     </div>
   )
-
-
 
   return(
     <Modal
     open={props.showCart}
     onClose={props.handleClose}
     >
-      {cartData}
+      {cart}
     </Modal>
   )
 }
