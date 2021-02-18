23b2fc5d86f1654f0a2a0dbcab7fb8656e8b5efe
a-ebra <a-ebra@users.noreply.github.com>
2021-02-17 19:09:43 -0700

    updated for host machine settings

--- express-back-end/lib/db.js
+++ express-back-end/lib/db.js
@@ -5,7 +5,7 @@ if (process.env.DATABASE_URL) {
   dbParams = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    user: process.env.DB_USER2,
+    user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
   };
