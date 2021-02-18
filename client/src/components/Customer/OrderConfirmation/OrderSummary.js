7f6a5cbcc0ac4aaa3767f5ad956332c760f670ec
Joseph Williams <josephwilliamsjs@gmail.com>
2021-02-17 21:18:09 -0500

    adding flexbox and moving helpers

--- client/src/components/Customer/OrderConfirmation/OrderSummary.js
+++ client/src/components/Customer/OrderConfirmation/OrderSummary.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { appContext } from '../../appContext'
 
+import {orderSummarySorter, getMostRecent} from '../../../helpers/orderSummary'
 
 export default function StoreSummary(props) {
 
@@ -8,106 +9,20 @@ export default function StoreSummary(props) {
 
   const context = useContext(appContext)
   const orderInfo = context.state.orders
-  
-  const mockData = [
-  {
-    order_id: 1, 
-    menu_item_id: 2,
-    name: 'latte', 
-    price: 2.5,
-    total_price: 10
-  },
-  {
-    order_id: 1, 
-    menu_item_id: 3,
-    name: 'XL latte', 
-    price: 5,
-    total_price: 10
-  },
-  {
-    order_id: 1, 
-    menu_item_id: 4,
-    name: 'hot chocolate', 
-    price: 2.5,
-    total_price: 10
-  },
-  {
-    order_id: 2, 
-    name: 'latte',
-    menu_item_id: 2, 
-    price: 2.5,
-    total_price: 5
-  },
-  {
-    order_id: 2, 
-    menu_item_id: 7,
-    name: 'london fog', 
-    price: 2.5,
-    total_price: 5
-  },
-  {
-    order_id: 3, 
-    menu_item_id: 2,
-    name: 'latte', 
-    price: 2.5,
-    total_price: 2.5
-  },
-  {
-    order_id: 3, 
-    menu_item_id: 2,
-    name: 'latte', 
-    price: 2.5,
-    total_price: 2.5
-  }
-]
-
-  const orderSummarySorter = (arr) => {
-    let output = {};
-    // sort orders
-    arr.forEach((item) => {
-      if(output[item.order_id]) {
-        output[item.order_id].push({
-          name: item.name, 
-          price: item.price,
-          totalPrice: item.total_price
-        })
-      } else {
-        output[item.order_id] = [{
-          name: item.name, 
-          price: item.price,
-          totalPrice: item.total_price
-        }]
-      }
-    })
-    return output;
-  }
-
-  const getMostRecent = (obj) => {
-    let mostRecentOrder = 0;
-    for(const key in obj) {
-      if(key > mostRecentOrder) {
-        mostRecentOrder = key;
-      }
-    }
-    return obj[mostRecentOrder]
-  }
 
   const customerOrderHistory = orderSummarySorter(orderInfo)
   const mostRecentOrder = getMostRecent(customerOrderHistory)
   const mostRecentTotal = mostRecentOrder[0].totalPrice
   
-  console.log('most recent order', mostRecentOrder)
-  
-
 
   return(
     <div>
       <h3>Order Summary</h3>
-      <p>You have earned {(mostRecentTotal / 10)} beans</p>
+      <p>You have earned {(mostRecentTotal / 10)} beans!</p>
       <div className='orders-container'>
       {mostRecentOrder.map((item) => {
         return(
-        <div>
+        <div className='item-container'>
           <p>{item.name}</p>
           <p>${item.price}</p>
         </div>
