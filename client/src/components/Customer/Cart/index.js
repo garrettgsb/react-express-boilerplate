d9df7418420d69111fde7844efaf627b8d685580
Stewart <stewart.mckinlay@gmail.com>
2021-02-19 07:24:39 -0700

    Fix beans values bug

--- client/src/components/Customer/Cart/index.js
+++ client/src/components/Customer/Cart/index.js
@@ -1,94 +1,102 @@
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
-import { Modal } from '@material-ui/core'
-import RemoveIcon from '@material-ui/icons/Remove';
-import AddIcon from '@material-ui/icons/Add';
+import { Modal } from "@material-ui/core";
+import RemoveIcon from "@material-ui/icons/Remove";
+import AddIcon from "@material-ui/icons/Add";
 
-import BeanSlider from './BeanSlider'
+import BeanSlider from "./BeanSlider";
 
-import './styles.scss'
-import { convertCentsToDollars } from '../../../helpers/math';
+import "./styles.scss";
+import { convertCentsToDollars } from "../../../helpers/math";
 
 function Cart(props) {
-  const history = useHistory()
+  const history = useHistory();
 
+  // 1 bean = 50c
   const removeFromTotal = (beans) => {
-    props.setTotal(props.total - beans) 
-  }
+    props.setTotal(props.total - beans * 50);
+  };
   const addToTotal = (beans) => {
-    props.setTotal(props.total + beans) 
-  }
+    props.setTotal(props.total + beans * 50);
+  };
 
   const handleSubmit = (event) => {
-    props.handleClose()
-    history.push('/checkout')
-    event.preventDefault()
-  }
+    props.handleClose();
+    history.push("/checkout");
+    event.preventDefault();
+  };
 
   const cart = (
-    <div className='cart-data'>
+    <div className="cart-data">
       <h2>Your cart</h2>
       <form autoComplete="off" onSubmit={(event) => handleSubmit(event)}>
-      {props.cart.map((item, index) => {
-        return (
-          <>
-          { props.cart[index].quantity > 0 &&
-          <>
-          <p>{props.cart[index].itemName}</p>
-          <RemoveIcon onClick={(event) => props.setCart((prev) => {
-            const cartCopy = [...prev]
-            cartCopy[index].quantity -= 1
-            // remove item from cart 
-            cartCopy[index].quantity === 0 && cartCopy.splice(index, 1)
-            return cartCopy 
-            }
-             )} />
-          <input 
-            value={props.cart[index].quantity}
-            onChange={(event) => props.setCart((prev) => {
-              const cartCopy = [...prev]
-            cartCopy[index].quantity = event.target.value
-            return cartCopy 
-            }
-             )}
-          />
-          <AddIcon onClick={(event) => props.setCart((prev) => {
-               const cartCopy = [...prev]
-               cartCopy[index].quantity += 1
-               return cartCopy  
-            }
-             )} />
-          <p>${convertCentsToDollars(item.price * props.cart[index].quantity)}</p>
-            
-          </>
-          }
-          </>
-          )
-
-      })}
-      <p>TOTAL: ${convertCentsToDollars(props.total)}</p>
-      <p>Grind some beans?</p>
-      <BeanSlider 
-        removeFromTotal={removeFromTotal} 
-        addToTotal={addToTotal}
-        beansSpent={props.beansSpent}
-        setBeansSpent={props.setBeansSpent}
-        total={props.cart}
-      />
-      <input type='submit' value='Bean me up Scottie!' />
+        {props.cart.map((item, index) => {
+          return (
+            <>
+              {props.cart[index].quantity > 0 && (
+                <>
+                  <p>{props.cart[index].itemName}</p>
+                  <RemoveIcon
+                    onClick={(event) =>
+                      props.setCart((prev) => {
+                        const cartCopy = [...prev];
+                        cartCopy[index].quantity -= 1;
+                        // remove item from cart
+                        cartCopy[index].quantity === 0 &&
+                          cartCopy.splice(index, 1);
+                        return cartCopy;
+                      })
+                    }
+                  />
+                  <input
+                    value={props.cart[index].quantity}
+                    onChange={(event) =>
+                      props.setCart((prev) => {
+                        const cartCopy = [...prev];
+                        cartCopy[index].quantity = event.target.value;
+                        return cartCopy;
+                      })
+                    }
+                  />
+                  <AddIcon
+                    onClick={(event) =>
+                      props.setCart((prev) => {
+                        const cartCopy = [...prev];
+                        cartCopy[index].quantity += 1;
+                        return cartCopy;
+                      })
+                    }
+                  />
+                  <p>
+                    $
+                    {convertCentsToDollars(
+                      item.price * props.cart[index].quantity
+                    )}
+                  </p>
+                </>
+              )}
+            </>
+          );
+        })}
+        <p>TOTAL: ${convertCentsToDollars(props.total)}</p>
+        <p>Grind some beans?</p>
+        <BeanSlider
+          removeFromTotal={removeFromTotal}
+          addToTotal={addToTotal}
+          beansSpent={props.beansSpent}
+          setBeansSpent={props.setBeansSpent}
+          total={props.cart}
+        />
+        <input type="submit" value="Bean me up Scottie!" />
       </form>
-
     </div>
-  )
+  );
 
-  return(
-    <Modal
-    open={props.showCart}
-    onClose={props.handleClose}
-    >
+  return (
+    <Modal open={props.showCart} onClose={props.handleClose}>
       {cart}
     </Modal>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
--- client/src/helpers/math.js
+++ client/src/helpers/math.js
@@ -1,18 +1,28 @@
 const accelerator = {
   "One-Cup-A-Day": { min: 0, max: 1000 },
   "Regular-Joe": { min: 1001, max: 2000 },
-  Purist: { min: 2001 },
+  "Purist": { min: 2001 },
 };
 
 export const convertCentsToDollars = function (price) {
   return (price / 100).toFixed(2);
 };
 
-export const beansEarned = function (accelerator, centsCharged) {
+export const beansEarned = function (accelerator, centsCharged, prev=0) {
   // user can only earn beans on price charged and not total price
-  return Math.floor((centsCharged / 100) * accelerator);
+  return Math.floor((centsCharged / 100) * accelerator) + prev;
 };
 
 export const totalFromCart = function (cart) {
   return cart.reduce((sum, cur) => sum + cur.price * cur.quantity, 0);
 };
+
+export const newCurrentBeans = (currentBeans , beansSpent) => {
+  return currentBeans - beansSpent;
+}
+
+export const newLifetimeBeans = (currentLifetimeBeans, total) => {
+  const beansToAdd = Math.floor(total / 100)
+  return currentLifetimeBeans + beansToAdd;
+}
+
