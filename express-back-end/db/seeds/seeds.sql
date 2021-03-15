be8e2949f9e556d76077d0c7a263c3e35e60b711
JSmith604 <jacquelinesmith190@gmail.com>
2021-03-15 15:06:36 -0700

    Start draft of species table in seeds

--- express-back-end/db/seeds/seeds.sql
+++ express-back-end/db/seeds/seeds.sql
@@ -19,7 +19,23 @@ VALUES ('African', 'Grey', 'smart_bird@gmail.com', 'password5', '33333333');
 
 
 INSERT INTO species (common_name, scientific_name, photo_url, description, watering_requirement_rating, sunlight_requirement_rating, difficulty_rating, temperature_requirements, fertilizer_requirements, poison_for_pets)
-VALUES ('Alo Vera', 'Aloe Barbadensis', 'https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/image_nodes/aloe-vera-white-pot_sunwand24-ss_edit.jpg?itok=Y7HnaYk3', 20, 'J.K. Rowling', 'Like new', 'https://images.freeimages.com/images/large-previews/8de/harry-potter-book-5-1458017.jpg');
+VALUES ('Alo Vera', 'Aloe Barbadensis', 'https://www.almanac.com/sites/default/files/styles/primary_image_in_article/public/image_nodes/aloe-vera-white-pot_sunwand24-ss_edit.jpg?itok=Y7HnaYk3', 'Plant description here', 1, 4, 2, '15-24 C', 'Once per year', true);
 
+INSERT INTO species (common_name, scientific_name, photo_url, description, watering_requirement_rating, sunlight_requirement_rating, difficulty_rating, temperature_requirements, fertilizer_requirements, poison_for_pets)
+VALUES ('ZZ Plant', 'Zamioculcas Zamiifolia ', 'http://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_zz-plant_variant_medium_grant_terracotta_1200x.jpg?v=1613670666', 'Plant description here', 4, 3, 2, '18-24 C', 'None', true);
 
+INSERT INTO species (common_name, scientific_name, photo_url, description, watering_requirement_rating, sunlight_requirement_rating, difficulty_rating, temperature_requirements, fertilizer_requirements, poison_for_pets)
+VALUES ('Snake Plant', 'Dracaena trifasciata (prev. Sanseveria trifasciata)', 'https://www.thespruce.com/thmb/3ZzeafMMYBupme3O5dodMz3uoxI=/2048x1545/filters:no_upscale():max_bytes(150000):strip_icc()/snake-plant-care-overview-1902772-04-3f69d04885af4613bf2eda197704fe20.jpg', 'Plant description here', 2, 3, 1, '21-30 C', 'None, may fertilize once or twice during growing season', true);
+
+INSERT INTO species (common_name, scientific_name, photo_url, description, watering_requirement_rating, sunlight_requirement_rating, difficulty_rating, temperature_requirements, fertilizer_requirements, poison_for_pets)
+VALUES ('Spider Plant', 'Chlorophytum Comosum', 'https://cdn.shopify.com/s/files/1/0108/9460/6436/products/artificial-spider-plant-hanging-basket-nearly-natural-137604.jpg?v=1584171644', 'Plant description here', 2, 2, 1, '21-30 C', 'None', false);
+
+INSERT INTO species (common_name, scientific_name, photo_url, description, watering_requirement_rating, sunlight_requirement_rating, difficulty_rating, temperature_requirements, fertilizer_requirements, poison_for_pets)
+VALUES ('Boston Fern', 'Nephrolepis exaltata', 'https://thelittlebotanical.com/wp-content/uploads/2019/03/07_JT_006_1000x1000.jpg', 'Plant description here', 5, 3, 3, '15-24 C', 'Every 8-8 weeks', false);
+
+INSERT INTO species (common_name, scientific_name, photo_url, description, watering_requirement_rating, sunlight_requirement_rating, difficulty_rating, temperature_requirements, fertilizer_requirements, poison_for_pets)
+VALUES ('African Violet', 'Saintpaulia Ionantha', 'https://brownsflorist.com/wp-content/uploads/2020/04/browns-10-scaled-e1590596735884.jpg', 'Plant description here', 5, 2, 4, '15-24 C', 'Every 2 weeks', false);
+
+INSERT INTO species (common_name, scientific_name, photo_url, description, watering_requirement_rating, sunlight_requirement_rating, difficulty_rating, temperature_requirements, fertilizer_requirements, poison_for_pets)
+VALUES ('Haworthia', 'Haworthia', 'https://cdn.shopify.com/s/files/1/2097/3287/products/4.00-ozSucculent.ZebraPlant-Haworthiafasciata-count32_2048x@2x.jpg?v=1589641850', 'Plant description here', 1, 4, 2, '21-30 C', 'Twice per year', False);
 
