865c37849ed907094e19e77dc1f0cdb1e5990495
JJ <jerrica.mj@gmail.com>
2021-03-17 05:03:58 +0000

    Add a styled component navbar menu. TODO: Add logo image, center links to middle, and update button color.

--- react-front-end/src/components/navbar/index.js
+++ react-front-end/src/components/navbar/index.js
@@ -1,75 +1,77 @@
-import React from "react";
-import {Navbar, Nav, Button} from "react-bootstrap";
+// import React from "react";
+// import {Navbar, Nav, Button} from "react-bootstrap";
 
-export default function NavMenu(props) {
+// export default function NavMenu(props) {
 
-  return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Leaf It To Me</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="justify-content-end" style={{ width: "100%" }}>
-        {/* <Nav className="ml-auto"> */}
-          <Nav.Link href="#home">About</Nav.Link>
-          <Nav.Link href="#link">Find a Plant</Nav.Link>
-        </Nav>
-          <Button variant="outline-success">Log In</Button>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
-
-
-
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { Nav, Navbar } from 'react-bootstrap';
-// // import styled from 'styled-components';
-
-// // const Styles = styled.div`
-// //   .navbar {
-// //     background-color: #222;
-// //   }
-// //   a, .navbar-brand, .navbar-nav .nav-link {
-// //     color: #bbb;
-// //     &:hover {
-// //       color: white;
-// //     }
-// //   }
-// // `;
-
-// export default function NavMenu(){
 //   return (
-//   // <Styles>
-//     <Navbar expand="lg">
-//       <Navbar.Brand href="/">Code Life</Navbar.Brand>
+//     <Navbar bg="light" expand="lg">
+//       <Navbar.Brand href="#home">Leaf It To Me</Navbar.Brand>
 //       <Navbar.Toggle aria-controls="basic-navbar-nav" />
 //       <Navbar.Collapse id="basic-navbar-nav">
-//         <Nav className="ml-auto">
-//           <Nav.Item>
-//             <Nav.Link>
-//               <Link to="/">Home</Link>
-//             </Nav.Link>
-//           </Nav.Item>
-//           <Nav.Item>
-//             <Nav.Link>
-//               <Link to="/garden">Garden</Link>
-//             </Nav.Link>
-//           </Nav.Item>
-//           <Nav.Item>
-//             <Nav.Link>
-//               <Link to="/search">Search</Link>
-//             </Nav.Link>
-//           </Nav.Item>
-//           <Nav.Item>
-//             <Nav.Link>
-//               <Link to="/login">Log In</Link>
-//             </Nav.Link>
-//           </Nav.Item>
+//         <Nav className="justify-content-end" style={{ width: "100%" }}>
+//         {/* <Nav className="ml-auto"> */}
+//           <Nav.Link href="#home">About</Nav.Link>
+//           <Nav.Link href="#link">Find a Plant</Nav.Link>
 //         </Nav>
+//           <Button variant="outline-success">Log In</Button>
 //       </Navbar.Collapse>
 //     </Navbar>
-//   // </Styles >
 //   );
-// }
+// };
+
+
+
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Nav, Navbar, Button } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #FBF6EE;
+  }
+  a, .navbar-brand, .navbar-nav .nav-link {
+    color: #3B715A;
+    font-family: "Averia Libre", Helvetica, sans-serif !important;
+    text-decoration: none;
+    &:hover {
+      color: #F9C78C;
+    }
+  }
+`;
+
+export default function NavMenu(){
+  return (
+    <Styles>
+      <Navbar expand="lg">
+        <Navbar.Brand href="/">Leaf It To Me</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/garden">Garden</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/search">Search</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/login"><Button variant="outline-primary">Log In</Button></Link>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Styles >
+  );
+}
