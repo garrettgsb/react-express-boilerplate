9d35a64bf5839015d499f220fce0653f1f945020
JJ <jerrica.mj@gmail.com>
2021-03-17 05:26:05 +0000

    Add logo images to assets. Create cards component for plants.

---
+++ react-front-end/src/components/plants/card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+// import Styles from "styled-components";
+
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
