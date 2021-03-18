67cec0fbca897c1774f123566f495b29268e7a10
JJ <jerrica.mj@gmail.com>
2021-03-18 06:28:58 +0000

    Update PlantListItem to use props for dynamic element creation. Move styling and container to PlantList component. TODO: create mock data and render a full list of plants in the garden.

--- react-front-end/src/components/plantList/plantListItem.js
+++ react-front-end/src/components/plantList/plantListItem.js
@@ -1,183 +1,56 @@
 import React from "react";
-import { Card, Container } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import aloeVeraImage from "../../assets/aloe-vera-cropped.png";
-import styled from "styled-components";
 
-const Styles = styled.div`
-  .card {
-    font-family: "Montserrat", Helvetica, sans-serif;
-    border-radius: 15px;
 
-    img {
-      height: 175px;
-      width: 175px;
-      border-radius: 100%;
-      margin: 15px auto 5px auto;
-    }
-  }
-`;
-
-export default function PlantListItem() {
+export default function PlantListItem(props) {
   return (
-    <Styles>
+    <>
 
-      <Container>
-        <div class="row my-4 mx-4 justify-content-center">
+      <div class="col-12 col-sm-11 col-md-6 col-lg-4 col-xl-3 my-3">
+      <Card>
+        <Card.Img variant="top" src={aloeVeraImage} />
+        <Card.Body class="mx-auto">
+          <Card.Title>{props.nickname || props.name}</Card.Title>
+          <Card.Subtitle class="text-center">{props.nickname && props.name}</Card.Subtitle>
+          {!props.nickname && <br/>}
+        </Card.Body>
 
-          <div class="col-12 col-md-6 col-lg-4 col-xl-3 my-3">
-          <Card>
-            <Card.Img variant="top" src={aloeVeraImage} />
-            <Card.Body class="mx-auto">
-              <Card.Title>Ferdinand</Card.Title>
-              <Card.Subtitle class="text-center">Aloe Vera 1</Card.Subtitle>
-            </Card.Body>
-
-            <Card.Body>
-              <div class="row px-2 no-gutters">
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-seedling"></i></h3>
-                    <p class="text-center">easy</p>
-                </div>
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-tint"></i></h3>
-                    <p class="text-center">10-14 days</p>
-                </div>
-              </div>
-              <div class="row px-2 no-gutters">
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-sun"></i></h3>
-                    <p class="text-center">full sun</p>
-                </div>
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-thermometer-half"></i></h3>
-                    <p class="text-center">18-24°C</p>
-                </div>
-              </div>
-            </Card.Body>
-
-            <Card.Body class="mx-auto mb-2">
-              <Card.Link href="#" class="btn btn-outline-success">See More</Card.Link>
-            </Card.Body>
-          </Card>
+        <Card.Body>
+          <div class="row px-2 no-gutters">
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-seedling"></i></h3>
+                <p class="text-center">{props.difficulty}</p>
+            </div>
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-tint"></i></h3>
+                <p class="text-center">{props.water} days</p>
+            </div>
           </div>
-
-          <div class="col-12 col-md-6 col-lg-4 col-xl-3 my-3">
-          <Card>
-            <Card.Img variant="top" src={aloeVeraImage} />
-            <Card.Body class="mx-auto">
-              <Card.Title>Ferdinand</Card.Title>
-              <Card.Subtitle class="text-center">Aloe Vera 2</Card.Subtitle>
-            </Card.Body>
-
-            <Card.Body>
-              <div class="row px-2 no-gutters">
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-seedling"></i></h3>
-                    <p class="text-center">easy</p>
-                </div>
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-tint"></i></h3>
-                    <p class="text-center">10-14 days</p>
-                </div>
-              </div>
-              <div class="row px-2 no-gutters">
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-sun"></i></h3>
-                    <p class="text-center">full sun</p>
-                </div>
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-thermometer-half"></i></h3>
-                    <p class="text-center">18-24°C</p>
-                </div>
-              </div>
-            </Card.Body>
-
-            <Card.Body class="mx-auto mb-2">
-              <Card.Link href="#" class="btn btn-outline-success">See More</Card.Link>
-            </Card.Body>
-          </Card>
+          <div class="row px-2 no-gutters">
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-sun"></i></h3>
+                <p class="text-center">{props.sun}</p>
+            </div>
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-thermometer-half"></i></h3>
+                <p class="text-center">{props.temp}°C</p>
+            </div>
           </div>
+        </Card.Body>
 
-          <div class="col-12 col-sm-11 col-md-6 col-lg-4 col-xl-3 my-3">
-          <Card>
-            <Card.Img variant="top" src={aloeVeraImage} />
-            <Card.Body class="mx-auto">
-              <Card.Title>Ferdinand</Card.Title>
-              <Card.Subtitle class="text-center">Aloe Vera 3</Card.Subtitle>
-            </Card.Body>
-            <Card.Body>
-              <div class="row px-2 no-gutters">
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-seedling"></i></h3>
-                    <p class="text-center">easy</p>
-                </div>
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-tint"></i></h3>
-                    <p class="text-center">10-14 days</p>
-                </div>
-              </div>
-              <div class="row px-2 no-gutters">
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-sun"></i></h3>
-                    <p class="text-center">full sun</p>
-                </div>
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-thermometer-half"></i></h3>
-                    <p class="text-center">18-24°C</p>
-                </div>
-              </div>
-            </Card.Body>
-            <Card.Body class="mx-auto mb-2">
-              <Card.Link href="#" class="btn btn-outline-success">See More</Card.Link>
-            </Card.Body>
-          </Card>
-          </div>
+        <Card.Body class="mx-auto mb-2">
+          <Card.Link href="#" class="btn btn-outline-success">See More</Card.Link>
+        </Card.Body>
+      </Card>
+      </div>
 
-          <div class="col-12 col-sm-11 col-md-6 col-lg-4 col-xl-3 my-3">
-          <Card>
-            <Card.Img variant="top" src={aloeVeraImage} />
-            <Card.Body class="mx-auto">
-              <Card.Title>Ferdinand</Card.Title>
-              <Card.Subtitle class="text-center">Aloe Vera 3</Card.Subtitle>
-            </Card.Body>
-            <Card.Body>
-              <div class="row px-2 no-gutters">
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-seedling"></i></h3>
-                    <p class="text-center">easy</p>
-                </div>
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-tint"></i></h3>
-                    <p class="text-center">10-14 days</p>
-                </div>
-              </div>
-              <div class="row px-2 no-gutters">
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-sun"></i></h3>
-                    <p class="text-center">full sun</p>
-                </div>
-                <div class="col-6">
-                    <h3 class="card card-block border-0 text-center"><i class="fas fa-thermometer-half"></i></h3>
-                    <p class="text-center">18-24°C</p>
-                </div>
-              </div>
-            </Card.Body>
-            <Card.Body class="mx-auto mb-2">
-              <Card.Link href="#" class="btn btn-outline-success">See More</Card.Link>
-            </Card.Body>
-          </Card>
-          </div>
-
-        </div>
-      </Container>
-
-
-
-      {/* <Card style={{ width: '18rem' }}>
+      <div class="col-12 col-sm-11 col-md-6 col-lg-4 col-xl-3 my-3">
+      <Card>
         <Card.Img variant="top" src={aloeVeraImage} />
         <Card.Body class="mx-auto">
           <Card.Title>Ferdinand</Card.Title>
-          <Card.Subtitle class="text-center">Aloe Vera</Card.Subtitle>
+          <Card.Subtitle class="text-center">Aloe Vera 2</Card.Subtitle>
         </Card.Body>
 
         <Card.Body>
@@ -204,9 +77,82 @@ export default function PlantListItem() {
         </Card.Body>
 
         <Card.Body class="mx-auto mb-2">
-          <Card.Link href="#"><Button>See More</Button></Card.Link>
+          <Card.Link href="#" class="btn btn-outline-success">See More</Card.Link>
         </Card.Body>
-      </Card> */}
-    </Styles>
+      </Card>
+      </div>
+
+      <div class="col-12 col-sm-11 col-md-6 col-lg-4 col-xl-3 my-3">
+      <Card>
+        <Card.Img variant="top" src={aloeVeraImage} />
+        <Card.Body class="mx-auto">
+          <Card.Title>Ferdinand</Card.Title>
+          <Card.Subtitle class="text-center">Aloe Vera 3</Card.Subtitle>
+        </Card.Body>
+        <Card.Body>
+          <div class="row px-2 no-gutters">
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-seedling"></i></h3>
+                <p class="text-center">easy</p>
+            </div>
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-tint"></i></h3>
+                <p class="text-center">10-14 days</p>
+            </div>
+          </div>
+          <div class="row px-2 no-gutters">
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-sun"></i></h3>
+                <p class="text-center">full sun</p>
+            </div>
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-thermometer-half"></i></h3>
+                <p class="text-center">18-24°C</p>
+            </div>
+          </div>
+        </Card.Body>
+        <Card.Body class="mx-auto mb-2">
+          <Card.Link href="#" class="btn btn-outline-success">See More</Card.Link>
+        </Card.Body>
+      </Card>
+      </div>
+
+      <div class="col-12 col-sm-11 col-md-6 col-lg-4 col-xl-3 my-3">
+      <Card>
+        <Card.Img variant="top" src={aloeVeraImage} />
+        <Card.Body class="mx-auto">
+          <Card.Title>Ferdinand</Card.Title>
+          <Card.Subtitle class="text-center">Aloe Vera 3</Card.Subtitle>
+        </Card.Body>
+        <Card.Body>
+          <div class="row px-2 no-gutters">
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-seedling"></i></h3>
+                <p class="text-center">easy</p>
+            </div>
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-tint"></i></h3>
+                <p class="text-center">10-14 days</p>
+            </div>
+          </div>
+          <div class="row px-2 no-gutters">
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-sun"></i></h3>
+                <p class="text-center">full sun</p>
+            </div>
+            <div class="col-6">
+                <h3 class="card card-block border-0 text-center"><i class="fas fa-thermometer-half"></i></h3>
+                <p class="text-center">18-24°C</p>
+            </div>
+          </div>
+        </Card.Body>
+        <Card.Body class="mx-auto mb-2">
+          <Card.Link href="#" class="btn btn-outline-success">See More</Card.Link>
+        </Card.Body>
+      </Card>
+      </div>
+
+    </>
+
   );
 }
