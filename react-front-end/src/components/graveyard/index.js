3b9a4fad88171be7d3b83d00e3c1a009fdcddeb8
JJ <jerrica.mj@gmail.com>
2021-03-20 21:26:56 +0000

    Add dynamic rendering of the user's plants from the db that are dead.

--- react-front-end/src/components/graveyard/index.js
+++ react-front-end/src/components/graveyard/index.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {Container} from "react-bootstrap";
 
 import Hero from "../hero";
+import PlantList from "../plantList/plantList";
 
 export default function Graveyard(props) {
+  const [myGraveyard, setMyGraveyard] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/garden", {withCredentials: true})
+    .then((res) => {
+      console.log("Request for garden data received by the server");
+      // console.log(res.data);
+
+      const filtered = res.data.filter((myPlant)=> {
+        return myPlant.is_dead === true;
+      })
+      console.log(filtered);
+      setMyGraveyard(filtered);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, []);
 
   return (
     <>
@@ -13,7 +33,10 @@ export default function Graveyard(props) {
         gardenButton="true"
       ></Hero>
       <Container>
-      <h2>Plant Graveyard</h2>
+      {/* <h2>Plant Graveyard</h2> */}
+      <PlantList
+          list={myGraveyard}
+        />
       </Container>
     </>
   );
