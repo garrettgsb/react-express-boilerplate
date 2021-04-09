8421348305e0e3ee5c9496e78146f797fa8fd549
Sknoop2 <sknoop@hotmail.ca>
2021-04-09 10:38:13 -0600

    adjust db table order

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -12,49 +12,56 @@ DROP TABLE IF EXISTS location_ratings CASCADE;
 
 CREATE TABLE photographers (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL, 
   email VARCHAR(255) NOT NULL,
   city VARCHAR(255),
-  phone_number INTEGER NOT NULL
+  phone_number VARCHAR(255) NOT NULL
 );
 
 -- photographer_id here is the *one endorsing another photographer* --
 CREATE TABLE endorsements (
   id SERIAL PRIMARY KEY NOT NULL,
-  text VARCHAR(255) NOT NULL,
   photographer_id INTEGER REFERENCES photographers(id),
-)
+  text VARCHAR(255) NOT NULL
+);
 
 -- photographer_id here is the *one being endorsed* --
 CREATE TABLE photographers_endorsements (
   id SERIAL PRIMARY KEY NOT NULL,
   photographer_id INTEGER REFERENCES photographers(id),
   endorsement_id INTEGER REFERENCES endorsements(id)
-)
+);
+
+CREATE TABLE auroras (
+  id SERIAL PRIMARY KEY NOT NULL,
+  forecast VARCHAR(255) NOT NULL,
+  location VARCHAR(255) NOT NULL,
+  strength INTEGER NOT NULL
+);
 
 CREATE TABLE albums (
   id SERIAL PRIMARY KEY NOT NULL,
-  photos VARCHAR(255) NOT NULL,
-  photographer_id INTEGER REFERENCES photographers(id) ON DELETE CASCADE
+  photographer_id INTEGER REFERENCES photographers(id) ON DELETE CASCADE,
+  photos VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE locations (
   id SERIAL PRIMARY KEY NOT NULL,
-  location_name VARCHAR(255) NOT NULL,
-  coordinates VARCHAR(255) NOT NULL,
+  average_rating INTEGER,
   aurora_id INTEGER REFERENCES auroras(id) ON DELETE CASCADE,
-  location_rating INTEGER REFERENCES location_ratings(id) ON DELETE CASCADE
+  location_name VARCHAR(255) NOT NULL,
+  coordinates VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE meetups (
   id SERIAL PRIMARY KEY NOT NULL,
-  location_id INTEGER REFERENCES locations(id) ON DELETE CASCADE,
+  location_id INTEGER REFERENCES locations(id) ON DELETE CASCADE
 );
 
 CREATE TABLE meetup_photographers (
   id SERIAL PRIMARY KEY NOT NULL,
-  meetup_id INTEGER REFERENCES meetups(id) ON DELETE CASCADE
-  photographer_id INTEGER REFERENCES photographers(id) ON DELETE CASCADE,
+  meetup_id INTEGER REFERENCES meetups(id) ON DELETE CASCADE,
+  photographer_id INTEGER REFERENCES photographers(id) ON DELETE CASCADE
 );
 
 CREATE TABLE equipment (
@@ -67,14 +74,6 @@ CREATE TABLE equipment (
 
 CREATE TABLE location_ratings (
   id SERIAL PRIMARY KEY NOT NULL,
-  location_id INTEGER REFERENCES location(id) ON DELETE CASCADE,
+  location_id INTEGER REFERENCES locations(id) ON DELETE CASCADE,
   number_stars INTEGER NOT NULL
-)
-
-CREATE TABLE auroras (
-  id SERIAL PRIMARY KEY NOT NULL,
-  forecast VARCHAR(255) NOT NULL,
-  location VARCHAR(255) NOT NULL,
-  strength INTEGER NOT NULL
 );
-
