3f96f21b20e42b1ea87c52cfdbb5c1ed2248818b
Neil Oribello <neil.oribello@gmail.com>
2021-04-09 23:09:03 +0000

    seeding data for  reviews

---
+++ express-back-end/db/seeds/reviews.sql
@@ -0,0 +1,9 @@
+INSERT INTO reviews (title, building_rating, area_rating, comment, landlord_rating, recommend_to_friend)
+VALUES
+('This place is amazing!', 5, 4, 'Great building with great ammenties around the building. Landlord is sensational!', true, true),
+
+('Buyers Beware!', 2, 2, 'The apartment might be nice but the surronding area is just so distracting and noisy! The landlord does not fix anything around here.', false, false),
+
+('Great place to live!', 5, 5, 'Ammentities are amazing, great view, great landlord', false, false),
+
+('Could use some work!', 3, 3, 'Landlords are trust worthy however area is lacking ammentities', false, false);
