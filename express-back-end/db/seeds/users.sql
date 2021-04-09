478bfd6ebfc0a7cafc25ad1b70d516cbbd51e8cd
Brody Bayley <brodybayley@gmail.com>
2021-04-09 23:17:42 +0000

    Add users seeds

---
+++ express-back-end/db/seeds/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, email, password, created_at)
+      VALUES ('paddycake', 'bakersman@gmail.com', '1234', '2021-04-01 04:07:31'),
+      ('hughie', 'hola@gmail.com', '1234', '2021-04-02 09:07:31'),
+      ('fivestar', 'ttyl@gmail.com', '1234', '2021-04-03 07:07:31');
