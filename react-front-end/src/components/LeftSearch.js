c83d9968fdcb7afc3faad223805801ae33c280fb
hansori01 <19sorihan88@gmail.com>
2021-04-10 14:35:11 -0700

    changing file names for react components

---
+++ react-front-end/src/components/LeftSearch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './LeftSearch.scss';
+
+export default function Leftsearch () {
+
+return (
+  <section className="leftsearch">
+    <div> sidebar-search/filters </div>
+  </section>
+);
+}
---
+++ react-front-end/src/components/RightTweets.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './RightTweets.scss';
+
+export default function RightTweets () {
+
+return (
+  <section className="righttweets">
+    <div> right- tweets tweets go here! duh ;) </div>
+  </section>
+);
+}
