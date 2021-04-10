cf76f8f4eebc5eff10d4972ebf390686c6506424
Sam Ito <isamsito@gmail.com>
2021-04-09 22:35:45 -0600

    Add US and CA stream, plus local query

--- express-back-end/queries.js
+++ express-back-end/queries.js
@@ -1,5 +1,7 @@
 const Twit = require('twit');
 const Sentiment = require('sentiment');
+const needle = require('needle');
+const _ = require('lodash');
 require('dotenv').config()
 
 const T = new Twit({
@@ -14,11 +16,10 @@ const T = new Twit({
 const calgaryPointRadius = '51.0447,-114.0719,100mi'
 
 
-const streamCanadaBorderBox = function() {
-  const canada = ['-140.99778', '41.6751050889', '-52.6480987209', '83.23324'];
+// This works, basic search for word
+const streamKeyword = function(searchWord) {
   const stream = T.stream('statuses/filter', {
-    locations: canada,
-    track: '#RemoveThePM',
+    track: searchWord,
     language: 'en'
   });
   stream.on('tweet', async tweet => {
@@ -26,21 +27,53 @@ const streamCanadaBorderBox = function() {
   });
 }
 
-const streamUSBorderBox = function() {
-  const USA = ['-171.791110603', '18.91619', '-66.96466', '71.3577635769'];
+let tweetCount = 0;
+
+const streamCanadaBorderBox = function(searchWord) {
+  const canada = ['-140.99778', '41.6751050889', '-52.6480987209', '83.23324'];
+  // const regexpression = /(?i)#RemoveThePM(?-i).*/gi
+  const regexpression = searchWord
+  const regex = new RegExp(regexpression, "gi");
+  
+
   const stream = T.stream('statuses/filter', {
+    track: searchWord,
     locations: canada,
-    track: '#RemoveThePM',
     language: 'en'
   });
   stream.on('tweet', async tweet => {
+    if(tweet.text.match(regex)){
       console.log(tweet);
+    }
+      console.log(tweetCount);
+      tweetCount++;
+  });
+}
+
+
+const streamUSBorderBox = function(searchWord) {
+  const USA = ['-171.791110603', '18.91619', '-66.96466', '71.3577635769'];
+
+  const regexpression = searchWord
+  const regex = new RegExp(regexpression, "gi");
+
+  const stream = T.stream('statuses/filter', {
+    locations: USA,
+    track: '#Trump',
+    language: 'en'
+  });
+  stream.on('tweet', async tweet => {
+    if(tweet.text.match(regex)){
+      console.log(tweet);
+    }
+    console.log(tweetCount);
+      tweetCount++;
   });
 }
 
 
 const getTweetsFromPointRadius = function(pointRadius) {
-  T.get('search/tweets', { q:`#RemoveThePM geocode:${pointRadius} -filter:RT`, count: 10 }, function(err, data, response) {
+  T.get('search/tweets', { q:`#RemoveThePM geocode:${pointRadius}`, count: 10 }, function(err, data, response) {
     console.log(data)
   
     console.log('######################################################################');
@@ -50,5 +83,53 @@ const getTweetsFromPointRadius = function(pointRadius) {
   })
 }
 
-// getTweetsFromPointRadius(calgaryPointRadius);
-streamCanadaBorderBox()
+// const canada = ['-140.99778', '41.6751050889', '-52.6480987209', '83.23324'];
+// let stream = T.stream('statuses/filter', {
+//   track: '#RemoveThePM',
+//   language: 'en'
+// });
+// stream.on('tweet', async tweet => {
+//   console.log('########################### NEW TWEET ###########################');
+//     console.log(tweet.text);
+//     if (tweet.user){
+//       console.log('User location: ', tweet.user.location);
+//     }
+//     if (tweet.place) {
+//       console.log('_______________Place data__________________');
+//       console.log(tweet.place)
+//     }
+//     console.log('######################################################################');
+// });
+
+// streamCanadaBorderBox('#RemoveThePM');
+// streamUSBorderBox('#Obama');
+
+// const runSingleQuery = function() {
+//   const headers = {
+//     Authorization: `Bearer ${process.env.BEARER_TOKEN}`
+//   }
+//   needle.get('https://api.twitter.com/1.1/search/tweets.json?q=%23RemoveThePM%20-filter%3Aretweets%20AND%20-filter%3Areplies&geocode=48.428421,-123.365646,30km',{headers: headers}, function(error, response) {
+//     if (!error && response.statusCode == 200)
+//     console.log(response.body);
+//   });
+// }
+
+const senti = new Sentiment();
+
+const runSingleQuery = function() {
+  const headers = {
+    Authorization: `Bearer ${process.env.BEARER_TOKEN}`
+  }
+  needle.get(`https://api.twitter.com/1.1/search/tweets.json?q=%23RemoveThePM%20-filter%3Aretweets%20AND%20-filter%3Areplies&geocode=${calgaryPointRadius}`,{headers: headers}, function(error, response) {
+    if (!error && response.statusCode == 200)
+    console.log(response.body);
+    console.log('##############################################################################')
+    response.body.statuses.forEach(status => {
+      // console.log(status.user);
+      console.log(senti.analyze(status.text));
+    })
+  });
+}
+
+
+runSingleQuery();
