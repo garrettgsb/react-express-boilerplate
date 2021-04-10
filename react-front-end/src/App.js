7a898ba3ba43fd0b24b0fd0fbe3b59511ce7a2b5
Brody Bayley <brodybayley@gmail.com>
2021-04-10 05:39:41 +0000

    Integrate leaflet map into app js and add css and html formatting

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,38 +1,60 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "./App.css";
+import useSwr from "swr";
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+function App() {
+  const url =
+    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSwr(url, { fetcher });
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
+  const crimes = data && !error ? data.slice(0, 100) : [];
 
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
-    );
-  }
+  return (
+    <MapContainer center={[52.6376, -1.135171]} zoom={12}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {crimes.map((crime) => (
+        <Marker
+          key={crime.id}
+          position={[crime.location.latitude, crime.location.longitude]}
+        />
+      ))}
+    </MapContainer>
+  );
 }
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     message: "Click the button to load data!",
+//   };
+// }
+
+// fetchData = () => {
+//   axios
+//     .get("/api/data") // You can simply make your requests to "/api/whatever you want"
+//     .then((response) => {
+//       // handle success
+//       console.log(response.data); // The entire response from the Rails API
+
+//       console.log(response.data.message); // Just the message
+//       this.setState({
+//         message: response.data.message,
+//       });
+//     });
+// };
+
+// render() {
+//   return (
+//     <div className="App">
+//       <h1>{this.state.message}</h1>
+//       <button onClick={this.fetchData}>Fetch Data</button>
+//     </div>
+//   );
+// }
 
 export default App;
