d177c88b27c213a8341af76778790a314c9cc912
Sam Ito <isamsito@gmail.com>
2021-04-10 15:35:44 -0600

    refactor slightly

--- express-back-end/queries.js
+++ express-back-end/queries.js
@@ -2,6 +2,9 @@ const Twit = require('twit');
 const Sentiment = require('sentiment');
 const needle = require('needle');
 const _ = require('lodash');
+const { identity } = require('lodash');
+const fs = require('fs');
+const util = require('util');
 require('dotenv').config()
 
 const T = new Twit({
@@ -27,25 +30,23 @@ const streamKeyword = function(searchWord) {
   });
 }
 
-let tweetCount = 0;
-
 const streamCanadaBorderBox = function(searchWord) {
   const canada = ['-140.99778', '41.6751050889', '-52.6480987209', '83.23324'];
-  // const regexpression = /(?i)#RemoveThePM(?-i).*/gi
+  
   const regexpression = searchWord
   const regex = new RegExp(regexpression, "gi");
   
-
   const stream = T.stream('statuses/filter', {
     track: searchWord,
     locations: canada,
     language: 'en'
   });
+
   stream.on('tweet', async tweet => {
     if(tweet.text.match(regex)){
       console.log(tweet);
     }
-      console.log(tweetCount);
+    console.log(tweetCount);
       tweetCount++;
   });
 }
