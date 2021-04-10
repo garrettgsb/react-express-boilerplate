b5e4efff4a8b922f3a2fa947d68866b69b2197ec
gavinswan <gavtswan@gmail.com>
2021-04-10 23:41:10 +0000

    update tables to match updated erd

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -1,13 +1,14 @@
 DROP TABLE IF EXISTS photographers CASCADE;
-DROP TABLE IF EXISTS endorsements CASCADE;
-DROP TABLE IF EXISTS photographers_endorsements CASCADE;
-DROP TABLE IF EXISTS albums CASCADE;
+-- DROP TABLE IF EXISTS endorsements CASCADE;
+-- DROP TABLE IF EXISTS photographers_endorsements CASCADE;
 DROP TABLE IF EXISTS locations CASCADE;
 DROP TABLE IF EXISTS meetups CASCADE;
-DROP TABLE IF EXISTS meetup_photographers CASCADE;
 DROP TABLE IF EXISTS auroras CASCADE;
-DROP TABLE IF EXISTS equipment CASCADE;
-DROP TABLE IF EXISTS location_ratings CASCADE;
+DROP TABLE IF EXISTS meetup_photographers CASCADE;
+DROP TABLE IF EXISTS auroras_locations CASCADE;
+DROP TABLE IF EXISTS messages CASCADE;
+-- DROP TABLE IF EXISTS equipment CASCADE;
+-- DROP TABLE IF EXISTS location_ratings CASCADE;
 
 
 CREATE TABLE photographers (
@@ -15,21 +16,24 @@ CREATE TABLE photographers (
   name VARCHAR(255) NOT NULL, 
   email VARCHAR(255) NOT NULL,
   city VARCHAR(255),
-  phone_number VARCHAR(255) NOT NULL
+  phone_number VARCHAR(255) NOT NULL,
+  photo1_url VARCHAR(255) NOT NULL,
+  photo2_url VARCHAR(255) NOT NULL,
+  photo3_url VARCHAR(255) NOT NULL
 );
 
--- photographer_id here is the *one endorsing another photographer* --
-CREATE TABLE endorsements (
+
+CREATE TABLE locations (
   id SERIAL PRIMARY KEY NOT NULL,
-  description VARCHAR(255),
-  photographer_id INTEGER REFERENCES photographers(id)
+  location_name VARCHAR(255) NOT NULL,
+  coordinates VARCHAR(255) NOT NULL
 );
 
--- photographer_id here is the *one being endorsed* --
-CREATE TABLE photographers_endorsements (
+CREATE TABLE meetups (
   id SERIAL PRIMARY KEY NOT NULL,
-  photographer_id INTEGER REFERENCES photographers(id),
-  endorsement_id INTEGER REFERENCES endorsements(id)
+  location_id INTEGER REFERENCES locations(id) ON DELETE CASCADE
+  date DATE NOT NULL,
+  time INTEGER NOT NULL
 );
 
 CREATE TABLE auroras (
@@ -39,22 +43,9 @@ CREATE TABLE auroras (
   strength INTEGER NOT NULL
 );
 
-CREATE TABLE albums (
+CREATE TABLE auroras_locations (
   id SERIAL PRIMARY KEY NOT NULL,
-  photographer_id INTEGER REFERENCES photographers(id) ON DELETE CASCADE,
-  photos VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE locations (
-  id SERIAL PRIMARY KEY NOT NULL,
-  location_name VARCHAR(255) NOT NULL,
-  coordinates VARCHAR(255) NOT NULL,
   aurora_id INTEGER REFERENCES auroras(id) ON DELETE CASCADE,
-  average_rating INTEGER
-);
-
-CREATE TABLE meetups (
-  id SERIAL PRIMARY KEY NOT NULL,
   location_id INTEGER REFERENCES locations(id) ON DELETE CASCADE
 );
 
@@ -64,16 +55,39 @@ CREATE TABLE meetup_photographers (
   photographer_id INTEGER REFERENCES photographers(id) ON DELETE CASCADE
 );
 
-CREATE TABLE equipment (
+CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
-  photographer_id INTEGER REFERENCES photographers(id) ON DELETE CASCADE,
-  camera_body VARCHAR(255),
-  lens1 VARCHAR(255),
-  lens2 VARCHAR(255)
+  photographers_id INTEGER REFERENCES photographer_id ON DELETE CASCADE,
+  message VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP NOT NULL
 );
 
-CREATE TABLE location_ratings (
-  id SERIAL PRIMARY KEY NOT NULL,
-  location_id INTEGER REFERENCES locations(id) ON DELETE CASCADE,
-  number_stars INTEGER NOT NULL
-);
+-- STRETCH TABLES
+
+-- CREATE TABLE equipment (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   photographer_id INTEGER REFERENCES photographers(id) ON DELETE CASCADE,
+--   camera_body VARCHAR(255),
+--   lens1 VARCHAR(255),
+--   lens2 VARCHAR(255)
+-- );
+
+-- CREATE TABLE location_ratings (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   location_id INTEGER REFERENCES locations(id) ON DELETE CASCADE,
+--   number_stars INTEGER NOT NULL
+-- );
+
+-- photographer_id here is the *one endorsing another photographer* --
+-- CREATE TABLE endorsements (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   description VARCHAR(255),
+--   photographer_id INTEGER REFERENCES photographers(id)
+-- );
+
+-- photographer_id here is the *one being endorsed* --
+-- CREATE TABLE photographers_endorsements (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   photographer_id INTEGER REFERENCES photographers(id),
+--   endorsement_id INTEGER REFERENCES endorsements(id)
+-- );
