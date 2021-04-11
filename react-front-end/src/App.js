8fe7a8835a3e05478f1bbb983aa622cec82529ed
Vanessa Y <vyeung.wt@gmail.com>
2021-04-11 01:44:49 +0000

    add border to UK

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
 import "./App.css";
 import { Icon } from "leaflet";
 import useSwr from "swr";
@@ -18,6 +18,41 @@ function App() {
   const url = "https://data.sfgov.org/resource/ramy-di5m.json";
   const { data, error } = useSwr(url, { fetcher });
   const buildings = data && !error ? data.slice(0, 100) : [];
+  const borderJSON = "https://data.sfgov.org/resource/6ia5-2f8k.json"
+  const testJSON = {
+    "type": "FeatureCollection",
+    "crs": {
+      "type": "name",
+      "properties": {
+        "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+      }
+    },
+    "features": [{
+      "type": "Feature",
+      "properties": {
+  
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [-5.61, 50.11],
+            [-2.73, 51.42],
+            [-4.90, 51.82],
+            [-4.00, 53.37],
+            [-2.51, 53.39],
+            [-3.22, 54.87],
+            [-1.89, 55.63],
+            [0.56, 52.85],
+            [1.98, 52.59],
+            [0.81, 51.44],
+            [1.91, 51.34],
+            [-5.61, 50.11]
+          ]
+        ]
+      }
+    }]
+  }
 
   return (
     <MapContainer center={[37.70820204901914, -122.45808060394913]} zoom={12}>
@@ -25,7 +60,7 @@ function App() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      {buildings.map((building) => (
+      {/* {buildings.map((building) => (
         <Marker
           key={building.eas_fullid}
           position={[building.latitude, building.longitude]}
@@ -37,7 +72,12 @@ function App() {
             </div>
           </Popup>
         </Marker>
-      ))}
+      ))} */}
+
+      <GeoJSON
+        data={testJSON}
+
+      />
     </MapContainer>
   );
 }
