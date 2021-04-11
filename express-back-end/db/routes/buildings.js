9c6ea3b89d16d4e5ad5286259492e14203ef714b
Brody Bayley <brodybayley@gmail.com>
2021-04-11 18:48:55 +0000

    Add query for all buildings in an area

--- express-back-end/db/routes/buildings.js
+++ express-back-end/db/routes/buildings.js
@@ -1,1 +1,20 @@
 const router = require("express").Router();
+
+//get all buildings in an area
+module.exports = (db) => {
+  router.get("/buildings", (req, res) => {
+    const areaID = req.params.area_id;
+    db.query(
+      `
+      SELECT *
+      FROM points
+      WHERE area_id = $1
+      `,
+      [areaID]
+    )
+      .then((buildings) => res.json(buildings))
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+};
