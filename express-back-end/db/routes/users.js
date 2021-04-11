ba9baf009722733616d0f9b9431ac6bb8274de41
Brody Bayley <brodybayley@gmail.com>
2021-04-11 21:35:39 +0000

    Add router for deleting a favourite

--- express-back-end/db/routes/users.js
+++ express-back-end/db/routes/users.js
@@ -10,12 +10,28 @@ module.exports = (db) => {
     const buildingID = req.body.id;
     db.query(
       `
-      INSERT into favourites (user_id, map_id) VALUES ($1, $2)
+      INSERT into favourites (user_id, building_id) VALUES ($1, $2)
       RETURNING *
       `,
       [userID, buildingID]
     )
       .then((favourite) => res.json(favourite))
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  //Delete a favourite
+  router.put("/users/:id/favourites/:id", (req, res) => {
+    const favouriteID = req.params.id;
+    db.query(
+      `
+      DELETE FROM favourites
+      WHERE id = $1
+      `,
+      [favouriteID]
+    )
+      .then(() => res.send("Deleted from favourites"))
       .catch((err) => {
         res.status(500).json({ error: err.message });
       });
