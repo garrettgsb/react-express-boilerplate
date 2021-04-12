0ec6bbf4957bcb4415142542e9023911656468e6
hansori01 <19sorihan88@gmail.com>
2021-04-11 19:42:16 -0700

    adding styled chips to trendinghash view

--- react-front-end/src/components/Header/TrendingHash.js
+++ react-front-end/src/components/Header/TrendingHash.js
@@ -4,10 +4,8 @@ import Chip from '@material-ui/core/Chip';
 
 
 
-const trendingHashtags = ['#joshua', '#isamu', '#sori', '#freebritney', '#awesomeweatherthisweek', 'smd']
-const trendingList = trendingHashtags.map((hashtag, i) => {
-  return (<Chip variant='outlined' size='medium' label={hashtag} key={i} color='' style={{backgroundColor:'transparent'}}/>)
-})
+const trendingHashtags = ['#joshua', '#isamu', '#sori', '#freebritney', '#awesomeweatherthisweek', 'devil.inc', 'trendiistrendy', 'testingchips', 'blahblahblah', 'hellotoptenhashtags']
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,15 +13,41 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     flexWrap: 'wrap',
     '& > *': {
-      margin: theme.spacing(0.5),
+      margin: theme.spacing(.7),
+      marginTop: theme.spacing(2),
     },
   },
+  chip: {
+    backgroundColor: 'transparent',
+    color: 'white',
+    borderColor: 'white',
+    fontSize: '1.1rem',
+    fontWeight: 600,
+    textTransform: 'uppercase',
+    opacity: .65,
+    "&:hover": {
+      opacity: 1,
+      borderColor: 'transparent',
+      background: 'linear-gradient(145deg, #49759D 20%, #35B696 80%)'
+    }
+  }
+
 }));
 
 
 export default function TrendingHash() {
   const classes = useStyles();
-
+  const trendingList = trendingHashtags.map((hashtag, i) => {
+    return (
+      <Chip
+        variant='outlined'
+        size='large'
+        label={hashtag}
+        key={i}
+        className={classes.chip}
+      />
+    )
+  })
 
   return (
     <div className={classes.root}>
