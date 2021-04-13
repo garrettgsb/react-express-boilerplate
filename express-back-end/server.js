266b86e246df089fc444f75da333913c4ddeae52
Sam Ito <isamsito@gmail.com>
2021-04-13 14:35:44 -0600

    Refactored and working without cors error

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,5 +1,5 @@
-const Express = require('express');
-const App = Express();
+const express = require('express');
+const app = express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
 const cors = require('cors')
@@ -8,19 +8,14 @@ const http = require("http");
 const socketIo = require("socket.io");
 // const index = require("./routes/index");
 
-App.use(cors())
-// App.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-//   });
+// Express Configuration
+app.use(BodyParser.urlencoded({ extended: false }));
+app.use(BodyParser.json());
+app.use(express.static('public'));
+app.use(cors())
 
-  // const app = Express();
-// app.use(index);
 
-const server = http.createServer(App);
+const server = http.createServer(app);
 const io = socketIo(server,
   {
     cors: {
@@ -30,24 +25,15 @@ const io = socketIo(server,
   }
 );
 
-
-
 io.on('connection', (socket) => {
-
   console.log('client connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 })
 
-
-// Express Configuration
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static('public'));
-
 // Sample GET route
-App.get('/api/data', (req, res) => res.json({
+app.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
 }));
 
@@ -55,7 +41,3 @@ server.listen(PORT, () => {
   console.log("Listen on port: ", PORT);
 })
 
-// App.listen(PORT, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
-// });
