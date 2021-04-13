ed9ceb7960fbe7e7edfa107985e3a690aa826652
Neil Oribello <neil.oribello@gmail.com>
2021-04-13 18:06:09 +0000

    fixed server.js for merge

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -6,8 +6,7 @@ const PORT = 8080;
 const ENV = process.env.ENV || "development";
 const CORS =  require('cors');
 
-const db = require("./db");
-const buildings = require("./routes/buildings");
+// const db = require("./db");
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
@@ -20,10 +19,12 @@ const dbParams = require('./lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
 
-App.use("/api", buildings(db));
 
 const reviewsRoutes = require("./routes/reviews");
+const buildings = require("./routes/buildings");
+
 App.use("/", reviewsRoutes(db))
+App.use("/api", buildings(db));
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
