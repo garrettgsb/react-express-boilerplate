5b3bd01f351e1ebe3aa315b94c2f3b8c0ad3a7f3
Sam Ito <isamsito@gmail.com>
2021-04-13 18:05:44 -0600

    Tweets now display properly and have link to profile

--- react-front-end/src/components/RightTweets/RightTweets.js
+++ react-front-end/src/components/RightTweets/RightTweets.js
@@ -1,22 +1,21 @@
 import React from 'react';
 
 import Tweets from './Tweets'
-import tempProps from './tempTweets'
+// import tempProps from './tempTweets'
 import './RightTweets.scss';
 
-const loadTweets = tempProps.map((tweet, i) =>
-  <Tweets
-    key={i}
-    name={tweet.name}
-    handle={tweet.handle}
-    text={tweet.text}
-    img={tweet.img}
-    date={tweet.date}
-  />
-);
-
-export default function RightTweets() {
-
+export default function RightTweets(props) {
+  // const loadTweets = tempProps.map((tweet, i) =>
+  const loadTweets = props.tweets.map((tweet, i) =>
+    <Tweets
+      key={i}
+      name={tweet.user.name}
+      handle={tweet.user.screen_name}
+      text={tweet.text}
+      img={tweet.user.profile_image_url}
+      date={tweet.created_at}
+    />
+  );
   return (
     <section className="righttweets">
       {loadTweets}
