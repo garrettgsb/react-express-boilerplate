c1f0e83f08d114af2858774a30223164fbce3b04
gavinswan <gavtswan@gmail.com>
2021-04-14 00:24:48 +0000

    add styling to navbar

--- react-front-end/src/components/Login/LoginUser.jsx
+++ react-front-end/src/components/Login/LoginUser.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import './LoginUser.scss';
 
 const RegisterUser = () => {
   return (
-    <h1>This is where we login :)</h1>
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
   )
 }
 export default RegisterUser; 
