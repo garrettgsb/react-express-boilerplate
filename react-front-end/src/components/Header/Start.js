5ff340ee8934ee6fa6a82aa42a7454f5bf648360
Sam Ito <isamsito@gmail.com>
2021-04-14 15:41:12 -0600

    add onClick to USA and canada images

--- react-front-end/src/components/Header/Start.js
+++ react-front-end/src/components/Header/Start.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HoverImage from "react-hover-image";
 
 
-export default function ChooseCountry() {
+export default function ChooseCountry(props) {
 
   return (
     <>
@@ -10,8 +10,8 @@ export default function ChooseCountry() {
         Choose a <span className='redText'>&nbsp;location&nbsp;</span> to see <span className='redText'>&nbsp;trends&nbsp;</span>
       </div>
       <div className='chooseCountry'>
-        <HoverImage src="./images/canada.png" hoverSrc="./images/canadahover.png" className="canada" />
-        <HoverImage src="./images/usa.png" hoverSrc="./images/usahover.png" className="usa" />
+        <HoverImage src="./images/canada.png" hoverSrc="./images/canadahover.png" className="canada" onClick={() => {props.onClick('canada')}} />
+        <HoverImage src="./images/usa.png" hoverSrc="./images/usahover.png" className="usa" onClick={() => {props.onClick('USA')}} />
       </div>
     </>
   );
