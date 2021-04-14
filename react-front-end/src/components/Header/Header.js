7c1bd0250d99c4d2066d54b4bfd2549b99bbfb1e
Sam Ito <isamsito@gmail.com>
2021-04-14 15:39:33 -0600

    add onClick to change country state

--- react-front-end/src/components/Header/Header.js
+++ react-front-end/src/components/Header/Header.js
@@ -8,14 +8,19 @@ import TrendingHash from './TrendingHash';
 
 export default function Header() {
   const [country, setCountry] = useState('');
+
+  const selectCountry = function(country) {
+    setCountry(country)
+  }
+
   return (
     <nav>
       <div className='headerParent'>
         <HoverImage src="./images/logo.png" hoverSrc="./images/logohover.png" className="logo" />
         <img src='./images/user.png' alt='' className="userAvatar"></img>
       </div>
-      {/* <ChooseCountry /> */}
-      <TrendingHash />
+      <ChooseCountry onClick={selectCountry} />
+      <TrendingHash country={country} />
     </nav>
 
   );
