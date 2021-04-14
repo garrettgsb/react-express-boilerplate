c1e137b0e4b6519962dd6e824403486cf65022aa
Vanessa Y <vyeung.wt@gmail.com>
2021-04-14 05:09:56 +0000

    amend amenities table and seed dummy amenities data from csv

--- express-back-end/db/schema/create.sql
+++ express-back-end/db/schema/create.sql
@@ -52,11 +52,16 @@ CREATE TABLE favourites (
 CREATE TABLE amenities (
   id SERIAL PRIMARY KEY NOT NULL,
   area_id INTEGER REFERENCES areas(id) ON DELETE CASCADE,
-  name VARCHAR(255) NOT NULL,
-  address VARCHAR(255) NOT NULL,
-  image_url VARCHAR(255) NOT NULL,
-  latitude FLOAT NOT NULL,
-  longitude FLOAT NOT NULL
+  name VARCHAR(255),
+  type VARCHAR(255),
+  image_url VARCHAR(255),
+  longitude FLOAT NOT NULL,
+  latitude FLOAT NOT NULL
 );
 
+\COPY amenities
+FROM 'seeds/amenities-data.csv'
+DELIMITER ','
+CSV HEADER;
 
+\COPY amenities(area_id, name, type, image_url, latitude, longitude) FROM 'seeds/amenities-data.csv' DELIMITER ',' CSV HEADER;
