821d31549d37dd6985fbae0b484e5ffaf0f9de05
Sknoop2 <sknoop@hotmail.ca>
2021-04-15 15:47:02 -0600

    add code for text message field. Currently not working, looking for bug

--- react-front-end/src/components/Meetups/SafetySend.jsx
+++ react-front-end/src/components/Meetups/SafetySend.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 
 const SafetySend = () => {
+
+  state = {
+    text: {
+      recipient: '',
+      textmessage: ''
+    }
+  }
+
+  sendText = () => {
+    const { text } = this.state
+
+    // pass variables in query string
+    fetch(`http://localhost:8080/text?recipient=${text.recipient}&textmessage=${text.textmessage}`)
+    .catch(err => console.log(err))
+  }
+
+  const { text } = this.state;
+  
+  const spacer = {
+    margin: 8
+  }
+  const textArea = {
+    borderRadius: 4
+  }
+
   return (
-    <div>
-      
+    <div className='textArea' style={{marginTop:10 }}>
+      <h2> Send Text Message </h2>
+      <label> Your Phone Number </label>
+      <br />
+      <input value = {text.recipient} 
+        onChange={e => this.setState({ text: { ...text, recipient: e.target.value} })} />
+      <div style={spacer} />
+      <label> Message </label>
+      <br />
+      <textarea rows={3} value={text.textmessage} style={textArea} 
+        onChange={e => this.setState({ text: { ...text, textmessage: e.target.value } })}/>
+      <div style={spacer} />
+      <button onClick={this.sendText}> Send Text </button>
     </div>
   )
 }
