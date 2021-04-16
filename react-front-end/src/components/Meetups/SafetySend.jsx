7a8df315543ae99fc4b630d2f9dbc7e6c789a8b7
Sknoop2 <sknoop@hotmail.ca>
2021-04-16 14:14:52 -0600

    Create predetermined safety message for text

--- react-front-end/src/components/Meetups/SafetySend.jsx
+++ react-front-end/src/components/Meetups/SafetySend.jsx
@@ -6,8 +6,8 @@ function SafetySend () {
 
   const [text, setText] = useState({
       recipient: '',
-      textmessage: ''
-    })
+      textmessage: 'From: *insert your name*: Hello, I planning on meeting new people on *insert date* at *insert time* to take northern lights photographs at: *insert location*. I plan on returning home by *insert time*. I am letting you know as a safety precaution.'
+  })
   
   
   function sendText () {
@@ -30,6 +30,7 @@ function SafetySend () {
       // set state as user types in phone number
       onChange={e => setText({ ...text, recipient: e.target.value})} />
       <div className='spacer' />
+      <p>Please edit the message below prior to sending:</p>
       <label> Message </label>
       <br />
       <textarea rows={3} value={text.textmessage} className='text-area' 
@@ -42,21 +43,3 @@ function SafetySend () {
 }
 
 export default SafetySend;
-
-// working version
-// <div className='safety-send' >
-// <h2> Send Text Message </h2>
-// <label> Your Phone Number </label>
-// <br />
-// <input value = {text.recipient} 
-// // set state as user types in phone number
-// onChange={e => setText({ ...text, recipient: e.target.value})} />
-// <div className='spacer' />
-// <label> Message </label>
-// <br />
-// <textarea rows={3} value={text.textmessage} className='text-area' 
-// // set state as user types in message
-//   onChange={e => setText({ ...text, textmessage: e.target.value })}/>
-// <div className='spacer' />
-// <button onClick={sendText}> Send Text </button>
-// </div>
