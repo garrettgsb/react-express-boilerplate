a85dff3f631e5a683b848d9a29c1cfeb51b2239a
Vanessa Y <vyeung.wt@gmail.com>
2021-04-17 21:15:58 +0000

    updated queries for building/:id route and reviews routes to fetch data

--- express-back-end/db/01_queries/3_friend_percentage.sql
+++ express-back-end/db/01_queries/3_friend_percentage.sql
@@ -1,2 +1,5 @@
-SELECT  	
-  (SELECT cast(count(id) as decimal) FROM reviews WHERE landlord_rating = 't') / (SELECT cast(COUNT(id) as decimal) FROM reviews) AS landlord_approval_percentage;
+SELECT buildings.id AS building_id, buildings.area_id AS area_id, buildings.name AS name, buildings.address AS building_address, buildings.neighbourhood AS neighbourhood, buildings.image_url AS image_url, buildings.longitude AS longitude, buildings.latitude AS latitude,
+(SELECT     
+(SELECT cast(count(id) as decimal) FROM reviews WHERE recommend_to_friend = 't' and building_id=101) / (SELECT cast(COUNT(id) as decimal) FROM reviews where building_id = 101)) AS recommend_to_friend_ratio
+FROM buildings
+WHERE buildings.id = 101;
--- express-back-end/db/01_queries/4_reviewer_name.sql
+++ express-back-end/db/01_queries/4_reviewer_name.sql
@@ -1,5 +1,9 @@
 -- Gets username associated with a review
-SELECT * FROM reviews AND 
-(SELECT users.username AS username
+-- SELECT * FROM reviews AND 
+-- SELECT reviews.id AS review_id, users.username AS username
+-- FROM users
+-- JOIN reviews ON users.id = reviews.user_id;
+
+SELECT reviews.id AS review_id, reviews.building_id AS building_id, reviews.title AS title, reviews.building_rating AS building_rating, reviews.comment AS comment, reviews.landlord_rating AS landlord_rating, reviews.recommend_to_friend AS recommend_to_friend, users.username AS username
 FROM users
-JOIN reviews ON users.id = reviews.user_id);
+JOIN reviews ON users.id = reviews.user_id; 
