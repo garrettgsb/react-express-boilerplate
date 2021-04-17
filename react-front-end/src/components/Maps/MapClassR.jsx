07ada0a0a78afa43f5bd04e789031337539454fe
Sknoop2 <sknoop@hotmail.ca>
2021-04-17 16:44:40 -0600

    variation on map page. Unsuccessful attempt to add pin to map with a click

---
+++ react-front-end/src/components/Maps/MapClassR.jsx
@@ -0,0 +1,54 @@
+// import React from "react";
+// import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+// import { Icon } from "leaflet";
+// import * as parkData from "./data/skateboard-parks.json";
+// import "./app.css";
+
+// export const icon = new Icon({
+//   iconUrl: "/skateboarding.svg",
+//   iconSize: [25, 25]
+// });
+
+// export default function Map() {
+//   const [activePark, setActivePark] = React.useState(null);
+
+//   return (
+//     <Map center={[45.4, -75.7]} zoom={12}>
+//       <TileLayer
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//       />
+
+//       {parkData.features.map(park => (
+//         <Marker
+//           key={park.properties.PARK_ID}
+//           position={[
+//             park.geometry.coordinates[1],
+//             park.geometry.coordinates[0]
+//           ]}
+//           onClick={() => {
+//             setActivePark(park);
+//           }}
+//           icon={icon}
+//         />
+//       ))}
+
+//       {activePark && (
+//         <Popup
+//           position={[
+//             activePark.geometry.coordinates[1],
+//             activePark.geometry.coordinates[0]
+//           ]}
+//           onClose={() => {
+//             setActivePark(null);
+//           }}
+//         >
+//           <div>
+//             <h2>{activePark.properties.NAME}</h2>
+//             <p>{activePark.properties.DESCRIPTIO}</p>
+//           </div>
+//         </Popup>
+//       )}
+//     </Map>
+//   );
+// }
