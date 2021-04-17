e32dd4a3bb654e24e1ec20313a66534e86a6135f
Brody Bayley <brodybayley@gmail.com>
2021-04-17 05:22:55 +0000

    Add more reviews seeds

--- express-back-end/db/seeds/reviews.sql
+++ express-back-end/db/seeds/reviews.sql
@@ -2,8 +2,20 @@ INSERT INTO reviews (building_id, user_id, area_id, title, building_rating, area
 VALUES
 (1001, 1, 1,'This place is amazing!', 5, 4, 'Great building with great ammenties around the building. Landlord is sensational!', true, true),
 
-(1001, 2, 1, 'Buyers Beware!', 2, 2, 'The apartment might be nice but the surronding area is just so distracting and noisy! The landlord does not fix anything around here.', false, false),
+(1002, 2, 1, 'Buyers Beware!', 2, 2, 'The apartment might be nice but the surronding area is just so distracting and noisy! The landlord does not fix anything around here.', false, false),
 
-(1001, 2, 2, 'Great place to live!', 5, 5, 'Ammentities are amazing, great view, great landlord', false, false),
+(1003, 2, 2, 'Great place to live!', 5, 5, 'Ammentities are amazing, great view, great landlord', false, false),
 
-(1001, 3, 2, 'Could use some work!', 3, 3, 'Landlords are trust worthy however area is lacking ammentities', false, false);
+(1004, 3, 2, 'Could use some work!', 3, 3, 'Landlords are trust worthy however area is lacking ammentities', false, false),
+
+(1005, 1, 1,'This place is an Oasis!', 5, 4, 'Beautiful building and friendly neighbours. Building Manager is so kind!', true, true),
+
+(1006, 2, 2,'Happy Here!', 5, 4, 'Great building with all the amenities. Area is good, but does have a few sketchy parts!', true, true),
+
+(1007, 3, 1,'This place is amazing!', 5, 5, 'Great building with great amenties around the building. Landlord is sensational!', true, true),
+
+(1008, 1, 1,'This place is dingy!', 2, 4, 'Apartment is run down, but rent is cheap. The area has everything you need which is why I stay. Landlord disappeared.', false, false),
+
+(1009, 2, 3,'What a gem!', 5, 5, 'So lucky to have found this spot. Rent is still affordable and building is superb! Landlord rocks!', true, true),
+
+(1009, 3, 4,'Mi Casa Forever!', 5, 5, 'I have lived here for 10 years and will never leave. Such a clean building and the views are incredible. Area is spectacular! Landlord is to die for!', true, true);
