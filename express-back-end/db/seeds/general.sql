0f44c314b68d3dfbf0c90003dd2c84e1467bc3f6
Sknoop2 <sknoop@hotmail.ca>
2021-04-17 20:40:26 -0600

    change format of location coordinates in DB

--- express-back-end/db/seeds/general.sql
+++ express-back-end/db/seeds/general.sql
@@ -6,12 +6,24 @@ VALUES
 ('d2066a6a-0adb-4369-9f5b-87b1c6e4bcf3', 'password', 'Dave Diggs', 'davey09@hotmail.com', 'Penticton', 7781111111, 'https://images.unsplash.com/photo-1430132594682-16e1185b17c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fG5vcnRoZXJuJTIwbGlnaHRzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60', 'https://images.unsplash.com/photo-1472419551702-dc4862cad8f9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fG5vcnRoZXJuJTIwbGlnaHRzfGVufDB8fDB8&auto=format&fit=crop&w=500&q=60', 'https://pbs.twimg.com/media/B8Q4YScCEAEL1cL.jpg'),
 ('923a7685-27ba-4108-a888-32e9c9d26f59', 'password', 'Ellie Eggerton', 'ellie@gmail.com', 'Vancouver', 6041111111, 'https://twistedsifter.com/wp-content/uploads/2017/05/33-best-auroras-nasa-has-ever-featured-3.jpg', 'https://adventures.com/media/6169/how-to-take-pictures-of-the-northern-lights-camera-tripod-4.jpg', 'https://lp-cms-production.imgix.net/news/2017/12/northern-lights.jpg');
 
-INSERT INTO locations (location_name, coordinates)
+INSERT INTO locations (location_name, latitude, longitude)
 VALUES
-('Banff', '51.1784° N, 115.5708° W'),
-('Moberly Lake', '55.8233° N, 121.7889° W'),
-('Northern Lights Centre','60.2343° N, 128.3215° W'),
-('High Level','58.5071° N, 117.1403° W');
+('Banff', '51.1784', '-115.5708'),
+('Moberly Lake', '55.8233', '-121.7889'),
+('Northern Lights Centre','60.2343', '-128.3215'),
+('High Level','58.5071', '-117.1403'),
+('Lake Minnewanka', '51.245171', '-115.496714'),
+('Vermillion Lakes', '51.245171', '-115.496714'),
+('McDougall Memorial United Church', '51.187684', '-114.826393'),
+('Barrier Lake', '51.033203', '-115.034114'),
+('Old Barn on Hwy 22', '51.250908', '-114.476620'),
+('Blizzard Lake', '51.250908', '-114.476620'),
+('Point 41', '51.250908', '-114.476620'),
+('Canola Valley', '52.053756', '-113.978676'),
+('Witherby Point', '49.484918', '-123.474140'),
+('Quercus Point', '49.646188', '-123.209316'),
+('Muncho Lake Provincial Park', '58.944773', '-125.769553'),
+('Topley Landing', '54.776879', '-126.098222');
 
 INSERT INTO auroras (forecast, location, strength) 
 VALUES
