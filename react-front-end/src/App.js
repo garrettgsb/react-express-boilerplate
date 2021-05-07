80685f94152f7e993282fd036b8b81015e67c4eb
ericguthrie <ericguthrie12@gmail.com>
2021-05-07 23:28:38 +0000

    added material UI

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import DenseAppBar from "./components/testnav";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      message: 'Click the button to load data!'
-    }
+      message: "Click the button to load data!",
+    };
   }
 
   fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+    axios
+      .get("/api/data") // You can simply make your requests to "/api/whatever you want"
+      .then((response) => {
+        // handle success
+        console.log(response.data); // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
+        console.log(response.data.message); // Just the message
+        this.setState({
+          message: response.data.message,
+        });
       });
-    }) 
-  }
+  };
 
   render() {
     return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
+      <>
+        <DenseAppBar />
+        <div className="App">
+          <h1>{this.state.message}</h1>
+          <button onClick={this.fetchData}>Fetch Data</button>
+        </div>
+      </>
     );
   }
 }
