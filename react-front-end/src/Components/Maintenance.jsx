6d3cdd1364873e9ced26386b6336b57638bcd3f9
Cole Kubossek <cole.kubossek@gmail.com>
2021-05-09 00:18:42 +0000

    adds tasks card for maintenance page. Table still needs syling work

---
+++ react-front-end/src/Components/Maintenance.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    width: 400,
+    marginLeft: '10%',
+    marginTop: '7%',
+    display: 'flex',
+    flexDirection: 'row',
+    // justifyContent: 'space-between'
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  heads: {
+    display: 'flex',
+    // flexDirection: 'row',
+    justifyContent: 'space-around',
+  }
+});
+
+export default function Maintenance() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <table >
+          <thead >
+            <tr >
+              <th>Task</th>
+              <th>Date</th>
+              <th>Complete</th>
+            </tr>
+          </thead>
+          <tbody >
+            <tr>
+              <td>
+                Water
+              </td>
+              <td>
+              1 day
+              </td>
+              <td>
+              <CardActions>
+                <Button size="small" variant="contained" color="primary">Complete</Button>
+              </CardActions>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Water
+              </td>
+              <td>
+              4 day
+              </td>
+              <td>
+              <CardActions>
+                <Button size="small" variant="contained" color="primary">Complete</Button>
+              </CardActions>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Fertilize
+              </td>
+              <td>
+              4 day
+              </td>
+              <td>
+              <CardActions>
+                <Button size="small" variant="contained" color="primary">Complete</Button>
+              </CardActions>
+              </td>
+            </tr>
+            
+          </tbody>
+          
+        </table>
+      </CardContent>
+    </Card>
+  );
+}
