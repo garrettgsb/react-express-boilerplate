31c59f268c65d68c07c24a7288bc5f4e58b5319c
Joel <joel.d.hart1@gmail.com>
2021-05-09 02:55:44 +0000

    setup vegetable basket component with list

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 // import './App.css';
 import Sidebar from './Components/Sidebar'
+import VegetableBasket from './Components/VegetableBasket/VegetableBasket';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Sidebar />
-      </div>
+    <main className="layout">
+
+      <nav className="sidebar">
+        <Sidebar/>
+      </nav>
+
+      <nav className="vegetable__basket">
+        <VegetableBasket/>
+      </nav>
+     </main>
     );
   }
 }
