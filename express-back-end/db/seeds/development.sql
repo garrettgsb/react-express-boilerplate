1b79451acc310daecca9f5f78236031fba2b53fc
Josiah <josiah.pilon@yahoo.ca>
2021-05-09 00:29:42 +0000

    Fake data for entries and categories table

--- express-back-end/db/seeds/development.sql
+++ express-back-end/db/seeds/development.sql
@@ -1,5 +1,6 @@
 INSERT INTO fonts (name, script) 
 VALUES ('default', 'script link placeholder');
 
-INSERT INTO users (email, name, password) 
+INSERT INTO users (email, userName, password) 
 VALUES ('egg@egg.egg', 'Egg Eggerson', 'egg');
+
