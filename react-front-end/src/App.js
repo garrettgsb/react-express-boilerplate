05d7b5a1fbb47cf89fbabd8ef794a3cf8ed7e3c9
Wagsnerlund <chelseawagner17@gmail.com>
2021-05-09 18:03:48 -0700

    Added veggie cards and veggie basket component

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 // import './App.css';
-import Sidebar from './Components/Sidebar'
+import Sidebar from './Components/Sidebar';
+import Content from './Components/vegetableBasket';
+// import VegetableBasket from './Components/vegetableBasket';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Sidebar />
-      </div>
+      <main className="layout">
+        <section className="App">
+          <Sidebar />
+        </section>
+        <section >
+          <Content />
+        </section>
+      </main>
     );
   }
 }
