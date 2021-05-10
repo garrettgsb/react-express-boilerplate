981a067fc84d46932a3b5ed86a1f359bc5ca371d
Justin <justinanderson101@gmail.com>
2021-05-10 21:16:09 +0000

    added seeds for Anxiety category

--- express-back-end/seeds/1_seeds.sql
+++ express-back-end/seeds/1_seeds.sql
@@ -1,14 +1,33 @@
 INSERT INTO users (name, password, email)
- VALUES ('Michael Scott', 'password', 'michaelscarn@dundermifflin.com');
+ VALUES ('Michael Scott', 'password', 'michaelscarn@dundermifflin.com'),
 
 
 
 INSERT INTO posts (body, time_stamp, isDeleted, user_id)
- VALUES ('test post', 050721, false, 1);
+ VALUES ('test post', 050721, false, 1),
 
 
 
-INSERT INTO resources (title, category, link, content,    favourited)
- VALUES ('Coping Technique - Anxiety', 'Anxiety', 'https://www.urmc.rochester.edu/behavioral-health-partners/bhp-blog/april-2018/5-4-3-2-1-coping-technique-for-anxiety.aspx', 'Coping techniue for an anxiety attack', false);
+INSERT INTO resources (title, category, link, content, favourited)
+ VALUES ('Stop An Anxiety Attack', 'Anxiety', 'https://v1.nitrocdn.com/rdVdpjsyHfwtqaZfkMplDiZritrnIecd/assets/static/optimized/rev-9ad92c3/wp-content/uploads/2019/09/stop-anxiety-attack.jpg', 'InfoGraphic - How to stop an anxiety attack.', false),
 
-hi
+INSERT INTO resources (title, category, link, content, favourited)
+ VALUES ('What An Anxiety Attack Looks Like', 'Anxiety', 'https://v1.nitrocdn.com/rdVdpjsyHfwtqaZfkMplDiZritrnIecd/assets/static/source/rev-9ad92c3/wp-content/uploads/2019/01/anxiety-infographics-450x800.jpg', 'InfoGraphic - Identifying an anxiety attack', false),
+
+INSERT INTO resources (title, category, link, content, favourited)
+ VALUES ('Calm Your Mind!', 'Anxiety', 'https://www.youtube.com/watch?v=-YUSGm5GJYA&ab_channel=JayShetty', 'Video - Quick watch for when you are spiralling', false),
+
+INSERT INTO resources (title, category, link, content, favourited)
+ VALUES ('Overcome Anxiety In 7 Minutes', 'Anxiety', 'https://www.ted.com/talks/mel_schwartz_overcome_anxiety_in_7_minutes?language=en', 'TedTalk - Tips for dealing with and overcoming anxiety', false),
+
+INSERT INTO resources (title, category, link, content, favourited)
+ VALUES ('How To Cope With Anxiety', 'Anxiety', 'https://www.youtube.com/watch?v=WWloIAQpMcQ&ab_channel=TEDxTalks', 'TedTalk - Learning to cope with anxiety', false),
+
+INSERT INTO resources (title, category, link, content, favourited)
+ VALUES ('Tips For Managing Anxiety', 'Anxiety', 'https://adaa.org/tips', 'Reading - Tips for dealing with and overcoming anxiety', false),
+
+INSERT INTO resources (title, category, link, content, favourited)
+ VALUES ('Meditation For Anxiety', 'Anxiety', 'https://www.youtube.com/watch?v=4pLUleLdwY4&t=8s&ab_channel=YogaWithAdriene', 'Meditation/Video - Guided meditation for anxiety', false),
+
+INSERT INTO resources (title, category, link, content, favourited)
+ VALUES ('Yoga For Anxiety', 'Anxiety', 'https://www.youtube.com/watch?v=bJJWArRfKa0&ab_channel=YogaWithAdriene', 'Yoga - Guided yoga practice for anxiety', false);
