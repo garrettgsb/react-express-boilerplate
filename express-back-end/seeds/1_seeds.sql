c2586cba1c8130c5bc48559bf4976ff4f93995e5
Justin <justinanderson101@gmail.com>
2021-05-10 21:14:28 +0000

    testing branch

--- express-back-end/seeds/1_seeds.sql
+++ express-back-end/seeds/1_seeds.sql
@@ -11,3 +11,4 @@ INSERT INTO posts (body, time_stamp, isDeleted, user_id)
 INSERT INTO resources (title, category, link, content,    favourited)
  VALUES ('Coping Technique - Anxiety', 'Anxiety', 'https://www.urmc.rochester.edu/behavioral-health-partners/bhp-blog/april-2018/5-4-3-2-1-coping-technique-for-anxiety.aspx', 'Coping techniue for an anxiety attack', false);
 
+hi
