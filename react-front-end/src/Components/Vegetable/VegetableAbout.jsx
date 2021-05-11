3bd7d66420618108993f924d7739a40d357ab522
Wagsnerlund <chelseawagner17@gmail.com>
2021-05-11 16:28:33 -0700

    Organized folders

--- react-front-end/src/Components/Vegetable/VegetableAbout.jsx
+++ react-front-end/src/Components/Vegetable/VegetableAbout.jsx
@@ -4,15 +4,17 @@ import './VegetableAbout.scss';
 export default function VegetableAbout() {
 
   return (
-    <div className="flipCard">
-      <div className="flipCardInner">
-        <div className="flipCardFront">
-          <img src={'images/vegetables/basil.png'} alt={'image'} style={{ width: '300px' }, { height: '300px' }}>
-          </img>
-        </div>
-        <div className="flipCardBack">
-          <h2>Basil</h2>
-          <p>Grow me!</p>
+    <div className="mainCard">
+      <div className="flipCard">
+        <div className="flipCardInner">
+          <div className="flipCardFront">
+            <img src={'images/vegetables/basil.png'} alt={'image'} style={{ width: '300px' }, { height: '200px' }}>
+            </img>
+          </div>
+          <div className="flipCardBack">
+            <h2>Basil</h2>
+            <p>Grow me!</p>
+          </div>
         </div>
       </div>
     </div>
