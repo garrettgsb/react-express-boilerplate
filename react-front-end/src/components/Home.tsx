f7d573741f88894b9e2741c7dad2b89bab2d1e30
Josiah <josiah.pilon@yahoo.ca>
2021-05-11 23:13:24 +0000

    Renders Form and Mood components

--- react-front-end/src/components/Home.tsx
+++ react-front-end/src/components/Home.tsx
@@ -1,12 +1,20 @@
-interface Props {
-  message: string;
-};
-const Home = ({ message }: Props) => {
+import Form from './Form'
+import Mood from './Mood'
+
+const Home = () => {
+
+  const timeElapsed = Date.now();
+  const currentDay = new Date(timeElapsed);
+
   return (
-    <div>
-      <h2>Home</h2>
-      {message}
-    </div>
+    
+      <div>
+        <h1>Create An Entry</h1>
+        <h2>{currentDay.toDateString()}</h2>
+        <Form/>
+        <Mood/>
+      </div>
+    
   );
 };
 
