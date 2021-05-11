6ba1a3a01561fec2396fdcbc0c12bc454f6f4960
Luke Bergmann <lukebergmann0@gmail.com>
2021-05-11 17:28:14 +0000

    Quick changes before switch to branch

--- react-front-end/src/components/moods.jsx
+++ react-front-end/src/components/moods.jsx
@@ -6,21 +6,21 @@ const Moods = (props) => {
       <section className="Moods">
       <div className="MoodsTop">
         <div className="MoodBox">
-        <input type="image" src={require("../images/newPanda.png")} className="Mood" width="85%" />
+        <input type="image" src={require("../images/newPanda.png")} alt="PandaImage"className="Mood" width="85%" />
       <h3>Depressed</h3>
       </div>
       <div className="MoodBox">
-      <input type="image" src={require("../images/newPanda.png")} className="Mood" width="85%" />
+      <input type="image" src={require("../images/newPanda.png")} alt="PandaImage"className="Mood" width="85%" />
       <h3>Anxious</h3>
       </div>
       </div>
       <div className="MoodsBottom">
       <div className="MoodBox">
-      <input type="image" src={require("../images/newPanda.png")} className="Mood" width="85%" />
+      <input type="image" src={require("../images/newPanda.png")} alt="PandaImage" className="Mood" width="85%" />
       <h3>Angry</h3>
       </div>
       <div className="MoodBox">
-      <input type="image" src={require("../images/newPanda.png")} className="Mood" width="85%" />
+      <input type="image" src={require("../images/newPanda.png")} alt="PandaImage" className="Mood" width="85%" />
       <h3>Scared</h3>
       </div>
       </div>
@@ -30,11 +30,11 @@ const Moods = (props) => {
       <h3>More Resources<hr></hr></h3>
       <a
         href="https://www.betterhelp.com/helpme/?utm_source=AdWords&utm_medium=Search_PPC_c&utm_term=betterhelp_e&utm_content=113425365251&network=g&placement=&target=&matchtype=e&utm_campaign=10969405372&ad_type=text&adposition=&gclid=Cj0KCQjws-OEBhCkARIsAPhOkIZjT-gD9dfHaAXRWFPjDRtLjjcQPLfVxiUrsLcsDJOAQZwFgRjkqLoaAmt1EALw_wcB&not_found=1&gor=helpme"
-        target="_blank"
+        target="_blank" rel="noopener noreferrer"
       >
         BetterHelp
       </a>
-      <a href="https://www.goodnewsnetwork.org/" target="_blank">
+      <a href="https://www.goodnewsnetwork.org/" target="_blank" rel="noopener noreferrer">
         GoodNewsNetwork
       </a>
       </div>
--- react-front-end/src/components/resource.jsx
+++ react-front-end/src/components/resource.jsx
@@ -2,13 +2,42 @@ import React from "react";
 
 const Resource = (props) => {
   return (
+    
     <div className="Resources">
+       <script src="https://kit.fontawesome.com/03a60bf669.js" crossorigin="anonymous"></script>
+       <section className="ResourceHeader">
+      <h1>Depression</h1>
+  
+      </section>
+      <section className="ResourceBox">
+      <h4>Depression - Signs & Symptoms</h4>
+      <h6>Depression</h6>
+   
+      <h5>InfoGraphic - Depression Signs/Symptoms</h5>
+   
+      <a href="https://au.reachout.com/-/media/young-people/images/image-library/depr_infographic_dontsee_fb.jpg?h=850&w=850&la=en&hash=8B89352F1259E85ED3141FA2390BE4CE8B24E9F8" target="_blank" rel="noopener noreferrer">Find Out More</a>
+      <i class="far fa-heart"></i>
+      </section>
       <section className="ResourceBox">
       <h4>Depression - Signs & Symptoms</h4>
       <h6>Depression</h6>
       <h5>InfoGraphic - Depression Signs/Symptoms</h5>
    
-      <a href="https://au.reachout.com/-/media/young-people/images/image-library/depr_infographic_dontsee_fb.jpg?h=850&w=850&la=en&hash=8B89352F1259E85ED3141FA2390BE4CE8B24E9F8" target="_blank">Find Out More</a>
+      <a href="https://au.reachout.com/-/media/young-people/images/image-library/depr_infographic_dontsee_fb.jpg?h=850&w=850&la=en&hash=8B89352F1259E85ED3141FA2390BE4CE8B24E9F8" target="_blank" rel="noopener noreferrer">Find Out More</a>
+      </section>
+      <section className="ResourceBox">
+      <h4>Depression - Signs & Symptoms</h4>
+      <h6>Depression</h6>
+      <h5>InfoGraphic - Depression Signs/Symptoms</h5>
+   
+      <a href="https://au.reachout.com/-/media/young-people/images/image-library/depr_infographic_dontsee_fb.jpg?h=850&w=850&la=en&hash=8B89352F1259E85ED3141FA2390BE4CE8B24E9F8" target="_blank" rel="noopener noreferrer">Find Out More</a>
+      </section>
+      <section className="ResourceBox">
+      <h4>Depression - Signs & Symptoms</h4>
+      <h6>Depression</h6>
+      <h5>InfoGraphic - Depression Signs/Symptoms</h5>
+   
+      <a href="https://au.reachout.com/-/media/young-people/images/image-library/depr_infographic_dontsee_fb.jpg?h=850&w=850&la=en&hash=8B89352F1259E85ED3141FA2390BE4CE8B24E9F8" target="_blank" rel="noopener noreferrer">Find Out More</a>
       </section>
     </div>
   );
