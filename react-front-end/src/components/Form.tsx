fd55f1f4081302a3097733b5067d8c95f811116a
Josiah <josiah.pilon@yahoo.ca>
2021-05-12 02:58:06 +0000

    Adds button component

--- react-front-end/src/components/Form.tsx
+++ react-front-end/src/components/Form.tsx
@@ -1,7 +1,10 @@
 // import { useState } from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -13,33 +16,42 @@ const useStyles = makeStyles((theme) => ({
     color: 'black',
     height: 300,
     padding: '0 30px',
+    margin: theme.spacing(1),
   }
 }));
 
 export default function Form() {
+  // const [ title, setTitle ] = useState("");
+  // const [ entry, setEntry ] = useState("");
+
   const classes = useStyles();
 
   return (
     <form className={classes.root} noValidate autoComplete="off" >
       <Box
-        height={300}
         width="100%"
         display="flex" 
         flexDirection="column"
         bgcolor="background.paper">
+
         <TextField 
           id="outlined-basic" 
           margin="normal"
           label="Title" 
           variant="outlined" 
-          fullWidth/>
+          fullWidth
+          value={""}
+          />
+
         <TextField 
           id="outlined-basic" 
           multiline
-          rows="10"
+          rows="15"
           label="Whats on your mind?" 
           variant="outlined" 
           fullWidth/>
+
+        <Button variant="contained" color="primary" onClick={() => console.log("clicked")}>Submit</Button>
       </Box>
     </form>
     
