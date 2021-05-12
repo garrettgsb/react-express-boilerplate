7178b8607ca85d66be87fe9ea38e2937084dba62
Josiah <josiah.pilon@yahoo.ca>
2021-05-12 03:15:10 +0000

    Repositioned emojis

--- react-front-end/src/components/Home.tsx
+++ react-front-end/src/components/Home.tsx
@@ -1,9 +1,9 @@
-import Form from './Form'
-import Mood from './Mood'
+import Form from './Form';
+import Mood from './Mood';
 
 const Home = () => {
 
-  const timeElapsed = Date.now();
+  const timeElapsed: number = Date.now();
   const currentDay = new Date(timeElapsed);
 
   return (
@@ -11,8 +11,8 @@ const Home = () => {
       <div>
         <h1>Create An Entry</h1>
         <h2>{currentDay.toDateString()}</h2>
-        <Form/>
-        <Mood/>
+          <Mood/>
+          <Form/>
       </div>
     
   );
--- react-front-end/src/components/Mood.jsx
+++ react-front-end/src/components/Mood.jsx
@@ -1,3 +1,5 @@
+import { makeStyles } from '@material-ui/core/styles';
+
 import Box from '@material-ui/core/Box';
 import smiley from '../emojis/smiley.ico';
 import mild from '../emojis/mildly_happy.ico';
@@ -5,12 +7,23 @@ import neutral from '../emojis/neutral.ico';
 import unhappy from '../emojis/unhappy.ico';
 import angry from '../emojis/angry.ico';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    border: 0,
+    borderRadius: 3,
+    height: 48,
+    padding: '0',
+    margin: theme.spacing(1),
+  }
+}));
+
 const Mood = () => {
+  const classes = useStyles();
 
   const stylingContainer = {display:'flex', flexDirection: 'row', 'list-style': 'none'}
 
   return (
-    <Box display="flex">
+    <Box className={classes.root}>
       <ul style={stylingContainer}
         onClick={event => event.target}>
         <li><img src={smiley} alt="smiley"/></li>
