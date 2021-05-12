20fb6497172e1ce09fa5ff29d1714e6e4a389124
Joel <joel.d.hart1@gmail.com>
2021-05-12 19:07:10 +0000

    set up useAppData hook and fixed merge conflicts

--- express-back-end/routes/vegetables.js
+++ express-back-end/routes/vegetables.js
@@ -1,42 +1,35 @@
 const Express = require('express');
-const router  = Express.Router();
+const router = Express.Router();
 const db = require('../db/lib/db');
 
 
-  router.get('/api/users', (req, res) => {
-    getUsers().then(data =>{
-      res.json(data)
-    })
-  }); 
-  
-  router.get('/api/vegetables', (req, res) => {
-    getVeg().then(data =>{
-      res.json(data)
-    })
-  }); 
-
-
-
-  //helper to select all veg
-  const getVeg = () => db.query(`SELECT * FROM vegetables ORDER BY name`)
-  .then(res => {
-    return res.rows
-  }).catch(err => console.log(err));
-
-  //helper to select all users
-  const getUsers = () => db.query(`SELECT * FROM users`)
-  .then(res => {
-    return res.rows
-  }).catch(err => console.log(err));
-
-// get planted veggies per plot.
-const getPlantedVeg = function() {
-  return db.query(`SELECT * FROM vegetables JOIN plots_vegs ON vegetables.id=plots_vegs.vegetable_id`)
-  .then(res => {
-    return res.rows
+router.get('/api/users', (req, res) => {
+  getUsers().then(data => {
+    res.json(data)
   })
-  .catch(err => console.log(err));
-}
-  
+});
+
+router.get('/api/vegetables', (req, res) => {
+  getVeg().then(data => {
+    res.json(data)
+  })
+});
+
+
+
+//helper to select all veg
+const getVeg = () => db.query(`SELECT * FROM vegetables ORDER BY name`)
+  .then(res => {
+    return res.rows
+  }).catch(err => console.log(err));
+
+//helper to select all users
+const getUsers = () => db.query(`SELECT * FROM users`)
+  .then(res => {
+    return res.rows
+  }).catch(err => console.log(err));
+
+
+
 
 module.exports = router;
--- react-front-end/src/Components/VegetableCard.jsx
+++ react-front-end/src/Components/VegetableCard.jsx
@@ -9,6 +9,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+
 const useStyles = makeStyles({
   root: {
     maxWidth: 250,
