69bb4d056e97c6d89d83d318329f471745407950
Wagsnerlund <chelseawagner17@gmail.com>
2021-05-11 19:25:44 -0700

    changed styling to use sass

--- react-front-end/src/Components/Weather/WeatherItem.jsx
+++ react-front-end/src/Components/Weather/WeatherItem.jsx
@@ -1,44 +1,8 @@
 import React from "react";
 import Card from '@material-ui/core/Card';
-import { makeStyles } from '@material-ui/core/styles';
+import './WeatherItem.scss';
 // import moment from "moment";
 const moment = require('moment');
-
-
-const useStyles = makeStyles({
-  weatherCard: {
-    margin: '5px',
-    minWidth: '110px',
-    maxWidth: '110px',
-    width: '110px',
-    textAlign: 'center'
-  },
-  row: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  column: {
-    margin: '10px',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    textAlign: 'center'
-  },
-  weatherName: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: '20px',
-    marginBottom: '10px',
-    marginLeft: '20px',
-    marginRight: '20px',
-    textAlign: 'center'
-  },
-  weatherIcon: {
-    margin: '10px'
-  }
-});
 
 const kelvinToCelcius = (K) => {
   return Math.round((K - 273.15));
@@ -53,20 +17,19 @@ const getDay = (dte) => {
 }
 
 export default function WeatherItem(props) {
-  const classes = useStyles();
 
   return (
-    <Card className={classes.weatherCard}>
-      <main className={classes.row}>
-        <section className={classes.column}>
+    <Card className="weatherCard">
+      <main className="row">
+        <section className="column">
           <div>
             <strong>{getDayName(props.dte)}</strong>
             <div>{getDay(props.dte)}</div>
           </div>
-          <div className={classes.weatherName}>
+          <div className="weatherName">
             {props.description}
           </div>
-          <img className={classes.weatherIcon} src={`http://openweathermap.org/img/w/${props.icon}.png`} alt={''}/>
+          <img className="weatherIcon" src={`http://openweathermap.org/img/w/${props.icon}.png`} alt={''}/>
           <div>
             <strong>{kelvinToCelcius(props.tempMax)}&deg;C / {kelvinToCelcius(props.tempMin)}&deg;C</strong>
           </div>
