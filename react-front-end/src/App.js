d2af805f04aebe379adf429dc1a77101eaa509f0
Justin Anderson <justinanderson101@gmail.com>
2021-05-12 18:06:12 -0600

    added rss-parser, articles are displaying in the console

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -4,13 +4,27 @@ import './App.scss';
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import Home from "./components/home";
 
+const Parser = require('rss-parser');
+const parser = new Parser();
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: 'Click the button to load data!'
+      // message: 'Click the button to load data!'
+      feed: []
     }
   }
+
+  async componentDidMount() {
+    const feed = await parser.parseURL('https://cors-anywhere.herokuapp.com/https://www.goodnewsnetwork.org/category/news/inspiring/feed/');
+    this.setState({ feed });
+    
+    feed.items.slice(-5).forEach(item => {
+      console.log(`${item.title}: ${item.link}`);
+    });
+   }    
+
   //this now fetches the user info and posts for user #1 Michael Scott
   fetchDataUser1 = () => {
     axios.get('/api/users/profile/1') // You can simply make your requests to "/api/whatever you want"
