487a2ebb651dd180532cb6e10208b6835bcebf54
Josiah <josiah.pilon@yahoo.ca>
2021-05-13 19:24:49 -0700

    Changed path to Home components

--- react-front-end/src/App.tsx
+++ react-front-end/src/App.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState, useEffect, useRef } from 'react';
 import './styles/App.css';
-import Home from './components/Home';
+import Home from './components/Home/index';
 import Categories from './components/Categories';
 import Entries from './components/Entries';
 import Navbar from './components/menu/Navbar';
