e949e75c0d2628dc1b60d533c62a9124a44997d8
sunny nguyen-bui <snguyenbui@gmail.com>
2021-05-15 15:49:32 -0700

    able to login with user ID number in input field

--- react-front-end/src/component/Login.js
+++ react-front-end/src/component/Login.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login(props) {
+  const [user, setUser] = useState();
   return (
-    <div>
+    <form onSubmit={(event) => event.preventDefault()}>
       <h1>The login page</h1>
+      <input
+        type="text"
+        name="User"
+        title="User"
+        onChange={(event) => setUser(event.target.value)}
+      ></input>
       <Link
         to="/"
         onClick={() => {
-          props.onLogin(1);
+          props.onLogin(user);
         }}
         style={{ textDecoration: "none", color: "lavender" }}
       >
-        <button>login</button>
+        <button>Login</button>
       </Link>
-    </div>
+    </form>
   );
 }
