7db88762ebf6731a4b87b5ad7c784ca0218a83b6
sunny nguyen-bui <snguyenbui@gmail.com>
2021-05-15 14:57:35 -0700

    removed useEffect import

--- react-front-end/src/component/Form.js
+++ react-front-end/src/component/Form.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
--- react-front-end/src/component/FormJobs.js
+++ react-front-end/src/component/FormJobs.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import { useStyles } from "./Component_Style/FormJobs.jsx";
