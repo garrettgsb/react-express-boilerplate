06d98103450555a39c77016ab3b41655efcb5081
Adam Hart <adamhayhart@gmail.com>
2021-05-15 19:24:35 -0700

    styled single job posts

--- react-front-end/src/component/Component_Style/JobListItem.jsx
+++ react-front-end/src/component/Component_Style/JobListItem.jsx
@@ -1,3 +1,39 @@
-// import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
-// export default useStyles;
+const useStyles = makeStyles({
+  rootContainer: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  root: {
+    minWidth: 275,
+    maxWidth: "100%",
+    margin: "20px",
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 4px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 16,
+  },
+  contactContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  contactButton: {
+    padding: "10px",
+    "&:hover": {
+      color: "green",
+    },
+  },
+});
+
+export { useStyles };
