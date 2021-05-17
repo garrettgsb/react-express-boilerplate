3b7b6348c136eb89ece2a472d705aba9f975fcac
Adam Hart <adamhayhart@gmail.com>
2021-05-17 11:37:19 -0700

    small 404 style adjustments

--- react-front-end/src/component/Component_Style/404.jsx
+++ react-front-end/src/component/Component_Style/404.jsx
@@ -1,26 +1,32 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   container_container: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "center",
     background: "#00007f",
-    // zIndex: "-5",
+    padding: theme.spacing(1),
+    // [theme.breakpoints.down("sm")]: {
+    //   display: "flex",
+    //   flexDirection: "column",
+    // },
+    // [theme.breakpoints.up("md")]: {
+    //   display: "flex",
+    //   flexDirection: "column",
+    // },
   },
-
   page_container: {
     background: "#00007f",
     display: "flex",
     flexDirection: "column",
-    // justifySelf: "center",
     alignItems: "center",
     width: "80%%",
     height: "100%",
     margin: "0",
     padding: "0",
     paddingTop: "20%",
-    paddingLeft: "20%",
+    paddingLeft: "12%",
     marginRight: "-50px",
     zIndex: "1",
   },
@@ -70,7 +76,9 @@ const useStyles = makeStyles({
     },
   },
   link_container: {
-    background: "#00007f",
+    // background: "#00007f",
+    // display: "flex",
+    // justifySelf: "flex-start",
     // marginLeft: "-100px",
     // zIndex: "-1",
   },
@@ -78,6 +86,6 @@ const useStyles = makeStyles({
   //   background: "#00007f",
   //   paddingBottom: "30px" /*whatever the height of your footer is*/,
   // },
-});
+}));
 
 export { useStyles };
