d7bd3444c94b94ca7df89d13771be553335f55dc
Eppa Lea Turniawan <eppalea@gmail.com>
2021-06-07 17:16:24 -0700

    update route names. fix spacing

--- express-back-end/routes/items.js
+++ express-back-end/routes/items.js
@@ -5,7 +5,7 @@ module.exports = (db) => {
 
   
   // BROWSE - view household decor page ==> GET /items
-  router.get('/items', (req, res) => {
+  router.get('/', (req, res) => {
 
     db.query(`SELECT * FROM items;`)
       .then(data => {
@@ -21,12 +21,12 @@ module.exports = (db) => {
   });
   
   // READ - view specific item's page ==> GET /items/:id
-  router.get('/items/:id', (req, res) => {
+  router.get('/:id', (req, res) => {
 
     db.query(`SELECT * FROM items WHERE id = $1`, [req.params.id])
       .then(data => {
         const item = data.rows[0];
-        res.render('item show', {item});
+        res.render('item_show', {item});
       })
       .catch(err => {
         res
@@ -35,105 +35,91 @@ module.exports = (db) => {
       });
   });
 
-  // EDIT - Admin edit data ==> POST /items/:id
-  router.post('/items/:id', (req, res) => {
 
-    db.query(`SELECT * FROM items WHERE id = $1`,[req.params.id])
-    .then(data => {
-      const item = data.rows[0];
-      res.render('item_show', {item, userID:item.user_id});
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-});
+  // EDIT - admin edit data ==> POST /items/:id
+  router.post('/:id', (req, res) => {
 
-// EDIT - admin edit data ==> POST /items/:id
-router.post('items/:id', (req, res) => {
+    const queryParams = [];
 
-  const queryParams = [];
+    let queryString = `UPDATE items SET `;
 
-  let queryString = `UPDATE items SET `;
+    if (req.body.name) {
+      queryParams.push(req.body.name);
+      queryString += `name = $${queryParams.length}, `;
+    }
 
-  if (req.body['item name']) {
-    queryParams.push(req.body['item name']);
-    queryString += `name = $${queryParams.length}, `;
-  }
+    if (req.body.price) {
+      queryParams.push(req.body.price);
+      queryString += `price = $${queryParams.length}, `;
+    }
 
-  if (req.body.price) {
-    queryParams.push(req.body.price);
-    queryString += `price = $${queryParams.length}, `;
-  }
-
-  if (req.body.current_status) {
-    queryParams.push(req.body.current_status);
-    queryString += `current_status = $${queryParams.length}, `;
-  }
-  
-  if (req.body.description) {
-    queryParams.push(req.body.description);
-    queryString += `description = $${queryParams.length}, `;
-  }
-
-  if (req.body.image) {
-    queryParams.push(req.body.image);
-    queryString += `image = $${queryParams.length}, `;
-  }
-
-  if (req.body.quantity) {
-    queryParams.push(req.body.quantity);
-    queryString += `quantity = $${queryParams.length}, `;
-  }
-
-  queryString = queryString.slice(0, queryString.length - 2);
-
-  queryParams.push(req.params.id);
-  queryString += `WHERE id = $${queryParams.length};`;
-
-  db.query(queryString, queryParams)
-    .then(data => {
-      const items = data.rows[0];
-      res.redirect('/items');
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-});
+    if (req.body.current_status) {
+      queryParams.push(req.body.current_status);
+      queryString += `current_status = $${queryParams.length}, `;
+    }
     
-// ADD - admin add item  ==> POST /items
-router.post('/items', (req, res) => {
+    if (req.body.description) {
+      queryParams.push(req.body.description);
+      queryString += `description = $${queryParams.length}, `;
+    }
 
-  db.query(`INSERT INTO items (user_id, name, price, current_status, description, image, quantity)
-   VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
-  [req.session.user_id, req.body['item name'], req.body.price, req.body.current_status, req.body.description, req.body.image, req.body.quantity])
-    .then(data => {
-      const items = data.rows[0];
-      res.redirect('/items');
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-});
+    if (req.body.image) {
+      queryParams.push(req.body.image);
+      queryString += `image = $${queryParams.length}, `;
+    }
 
- // DELETE - admin delete item ===> POST /items/:id/delete
- router.post('items/:id/delete', (req, res) => {
+    if (req.body.quantity) {
+      queryParams.push(req.body.quantity);
+      queryString += `quantity = $${queryParams.length}, `;
+    }
 
-  db.query(`DELETE FROM items WHERE id = $1;`,[req.params.id])
-    .then(data => {
-      res.redirect('/items');
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-});
+    queryString = queryString.slice(0, queryString.length - 2);
+
+    queryParams.push(req.params.id);
+    queryString += `WHERE id = $${queryParams.length};`;
+
+    db.query(queryString, queryParams)
+      .then(data => {
+        const items = data.rows[0];
+        res.redirect('/items');
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+    
+  // ADD - admin add item  ==> POST /items
+  router.post('/', (req, res) => {
+
+    db.query(`INSERT INTO items (user_id, name, price, current_status, description, image, quantity)
+    VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
+    [req.session.user_id, req.body['item name'], req.body.price, req.body.current_status, req.body.description, req.body.image, req.body.quantity])
+      .then(data => {
+        const items = data.rows[0];
+        res.redirect('/items');
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  // DELETE - admin delete item ===> POST /items/:id/delete
+  router.post('/:id/delete', (req, res) => {
+
+    db.query(`DELETE FROM items WHERE id = $1;`,[req.params.id])
+      .then(data => {
+        res.redirect('/items');
+      })
+      .catch(err => {
+        res
+          .status(204)
+          .json({ error: err.message });
+      });
+  });
 
 return router;
 
