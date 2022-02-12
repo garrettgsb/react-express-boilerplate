1e66193bf0b50a9a6b6e8b8c0c82c1ae90c75c15
Aisha Sabrie <sabriealt@gmail.com>
2022-02-12 14:47:40 -0500

    Write schemas add files to git ignore

---
+++ server/db/migrations/schema.sql
@@ -0,0 +1,76 @@
+
+DROP TABLE IF EXISTS users CASCADE;
+
+DROP TABLE IF EXISTS adventure CASCADE;
+
+DROP TABLE IF EXISTS reservations CASCADE;
+
+DROP TABLE IF EXISTS adventure_reviews CASCADE;
+
+DROP TABLE IF EXISTS favourites CASCADE;
+
+DROP TABLE IF EXISTS excursion_availibilties CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE adventures (
+   id SERIAL PRIMARY KEY NOT NULL,
+   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+   
+   title VARCHAR(255) NOT NULL,
+   description TEXT ,
+   thumbnail_photo_url VARCHAR(255) NOT NULL,
+   cover_photo_url VARCHAR(255) NOT NULL,
+   prev_price INTEGER  NOT NULL DEFAULT 0,
+   curr_price INTEGER  NOT NULL DEFAULT 0,
+   accesible BOOLEAN,
+
+   street VARCHAR(255) NOT NULL,
+   city VARCHAR(255) NOT NULL,
+   province_state VARCHAR(255) NOT NULL,
+   post_code_zip VARCHAR(255) NOT NULL,
+   country VARCHAR(255) NOT NULL,
+
+   active BOOLEAN NOT NULL DEFAULT TRUE,
+   is_full BOOLEAN NOT NULL DEFAULT FALSE,
+   max_occupancy INTEGER  NOT NULL DEFAULT 1,
+   season VARCHAR(255),
+   category VARCHAR(255)
+);
+
+CREATE TABLE reservations (
+   id SERIAL PRIMARY KEY NOT NULL,
+   date DATE NOT NULL,
+   adventure_id INTEGER REFERENCES adventures(id) ON DELETE CASCADE,
+   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+   total_guest INTEGER NOT NULL DEFAULT 1,
+   payment_id VARCHAR(255) NOT NULL,
+   total_price INTEGER NOT NULL
+);
+
+CREATE TABLE adventure_reviews (
+   id SERIAL PRIMARY KEY NOT NULL,
+   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+   adventure_id INTEGER REFERENCES adventures(id) ON DELETE CASCADE,
+   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+   rating SMALLINT NOT NULL DEFUALT 1,
+   comment TEXT
+);
+
+CREATE TABLE favourites (
+   id SERIAL PRIMARY KEY NOT NULL,
+   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+   adventure_id INTEGER REFERENCES adventures(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE excursion_availibilties (
+   id SERIAL PRIMARY KEY NOT NULL,
+   adventure_id INTEGER REFERENCES adventures(id) ON DELETE CASCADE,
+   date_time timestamp with time zone,
+   available BOOLEAN DEFAULT TRUE
+);
