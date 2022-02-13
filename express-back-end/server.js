c459b3f6b3df6f74d39e23c07c301fd4ef2d0bfc
Trigun Joshi <trigunjoshi@gmail.com>
2022-02-12 17:17:58 -0800

    express server created

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,17 +1,25 @@
 const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
+
+
+const { getAllUsers } = require('./database/queries');
+const { client } = require('./database/connection');
 const PORT = 8080;
 
-// Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
+
+
 
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
 }));
+
+
+
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
