1c835c0e525a621b03d17e9f2a02fa6ff4e461ea
Georgina Bada <georginabada@gmail.com>
2022-02-12 19:19:18 -0500

    Creating seeds for users database

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -2,9 +2,14 @@ DROP TABLE IF EXISTS users CASCADE;
  
 CREATE TABLE users (
  id SERIAL PRIMARY KEY NOT NULL,
- name VARCHAR(50),
- user_name VARCHAR(50),
+ first_name VARCHAR(50),
+ last_name VARCHAR(50),
  email VARCHAR(50),
  password VARCHAR(50),
- phone_number VARCHAR(50)
+ phone_number VARCHAR(50),
+ country VARCHAR(50),
+province VARCHAR(50),
+city VARCHAR(50),
+street VARCHAR(50),
+postal VARCHAR(50)
 );
---
+++ db/seeds/01_users_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO users
+  (id,first_name,last_name,email, password, phone_number, country, province, city, street, postal)
+VALUES
+(1,"Georgina", "Bada", "georgina@email.com","password123", 555-555-5551, "Nigeria", "Lagos","Lagos","Toronto St", "IJN 123"),
+(2,"Zainab", "Bokhari", "Zainab@email.com","1234", 555-555-5552,"Canada","Onatrio", "Toronto", "Fake St", "M1F 6H0"),
+(3,"Rofiat","Olusanya","rofiat@email.com","4567",555-555-5553, "Canada", "Ontario","Toronto", "Seseame St", "W4W 3R3"),
+(4,"Mary J","Blige","mary@email.com","123abc",555-555-5554, "America", "New York","Yonkers", "Soul St", "NY 1033"),
+(5,"Jane","Doe","jane@email.com","jane123",555-555-5554, "America", "California","oakland", "False St", "CA 2033");
