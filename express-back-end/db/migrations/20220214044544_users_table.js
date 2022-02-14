0590495a93ec4d2fce71b286708e0821d5e89ad8
Julia <julia.sarandi@gmail.com>
2022-02-13 21:34:46 -0800

    Create initial migration and seeds for users table

---
+++ express-back-end/db/migrations/20220214044544_users_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('name');
+    table.string('email');
+    table.text('password');
+    table.string('avatar');
+    table.string('quote');
+    table.timestamps;
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
--- express-back-end/server.js
+++ express-back-end/server.js
@@ -3,20 +3,30 @@ const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
 
-// Express Configuration
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static('public'));
+const main = async() => {
+  const pool = require('knex')(require('./knexfile').development);
+  await pool.migrate.latest();
+  
+  // Express Configuration
+  App.use(BodyParser.urlencoded({ extended: false }));
+  App.use(BodyParser.json());
+  App.use(Express.static('public'));
+  
 
-const pool = require('knex')(require('./knexfile').development);
+  // Sample GET route
+  App.get('/api/data', (req, res) => res.json({
+    message: "Seems to work!",
+  }));
+  
+  App.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+  
+    console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+  });
+};
 
-// Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
-
-App.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(pool);
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
-});
+main()
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  })
