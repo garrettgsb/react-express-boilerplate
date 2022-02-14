e93126c9264827ec2fa1d2d653435dbb8ebe3dae
Josue Arevalo <arevalo.josued@gmail.com>
2022-02-14 15:57:20 -0800

    Navbar with Semantics

--- react-front-end/src/components/Navbar.jsx
+++ react-front-end/src/components/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Dropdown, Menu } from "semantic-ui-react";
 
 export default function Navbar(props) {
   const { user } = props;
+
+  const [activeItem, setActiveItem] = useState("home");
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
   return (
+    <Menu size="large">
+      <Menu.Item
+        name="home"
+        active={activeItem === "home"}
+        onClick={handleItemClick}
+      />
 
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Button primary>Login/Sign Up</Button>
+        </Menu.Item>
 
+        <Dropdown item text="Language">
+          <Dropdown.Menu>
+            <Dropdown.Item>Newsfeed</Dropdown.Item>
+            <Dropdown.Item>Add Plant</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Menu>
+    </Menu>
   );
 }
