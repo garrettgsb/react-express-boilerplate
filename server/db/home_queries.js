2eaec52c28e8469b0a1aae8ade0a19a19c3e1fbb
“Victoria <victoriahall.dev@gmail.com>
2022-02-14 22:09:27 +0000

    created routes for homepage

---
+++ server/db/home_queries.js
@@ -0,0 +1,43 @@
+const { pool } = require('./database');
+
+
+const getCategories = () => {
+  return pool.query(`SELECT DISTINCT category FROM adventures;`)
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((err) => {
+      return err.message;
+    });
+}
+
+const placeholder = () => {
+  return pool.query(`SELECT count(*) FROM adventures`
+  )
+    .then(data => {
+      const x = Math.floor(Math.random() * data.rows[0].count);
+      return pool.query(`SELECT city FROM adventures where id = ${x}`)
+    })
+    .then(data => { return data.rows[0] })
+}
+
+const searchDestination = () => {
+  const search = req.params.input
+  return pool.query(
+    `SELECT * 
+    FROM adventures 
+    WHERE lower(city) LIKE lower(‘%$1%’) 
+    OR lower(country) LIKE lower(‘%$1%’) 
+    OR lower(province_state) LIKE lower(‘%$1%’)
+    `, [search])
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((err) => {
+      return 'Hmm... It doesnt seem like we have that location. Try searching again or make your own listing!';
+    });
+}
+
+
+module.exports = { getCategories, placeholder, searchDestination };
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //declarations
 const Express = require('express');
 const bodyParser = require('body-parser');
+const homepage = require('./db/home_queries.js')
 
 //env variables
 const PORT = 8080;
@@ -12,9 +13,13 @@ app.use(bodyParser.json());
 app.use(Express.static('public'));
 
 // Sample GET route
-app.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
+app.get('/api/data', (req, res) => {
+  homepage.searchDestination()
+    .then((products) => {
+      res.json(products);
+      // res.render()
+    });
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
