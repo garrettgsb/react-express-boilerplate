2f4b7fcc314e3f55caf7a8d3c37212cbb05e9c37
Josue Arevalo <arevalo.josued@gmail.com>
2022-02-13 20:05:17 -0800

    knex & postgres setup

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -8,6 +8,8 @@ App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
 
+const pool = require('knex')(require('./knexfile').development);
+
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
@@ -15,5 +17,6 @@ App.get('/api/data', (req, res) => res.json({
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
+  console.log(pool);
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
