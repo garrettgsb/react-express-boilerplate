a839841cf62ca981b3a5b5f1cf9c4723e5d36ad7
“Victoria <victoriahall.dev@gmail.com>
2022-02-15 04:30:05 +0000

    created route files and added comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,21 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import {BrowserRouter as Router, Link, Route, Routes, Navigate} from 'react-router-dom';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
-
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
+const App = () => {
+  return (
+    <div className='App'>
+    <h2>React Router</h2>
+    <Router>
+      <Link to="/">Home </Link>
+      <Link to="/users">Users </Link>
+      <Link to="/adventures">Adventures </Link>
+    </Router>
       </div>
-    );
-  }
-}
+  );
 
-export default App;
+};
+
+export default App
---
+++ server/routes/my_reviews.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+
+// get to /id to get reviews
+// post reviews form
