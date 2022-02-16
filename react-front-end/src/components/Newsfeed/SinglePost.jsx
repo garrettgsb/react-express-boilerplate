f5d86968d33f8bd91fcd322e8701b19f50145956
Julia <julia.sarandi@gmail.com>
2022-02-16 11:51:05 -0800

    Add single post elemets

--- react-front-end/src/components/Newsfeed/SinglePost.jsx
+++ react-front-end/src/components/Newsfeed/SinglePost.jsx
@@ -1,14 +1,32 @@
 import React from "react";
-import "semantic-ui-css/semantic.min.css"
-import { Link } from 'react-router-dom';
+import "semantic-ui-css/semantic.min.css";
+import { Container, Grid, Icon, Image, Segment, Feed } from "semantic-ui-react";
 
 export default function SinglePost({id, user_id, title, photo, description, topic, created_at}) {
   return (
-     <div className="ui link cards">
-        <h4>{title}</h4>
-        <img src={photo} alt="post image"/>
-        <h5>{description}</h5> 
-    </div>
+    
+       <Segment raised>
+         <h2 left>{title}</h2>
+         <Feed.Label>
+        <Image src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' avatar/>
+      </Feed.Label>
+         <Segment.Group horizontal>
+          <Image src={photo} width='50%' height='100%' rounded/>
+          <h5>{description}</h5> 
+         </Segment.Group>
+        
+        <Segment.Group horizontal>
+          <Segment><Icon name='heart'/> I love it!</Segment>
+          <Segment><Icon name='plus'/> Follow</Segment>
+          <Segment>
+            <Icon name='envelope'/> Send Message
+          </Segment>
+        </Segment.Group>
+    
+       </Segment>
+
+       
+    
        
   )
 }
