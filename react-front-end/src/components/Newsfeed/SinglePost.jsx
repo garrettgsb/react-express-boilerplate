60a15f007165c88e08275203e2f551db9c4efd16
Julia <julia.sarandi@gmail.com>
2022-02-16 14:55:07 -0800

    Updat Single post icons

--- react-front-end/src/components/Newsfeed/SinglePost.jsx
+++ react-front-end/src/components/Newsfeed/SinglePost.jsx
@@ -7,19 +7,41 @@ export default function SinglePost({id, user_id, title, photo, description, topi
     
        <Segment raised>
          <h2 left>{title}</h2>
-         <Feed.Label>
-        <Image src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' avatar/>
-      </Feed.Label>
+         <div>
+            <Image src='https://react.semantic-ui.com/images/wireframe/square-image.png' avatar />
+            <span>posted by Chikorita</span>
+          </div>
          <Segment.Group horizontal>
           <Image src={photo} width='50%' height='100%' rounded/>
           <h5>{description}</h5> 
          </Segment.Group>
         
         <Segment.Group horizontal>
-          <Segment><Icon name='heart'/> I love it!</Segment>
-          <Segment><Icon name='plus'/> Follow</Segment>
+          <Segment><div class="ui labeled button" tabindex="0">
+            <div class="ui button">
+              <i class="heart icon"></i> Like
+            </div>
+            <a class="ui basic label">
+              2,048
+            </a>
+          </div></Segment>
+          <Segment><div class="ui labeled button" tabindex="0">
+            <div class="ui button">
+              <i class="plus icon"></i>
+            </div>
+              <a class="ui basic label">
+                Follow
+              </a>
+            </div></Segment>
           <Segment>
-            <Icon name='envelope'/> Send Message
+          <div class="ui labeled button" tabindex="0">
+            <div class="ui button">
+              <i class="envelope icon"></i>
+            </div>
+              <a class="ui basic right pointing label">
+                Message
+              </a>
+            </div>
           </Segment>
         </Segment.Group>
     
