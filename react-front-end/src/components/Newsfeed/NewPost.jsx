498e001a47e4987a1420b0ee9e5c5ce180e08fa5
Julia <julia.sarandi@gmail.com>
2022-02-17 12:57:51 -0800

    Create form for new post

--- react-front-end/src/components/Newsfeed/NewPost.jsx
+++ react-front-end/src/components/Newsfeed/NewPost.jsx
@@ -1,67 +1,32 @@
-import React from 'react'
-import { Button, Checkbox, Form, Component, Segment } from 'semantic-ui-react'
+import React from "react";
+import { Segment } from "semantic-ui-react";
+import { useForm } from "react-hook-form";
 
+export default function NewPost() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+   
+  return (
+    <Segment raised>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Title</label>
+        <input {...register("title")} placeholder='Please enter a title for your post' />
+        <label>Description</label>
+        <input {...register("description")} placeholder='Tell us more...' />
+        <label>Topic of your post is:</label>
+        <select {...register("topic")}>
+          <option value="general">female</option>
+          <option value="question">male</option>
+          <option value="plant hack">other</option>
+        </select>
 
-class NewPost extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-     value: ''
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-    // this.setState({description: event.target.description});
-    // this.setState({topic: event.target.topic});
-    // this.setState({image: event.target.image});
-  }
-
-  handleSubmit(event) {
-    alert('Magic happens and new post appears: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-<Segment raised>
-  <Form onSubmit={this.handleSubmit}>
-    <Form.Field>
-      <label>Title</label>
-      <input value={this.state.title} onChange={this.handleChange} placeholder='Please enter a title for your post' />
-    </Form.Field>
-    <Form.Field>
-      <label>Description</label>
-      <textarea value={this.state.description} onChange={this.handleChange} placeholder='Tell us more...' />
-    </Form.Field>
-
-    <label>
-          Topic of you post is:
-          <select value={this.state.topic} onChange={this.handleChange}>
-            <option value="general">General</option>
-            <option value="question">Question</option>
-            <option value="plant hack">Plant Hack</option>
-          </select>
-        </label>
-
-    <Form.Field>
-    <label>Upload an Image</label>
-      <input type="file" />
-    </Form.Field>
-
-    <div class="ui buttons">
-      <button class="ui button">Cancel</button>
-      <div class="or"></div>
-      <button class="ui positive button">Save</button>
-    </div>
-
-  </Form>
-  </Segment>
-    
-    )
-  }
-}
-export default NewPost
+        <input type="submit" />
+        <div class="ui buttons">
+        <button class="ui button">Cancel</button>
+        <div class="or"></div>
+        <button class="ui positive button" onClick={onSubmit} >Save</button>
+      </div>
+      </form>
+    </Segment>
+  );
+};
