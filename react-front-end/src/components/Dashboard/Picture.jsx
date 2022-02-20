385da3e91b5d2ab08b0278db45a919f0f579e466
Lucy Shen <lucyshen77@gmail.com>
2022-02-20 15:00:34 -0800

    Style progress bars for Dashboard / Rooms

--- react-front-end/src/components/Dashboard/Picture.jsx
+++ react-front-end/src/components/Dashboard/Picture.jsx
@@ -6,16 +6,28 @@ import { Button, Image, Icon, Progress } from "semantic-ui-react";
 
 function Picture({ id, url, key, nickname, setSelectedPlant, plant, reminder }) {
 
-  const [progress, setProgress] = useState(33);
-
-  reminder && console.log('reminder!!!', reminder);
-  plant && console.log('plant!!!', plant);
+  // reminder && console.log('reminder!!!', reminder);
+  // plant && console.log('plant!!!', plant);
 
   const today = Date.now();
-  // const timeRemaining = today;
-  const timeRemaining = today;
+  const lastWatered = reminder && Date.parse(reminder.last_watered);
+  // reminder && console.log('Date.parse(reminder.last_watered)', Date.parse(reminder.last_watered));
+  // console.log('today', today);
+  // reminder && console.log('normal last water', reminder.last_watered);
+  // console.log('regular date today', Date.now());
 
-  console.log({ timeRemaining });
+  const diff = today - lastWatered;
+  const daysDiff = Math.floor(diff / 1000 / 60 / 60 / 24);
+
+  // reminder && console.log('interval !!!', reminder.watering_interval);
+  // console.log( {diff} )
+  // console.log({ daysDiff })
+
+  const daysRemaining = reminder && (reminder.watering_interval - daysDiff);
+
+  const waterPercent = reminder && Math.round((daysRemaining / reminder.watering_interval) * 100);
+  // console.log('waterPercent', waterPercent)
+  // const [progress, setProgress] = useState( 80 );
 
   const [{ isDragging }, drag] = useDrag(() => ({
     type: "image",
@@ -27,29 +39,66 @@ function Picture({ id, url, key, nickname, setSelectedPlant, plant, reminder }) 
     options: { dropEffect: "move" }
   }));
 
-  const water = () => {
-    setProgress((prev) => (prev >= 100 ? 0 : prev + 20));
-  }
+  // const water = () => {
+  //   setProgress((prev) => (prev >= 100 ? 0 : prev + 20));
+  // }
 
   return (
     <>
       <Image
-        size="small"
-        style={{ border: isDragging ? "5px solid pink" : "0px", background: "#EBEBE8", opacity: "0.95", borderRadius: "5%", padding: "10px", margin: "10px" }}
+        style={{
+          border: isDragging ? "5px solid pink" : "0px",
+          background: "#EBEBE8",
+          opacity: "0.95",
+          borderRadius: "5%",
+          padding: "10px",
+          margin: "10px",
+          width: "200px"
+        }}
+        size='tiny'
+      // circular
       >
-        <img
-          ref={drag}
-          src={url}
-          style={{ borderRadius: "15%" }}
-        />
-        <div>
-          <p><b>{nickname}</b></p>
-          <Button color="olive" onClick={() => {
-            setSelectedPlant(plant);
-          }}><Icon name="leaf" />Info</Button>
-          <div>
-            <Progress color="blue" percent={progress} indicating />
-            <Button onClick={water}>Water</Button>
+        <div className="plant-card">
+          <img
+            ref={drag}
+            src={url}
+            style={{ borderRadius: "15%", width: "80px" }}
+          />
+
+          <div className="plant-progress">
+            <b><i>{nickname}</i></b>
+            <Button
+              color="olive"
+              size="tiny"
+              onClick={() => {
+                setSelectedPlant(plant);
+              }}>
+              <Icon name="leaf" />Info
+            </Button>
+            {/* <Button
+            color="blue"
+            size="tiny"
+            onClick={water}
+          >
+            <Icon name="check circle" />Water
+          </Button> */}
+            <Progress
+              color="red"
+              // percent={waterPercent}
+              // onClick={water} 
+              indicating
+              // value={daysRemaining}
+              // total={reminder && reminder.watering_interval}
+              // progress="ratio"
+              // value={daysRemaining}
+              // total={reminder && reminder.watering_interval}
+              // progress="ratio"
+              value={waterPercent >= 0 ? daysRemaining : 0}
+              progress="ratio"
+              total={reminder && reminder.watering_interval}
+              label="Days Left"
+              style={{ fontSize: "11px" }}
+            />
           </div>
         </div>
       </Image>
