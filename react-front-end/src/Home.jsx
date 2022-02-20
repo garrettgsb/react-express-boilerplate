2cd32eb1bc7115044c1f01b6bd463ea434cc785f
Julia <julia.sarandi@gmail.com>
2022-02-20 02:49:32 -0800

    Refactor Homepage to show login form on click

--- react-front-end/src/Home.jsx
+++ react-front-end/src/Home.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { Image, Grid } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Button, Image } from 'semantic-ui-react';
 import './index.css';
 import flying_bee from './assets/flying_bee.png';
 import LoginForm from './components/LoginForm';
 
-export default function Home() {
+export default function Home({ login, user }) {
+  const [isVisibleLogin, setIsVisibleLogin] = useState(false);
+
   return (
     <main className='home'>
-      <LoginForm />
+      <Image src={flying_bee} size='large' id='bee' />
+
+      {isVisibleLogin && <LoginForm login={login} user={user} />}
 
       <h2 id='style-2' data-replace='Welcome to beleaf'>
         Welcome to beleaf
+        <Button inverted color='olive' size='huge' onClick={() => setIsVisibleLogin(true)}>
+          Let's Grow
+        </Button>
       </h2>
-      <span></span>
-      <Image src={flying_bee} size='large' id='bee' />
     </main>
   );
 }
