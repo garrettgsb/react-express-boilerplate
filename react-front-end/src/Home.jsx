073bf3195a03fd2c5fee6ac442e430144740e82d
Julia <julia.sarandi@gmail.com>
2022-02-20 00:29:26 -0800

    Adjust homepage

--- react-front-end/src/Home.jsx
+++ react-front-end/src/Home.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-import {Image} from "semantic-ui-react";
+import React from 'react';
+import { Image, Grid } from 'semantic-ui-react';
 import './index.css';
-import flying_bee from "./assets/flying_bee.png"
+import flying_bee from './assets/flying_bee.png';
+import LoginForm from './components/LoginForm';
 
 export default function Home() {
   return (
-   <main className="home" > 
-    
-      <h2 id="style-2" data-replace="Welcome to beleaf">Welcome to beleaf</h2><span></span>
-      <Image src={flying_bee} size="large" id="bee"/>
+    <main className='home'>
+      <LoginForm />
+
+      <h2 id='style-2' data-replace='Welcome to beleaf'>
+        Welcome to beleaf
+      </h2>
+      <span></span>
+      <Image src={flying_bee} size='large' id='bee' />
     </main>
-  
   );
 }
