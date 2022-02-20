bc8c3ecc9a1c6ff769cec2967804a7828c16dfd5
Julia <julia.sarandi@gmail.com>
2022-02-20 08:18:50 -0800

    Add timestamp column and value to comments table

--- express-back-end/db/seeds/08_comments.sql
+++ express-back-end/db/seeds/08_comments.sql
@@ -1,7 +1,7 @@
-INSERT INTO comments (post_id, comment_user_id, comment_text)
-VALUES (1, 2, 'That is so great, thenks for posting!'),
-(2, 3, 'Hey, lorem ipsum bla-bla'),
-(3, 1, 'Rise and shine it''s a new day!'),
-(2, 1, 'We all learning here, it is ok'),
-(3, 3, 'To be or not to be, this is a question')
+INSERT INTO comments (post_id, comment_user_id, comment_text, commented_at)
+VALUES (1, 2, 'That is so great, thenks for posting!', '02-02-2022'),
+(2, 3, 'Hey, lorem ipsum bla-bla', '02-02-2022'),
+(3, 1, 'Rise and shine it''s a new day!', '02-02-2022'),
+(2, 1, 'We all learning here, it is ok', '02-02-2022'),
+(3, 3, 'To be or not to be, this is a question', '02-02-2022')
 ;
