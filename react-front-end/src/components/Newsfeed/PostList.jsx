a16e116253d60c76ec49fd486151bf3e15b2adcd
Julia <julia.sarandi@gmail.com>
2022-02-19 23:07:26 -0800

    Update props for Newsfeed and Postlist

--- react-front-end/src/components/Newsfeed/PostList.jsx
+++ react-front-end/src/components/Newsfeed/PostList.jsx
@@ -1,25 +1,27 @@
-import React from "react";
-import "semantic-ui-css/semantic.min.css";
-import { Container } from "semantic-ui-react";
-import SinglePost from "./SinglePost";
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
+import SinglePost from './single-post/SinglePost';
 
-export default function PostList({ posts, users, updatePostsTableWithLikes }) {
-  const renderPosts = posts.sort((a,b) => new Date(b.created_at) - new Date(a.created_at)).map((post) => {
+export default function PostList({ posts, users }) {
+  const renderPosts = posts
+    .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+    .map((post) => {
       return (
-      <SinglePost
-        key={post.id}
-        user_id={post.user_id}
-        title={post.title}
-        photo={post.photo}
-        description={post.description}
-        topic={post.topic}
-        number_of_likes={post.number_of_likes}
-        created_at={post.created_at}
-        users={users}
-        updatePostsTableWithLikes={updatePostsTableWithLikes}
-      />
-      )
-    })
+        <SinglePost
+          key={post.id}
+          id={post.id}
+          user_id={post.user_id}
+          title={post.title}
+          photo={post.photo}
+          description={post.description}
+          topic={post.topic}
+          number_of_likes={post.number_of_likes}
+          created_at={post.created_at}
+          users={users}
+        />
+      );
+    });
 
-  return <Container> {renderPosts} </Container>
-};
+  return <Container> {renderPosts} </Container>;
+}
