06b8ccfbdf1ce5fb5081299715aa53a1c04b7c97
Julia <julia.sarandi@gmail.com>
2022-02-20 08:11:31 -0800

    Update schema and seeds to include comments table and values

---
+++ express-back-end/db/schema/08_comments.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS comments CASCADE;
+CREATE TABLE comments ( 
+  id SERIAL PRIMARY KEY NOT NULL,
+  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+  comment_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE, 
+  comment_text VARCHAR(255)
+);
---
+++ express-back-end/db/seeds/08_comments.sql
@@ -0,0 +1,7 @@
+INSERT INTO comments (post_id, comment_user_id, comment_text)
+VALUES (1, 2, 'That is so great, thenks for posting!'),
+(2, 3, 'Hey, lorem ipsum bla-bla'),
+(3, 1, 'Rise and shine it''s a new day!'),
+(2, 1, 'We all learning here, it is ok'),
+(3, 3, 'To be or not to be, this is a question')
+;
