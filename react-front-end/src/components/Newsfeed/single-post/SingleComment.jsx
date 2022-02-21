5bb8e01d50be7154a45167f4feeb1edcc4adeedb
Julia <julia.sarandi@gmail.com>
2022-02-20 22:35:27 -0800

    Link  user's name and avatar in comments to their profiles

--- react-front-end/src/components/Newsfeed/single-post/SingleComment.jsx
+++ react-front-end/src/components/Newsfeed/single-post/SingleComment.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
-import moment from 'moment';
-import 'semantic-ui-css/semantic.min.css';
-import { Feed } from 'semantic-ui-react';
-import { getUserById } from '../../../helpers/selectors';
+import React, { useState } from "react";
+import moment from "moment";
+import "semantic-ui-css/semantic.min.css";
+import { Link } from "react-router-dom";
+import { Feed } from "semantic-ui-react";
+import { getUserById } from "../../../helpers/selectors";
 
 export default function SingleComment({ users, comment_user_id, comment_text, commented_at }) {
-  const date = moment(commented_at).startOf('day').fromNow();
+  const date = moment(commented_at).startOf("day").fromNow();
 
   const user = getUserById(users, comment_user_id);
+  const linkToUserProfile = "/profile/" + user.id;
   return (
     <Feed>
       <Feed.Event>
-        <Feed.Label image={user && user.avatar} />
+        <Feed.Label image={user && user.avatar} href={linkToUserProfile} />
         <Feed.Content>
           <Feed.Date>{date}</Feed.Date>
           <Feed.Summary>
-            <a>{user && user.name}</a> commented:
+            <a href={linkToUserProfile}>{user && user.name} </a> commented:
           </Feed.Summary>
           <Feed.Extra text>{comment_text}</Feed.Extra>
         </Feed.Content>
