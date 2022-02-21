3c7ffab2aaf9626805ec85cca0cdc9a8a0c95a06
Lucy Shen <lucyshen77@gmail.com>
2022-02-20 17:58:43 -0800

    change styling for Picture component (Rooms)

--- react-front-end/src/components/Dashboard/Picture.jsx
+++ react-front-end/src/components/Dashboard/Picture.jsx
@@ -6,28 +6,13 @@ import { Button, Image, Icon, Progress } from "semantic-ui-react";
 
 function Picture({ id, url, key, nickname, setSelectedPlant, plant, reminder }) {
 
-  // reminder && console.log('reminder!!!', reminder);
-  // plant && console.log('plant!!!', plant);
-
   const today = Date.now();
   const lastWatered = reminder && Date.parse(reminder.last_watered);
-  // reminder && console.log('Date.parse(reminder.last_watered)', Date.parse(reminder.last_watered));
-  // console.log('today', today);
-  // reminder && console.log('normal last water', reminder.last_watered);
-  // console.log('regular date today', Date.now());
-
   const diff = today - lastWatered;
   const daysDiff = Math.floor(diff / 1000 / 60 / 60 / 24);
-
-  // reminder && console.log('interval !!!', reminder.watering_interval);
-  // console.log( {diff} )
-  // console.log({ daysDiff })
-
   const daysRemaining = reminder && (reminder.watering_interval - daysDiff);
 
   const waterPercent = reminder && Math.round((daysRemaining / reminder.watering_interval) * 100);
-  // console.log('waterPercent', waterPercent)
-  // const [progress, setProgress] = useState( 80 );
 
   const [{ isDragging }, drag] = useDrag(() => ({
     type: "image",
@@ -39,10 +24,6 @@ function Picture({ id, url, key, nickname, setSelectedPlant, plant, reminder }) 
     options: { dropEffect: "move" }
   }));
 
-  // const water = () => {
-  //   setProgress((prev) => (prev >= 100 ? 0 : prev + 20));
-  // }
-
   return (
     <>
       <Image
@@ -53,10 +34,10 @@ function Picture({ id, url, key, nickname, setSelectedPlant, plant, reminder }) 
           borderRadius: "5%",
           padding: "10px",
           margin: "10px",
-          width: "200px"
+          width: "200px",
+          height: "120px"
         }}
         size='tiny'
-      // circular
       >
         <div className="plant-card">
           <img
@@ -64,44 +45,34 @@ function Picture({ id, url, key, nickname, setSelectedPlant, plant, reminder }) 
             src={url}
             style={{ borderRadius: "15%", width: "80px" }}
           />
-
           <div className="plant-progress">
             <b><i>{nickname}</i></b>
-            <Button
-              color="olive"
-              size="tiny"
+
+            <Button animated='vertical'
+              color='olive'
+              size="mini"
+              style={{ height: "25px" }}
               onClick={() => {
                 setSelectedPlant(plant);
               }}>
-              <Icon name="leaf" />Info
-            </Button>
-            {/* <Button
-            color="blue"
-            size="tiny"
-            onClick={water}
-          >
-            <Icon name="check circle" />Water
-          </Button> */}
-            <Progress
-              color="red"
-              // percent={waterPercent}
-              // onClick={water} 
-              indicating
-              // value={daysRemaining}
-              // total={reminder && reminder.watering_interval}
-              // progress="ratio"
-              // value={daysRemaining}
-              // total={reminder && reminder.watering_interval}
-              // progress="ratio"
-              value={waterPercent >= 0 ? daysRemaining : 0}
-              progress="ratio"
-              total={reminder && reminder.watering_interval}
-              label="Days Left"
-              style={{ fontSize: "11px" }}
-            />
-          </div>
+            <Button.Content hidden>See Info</Button.Content>
+            <Button.Content visible>
+              <Icon name='leaf' />
+            </Button.Content>
+          </Button>
+
+          <Progress
+            color="red"
+            indicating
+            value={waterPercent >= 0 ? daysRemaining : 0}
+            progress="ratio"
+            total={reminder && reminder.watering_interval}
+            label="Days Left"
+            style={{ fontSize: "11px" }}
+          />
         </div>
-      </Image>
+      </div>
+    </Image>
     </>
   );
 }
