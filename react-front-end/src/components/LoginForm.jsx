ec9dafd9890e361314d46ff1a5487074c406de92
Julia <julia.sarandi@gmail.com>
2022-02-20 19:58:46 -0800

    Update login form

--- react-front-end/src/components/LoginForm.jsx
+++ react-front-end/src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Form, Grid, Checkbox, Segment } from 'semantic-ui-react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Grid, Checkbox, Segment } from "semantic-ui-react";
 
 export default function LoginForm({ login, user }) {
   //const loggedUser = getUserById(users, user);
@@ -10,25 +10,26 @@ export default function LoginForm({ login, user }) {
   };
 
   return (
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
       <Grid.Column style={{ maxWidth: 450 }}>
-        <Segment raised >
+        <Segment raised>
           <Form onSubmit={submitForm}>
             <Form.Field>
-              <Form.Input required={true} label='Email' placeholder='Enter your email' />
-            </Form.Field>
-
-            <Form.Field>
-              <Form.Input required={true} label='Password' placeholder='Enter your password' />
+              <Form.Input required={true} label="Email" value="bulbasaur@plant.it" placeholder="Enter your email" />
             </Form.Field>
             <Form.Field>
-              <Checkbox label='I will follow water reminders' />
+              <Form.Input required={true} label="Password" type="password" value="bla-bla-bla" placeholder="Enter your password" />
             </Form.Field>
-            <Link to='/dashboard'>
-              <div className='ui buttons'>
-                <button className='ui yellow button'>Sign Up</button>
-                <div className='or'></div>
-                <button type='submit' className='ui olive button' onClick={submitForm}>
+            <Form.Field>
+              <Segment compact>
+                <Checkbox toggle label="I agree to follow watering reminders" />
+              </Segment>
+            </Form.Field>
+            <Link to="/dashboard">
+              <div className="ui buttons">
+                <button className="ui yellow button">Sign Up</button>
+                <div className="or"></div>
+                <button type="submit" className="ui olive button" onClick={submitForm}>
                   Login
                 </button>
               </div>
