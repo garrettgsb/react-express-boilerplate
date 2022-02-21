183aa4d070e4cbe6e495c7ee617c5043c1f7d9aa
Julia <julia.sarandi@gmail.com>
2022-02-20 18:02:39 -0800

    Update seed with some more comments

--- express-back-end/db/seeds/08_comments.sql
+++ express-back-end/db/seeds/08_comments.sql
@@ -1,7 +1,14 @@
 INSERT INTO comments (post_id, comment_user_id, comment_text, commented_at)
-VALUES (1, 2, 'That is so great, thenks for posting!', '02-02-2022'),
-(2, 3, 'Hey, lorem ipsum bla-bla', '02-02-2022'),
-(3, 1, 'Rise and shine it''s a new day!', '02-02-2022'),
-(2, 1, 'We all learning here, it is ok', '02-02-2022'),
-(3, 3, 'To be or not to be, this is a question', '02-02-2022')
+VALUES (1, 2, 'What did one flower say to the other? What''s up bud?', '02-02-2022'),
+(2, 3, 'What kind of flower grows on your face? Tulips!', '02-02-2022'),
+(3, 1, 'What did the bee say to the flower? Hello honey!', '02-15-2022'),
+(4, 2, 'What did the flower study in college? STEM!', '02-19-2022'),
+(5, 3, 'Why did the tree take a nap? For rest!', '01-02-2022'),
+(6, 1, 'Why did the leaf go to the doctor? It was feeling green!', '02-10-2022'),
+(1, 3, 'How do plants make themselves heard? With amp-leaf-ication!', '02-08-2022'),
+(2, 2, 'How do plants contact each other? They use the te-leaf-one!', '02-03-2022'),
+(3, 1, 'Knock knock. Who''s there? Leaf.Leaf who? Leaf me alone!', '02-20-2022'),
+(4, 1, 'What kind of tree can you hold in your hand? A palm tree!', '02-17-2022'),
+(5, 2, 'Someone has been adding soil to my pots. The plot thickens.', '02-13-2022'),
+(6, 3, 'Guys...don''t kale my vibe!', '02-08-2022')
 ;
