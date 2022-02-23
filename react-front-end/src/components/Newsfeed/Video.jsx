210ef7396a1fd601b19b8b09e2cdbe0955863717
Julia <julia.sarandi@gmail.com>
2022-02-23 06:29:44 -0800

    Add one more embeded video

--- react-front-end/src/components/Newsfeed/Video.jsx
+++ react-front-end/src/components/Newsfeed/Video.jsx
@@ -4,8 +4,11 @@ import { Segment, Embed, Header } from "semantic-ui-react";
 export default function Video() {
   return (
     <Segment raised inverted color="olive" className="video">
-      <Header>Plant Video of the Week</Header>
+      <Header>Plant Care Video of The Week</Header>
       <Embed id="FvmkXyboQYI" placeholder="../images/posts/preview.png" source="youtube" />
+
+      <Header>Plant DIY Video of The Week</Header>
+      <Embed id="YYIyQyGgCvs" placeholder="../images/posts/preview_1.png" source="youtube" />
     </Segment>
   );
 }
