7fb4cee04ece30e97ae993d527b1c452f280b256
Julia <julia.sarandi@gmail.com>
2022-02-23 18:40:10 -0800

    Change video of the week to white

--- react-front-end/src/components/Newsfeed/Video.jsx
+++ react-front-end/src/components/Newsfeed/Video.jsx
@@ -3,11 +3,20 @@ import { Segment, Embed, Header } from 'semantic-ui-react';
 
 export default function Video() {
   return (
-    <Segment raised color="olive" className="video" style={{ backgroundColor: "rgba(225, 205, 48, 0.40)", textShadow: "2px 2px 2px #325036", backgroundImage: "url(https://www.transparenttextures.com/patterns/asfalt-light.png)" }} >
-      <Header style={{ color: "white" }}>Plant Video of the Week</Header>
+    <Segment
+      raised
+      color="olive"
+      className="video"
+      style={{
+        backgroundColor: 'rgba(225, 205, 48, 0.40)',
+        textShadow: '2px 2px 2px #325036',
+        backgroundImage: 'url(https://www.transparenttextures.com/patterns/asfalt-light.png)',
+      }}
+    >
+      <Header style={{ color: 'white' }}>Plant Video of the Week</Header>
       <Embed id="FvmkXyboQYI" placeholder="../images/posts/preview.png" source="youtube" />
 
-      <Header>Plant DIY Video of The Week</Header>
+      <Header style={{ color: 'white' }}>Plant DIY Video of The Week</Header>
       <Embed id="YYIyQyGgCvs" placeholder="../images/posts/preview_1.png" source="youtube" />
     </Segment>
   );
