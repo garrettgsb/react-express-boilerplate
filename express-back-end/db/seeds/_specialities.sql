e3ea35b600b9455a70136727c1d7244685d0d372
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-03-18 17:52:18 -0700

    create specialities seed

---
+++ express-back-end/db/seeds/_specialities.sql
@@ -0,0 +1,19 @@
+-- Specialties table --
+
+INSERT INTO specialties (name) VALUES
+('Addiction'),
+('ADHD'),
+('Anxiety'),
+('bipolar Disorder'),
+('Depression'),
+('Domestic Abuse'),
+('Eating Disorders'),
+('Family Conflict'),
+('Parenting'),
+('Self Esteem'),
+('Self Harming'),
+('Sleep or insomnia'),
+('Stress'),
+('Suicidal Ideation'),
+('Trauma and PTSD');
+
