5202369ab33ef579479b82268f13c2306ef9cb93
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-03-18 21:43:40 -0700

    create database connection with pg

---
+++ express-back-end/lib/db.js
@@ -0,0 +1,18 @@
+//PG database client/connection setup
+const { Pool } = require("pg");
+
+const dbParams = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+const db = new Pool(dbParams);
+
+db.connect(() => {
+  console.log("database is connected");
+});
+
+module.exports = db;
