46a1b9600fc90f9999b1b01642c60772b6fcb7a1
Lindsay <lbtannahill@gmail.com>
2022-03-18 20:45:39 -0700

    added appointments and professionals_specialties data

---
+++ express-back-end/db/seeds/05_professionals_specialties.sql
@@ -0,0 +1,12 @@
+-- professional specialties table -- 
+
+insert into professionals_specialties (id, professionals_id, specialties_id,) values (1, 20, 2);
+insert into professionals_specialties (id, professionals_id, specialties_id,) values (2, 25, 2);
+insert into professionals_specialties (id, professionals_id, specialties_id,) values (3, 02, 3);
+insert into professionals_specialties (id, professionals_id, specialties_id,) values (4, 04, 4);
+insert into professionals_specialties (id, professionals_id, specialties_id,) values (5, 05, 4);
+insert into professionals_specialties (id, professionals_id, specialties_id,) values (6, 34, 5);
+insert into professionals_specialties (id, professionals_id, specialties_id,) values (7, 22, 6);
+insert into professionals_specialties (id, professionals_id, specialties_id,) values (8, 01, 7);
+insert into professionals_specialties (id, professionals_id, specialties_id,) values (9, 04, 7);
+
