53bf61143dc53738bb9ff12e410fc1791054a73f
Alvin <alvin.tol@hotmail.com>
2022-05-13 17:58:51 -0600

    adds visual modes to Main.js

--- react-front-end/src/component/mainView/Main.js
+++ react-front-end/src/component/mainView/Main.js
@@ -4,26 +4,25 @@ import BotNav from './BotNav';
 import Savings from './Savings';
 import Spending from './Spending';
 import Profile from './Profile';
+import useVisualMode from '../../hooks/useVisualMode';
 import '../../sass/main.scss';
 
-export default function Main() {
+export default function Main(props) {
 
-  
-  // const PROFILE = 'PROFILE';
-  // const SAVINGS = 'SAVINGS';
-  // const SPENDING = 'SPENDING';
+  const PROFILE = 'PROFILE';
+  const SAVINGS = 'SAVINGS';
+  const SPENDING = 'SPENDING';
+
+  const { mode, transition, back } = useVisualMode(SAVINGS);
 
   return (
     <div>
       <TopNav />
 
-      <Profile />
-      <Savings />
-      <Spending />
+      {mode === PROFILE && <Profile />}
+      {mode === SAVINGS && <Savings />}
+      {mode === SPENDING && <Spending />}
 
-        <h1>
-          Main
-        </h1>
       <BotNav />
     </div>
   );
