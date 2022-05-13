a65e6fd2030c064fb9737503e8f2a3891ae5c0a3
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-05-12 19:40:29 -0700

    make a component folder and add login, main, signup, spending, saving components into it

---
+++ react-front-end/src/component/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Login() {
+
+  return (
+    <h1>
+      Login
+    </h1>
+  );
+}
---
+++ react-front-end/src/component/Spending.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Spending() {
+
+  return (
+    <h1>
+      Spending
+    </h1>
+  );
+}
