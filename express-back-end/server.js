e5e68e3418a2fd210c5780371b4692381d4e152b
Alvin <alvin.tol@hotmail.com>
2022-05-12 20:17:20 -0600

    adds sass scripts and watch compiler

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -2,11 +2,20 @@ const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
+const sassMiddleware = require('../react-front-end/lib/sass-middleware');
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
+app.use(
+  "/styles",
+  sassMiddleware({
+    source: __dirname + "/styles",
+    destination: __dirname + "/public/styles",
+    isSass: false, // false => scss, true => sass
+  })
+);
 
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
