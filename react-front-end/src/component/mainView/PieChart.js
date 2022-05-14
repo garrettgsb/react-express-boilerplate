2d898845679970f2dc73e903966167c18c719b45
Alvin <alvin.tol@hotmail.com>
2022-05-13 18:08:08 -0600

    adds visual routes in savings page to charts

---
+++ react-front-end/src/component/mainView/PieChart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function PieGraph() {
+
+  return (
+    <div>
+      
+    <h1>
+      Savings
+    </h1>
+    </div>
+  );
+}
--- react-front-end/src/component/mainView/Savings.js
+++ react-front-end/src/component/mainView/Savings.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import PieGraph from './PieChart';
+import LineGraph from './LineGraph';
+import useVisualMode from '../../hooks/useVisualMode';
 
 export default function Savings() {
 
+  const PIE = 'PIE';
+  const LINE = 'LINE';
+
+  const { mode, transition, back } = useVisualMode(PIE);
+
   return (
+    <div>
+    
+    {mode === PIE && <PieChart />}
+    {mode === LINE && <LineGraph />}
+
     <h1>
       Savings
     </h1>
+    </div>
   );
 }
