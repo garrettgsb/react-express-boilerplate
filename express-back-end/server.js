06fe8a28f47b703fd16d60d6524801c7f73e0bd8
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-05-15 11:54:34 -0700

    delete sample route

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -37,13 +37,6 @@ App.use('/api', goals(db));
 App.use('/api', categories(db));
 App.use('/api', incomes(db));
 
-// Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
-
-
-
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
