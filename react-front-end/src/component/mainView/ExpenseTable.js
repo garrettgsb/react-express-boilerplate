1b5ecb5e7e6c40ef3ef7efa9b4f3f898e38d5538
Alvin <alvin.tol@hotmail.com>
2022-05-16 09:57:23 -0600

    adds dollar amount, categories & income input fields in expenses page

--- react-front-end/src/component/mainView/ExpenseTable.js
+++ react-front-end/src/component/mainView/ExpenseTable.js
@@ -8,6 +8,7 @@ export default function ExpenseTable(props) {
         <tr>
           <th scope="col">Date</th>
           <th scope="col">Category</th>
+          <th scope="col">DESCRIPTION(OPTIONAL)</th>
           <th scope="col">Amount</th>
         </tr>
       </thead>
@@ -15,16 +16,19 @@ export default function ExpenseTable(props) {
         <tr className="table-success">
           <th scope="row">DATE</th>
           <td>INCOME</td>
+          <td>DESCRIPTION</td>
           <td>$AMOUNT</td>
         </tr>
         <tr className="table-danger">
           <th scope="row">DATE</th>
           <td>EXPENSES</td>
+          <td>DESCRIPTION</td>
           <td>$AMOUNT</td>
         </tr>
         <tr className="table-warning">
           <th scope="row">DATE</th>
           <td>SAVINGS</td>
+          <td>DESCRIPTION</td>
           <td>$AMOUNT</td>
         </tr>
       </tbody>
--- react-front-end/src/component/mainView/Expenses.js
+++ react-front-end/src/component/mainView/Expenses.js
@@ -15,38 +15,86 @@ export default function Expenses(props) {
     <div>
       {mode === LINE && <LineGraph />}
       {mode === EXPENSES &&
-        
+
         <div id='user-expense-input'>
           <ExpenseTable />
-          <div className="dropdown">
-            <button
-              className="btn btn-primary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-mdb-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Dropdown button
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <li><a className="dropdown-item" href="/">Clothes</a></li>
-              <li><a className="dropdown-item" href="/">Entertainment</a></li>
-              <li><a className="dropdown-item" href="/">Groceries</a></li>
-              <li><a className="dropdown-item" href="/">Rent</a></li>
-              <li><a className="dropdown-item" href="/">Insurance</a></li>
-              <li><a className="dropdown-item" href="/">Fuel</a></li>
-              <li><a className="dropdown-item" href="/">Eating Out</a></li>
-              <li><a className="dropdown-item" href="/">Gifts</a></li>
-              <li><a className="dropdown-item" href="/">Donations</a></li>
-              <li><a className="dropdown-item" href="/">Shopping</a></li>
-              <li><a className="dropdown-item" href="/">Other</a></li>
-            </ul>
-            <div className="form-outline">
-              <input type="number" id="typeNumber" className="form-control" />
-              <label className="form-label" for="typeNumber">Amount</label>
-            </div>
+          <div>
+
+
+            <form class="row row-cols-lg-auto g-3 align-items-center">
+              <div class="col-12">
+                <label class="visually-hidden" for="inlineFormInputGroupUsername">Amount</label>
+                <div class="input-group">
+                  <div class="input-group-text">$</div>
+                  <input
+                    type="number"
+                    imputmode='decimal'
+                    min='0.01'
+                    step='0.01'
+                    class="form-control"
+                    id="inlineFormInputGroupUsername"
+                    placeholder="Amount" />
+                </div>
+              </div>
+
+              <div class="col-12">
+                <label class="visually-hidden" for="inlineFormSelectPref">Category</label>
+                <select class="select">
+                  <option value="1">Clothes</option>
+                  <option value="2">Eating Out</option>
+                  <option value="3">Entertainment</option>
+                  <option value="4">Fuel</option>
+                  <option value="5">Gifts</option>
+                  <option value="6">Groceries</option>
+                  <option value="7">Insurance</option>
+                  <option value="8">Rent</option>
+                  <option value="9">Shopping</option>
+                  <option value="10">Other</option>
+                </select>
+              </div>
+
+              <div class="col-12">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="inlineFormCheck" />
+                  <label class="form-check-label" for="inlineFormCheck">
+                    Income
+                  </label>
+                </div>
+              </div>
+
+              <div class="col-12">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+
+            </form>
+            {/* <select class="form-select">
+              <option selected>Default Form Select</option>
+              <option value="1">Web Designing</option>
+              <option value="2">Web Development</option>
+              <option value="3">Graphic Design</option>
+              <option value="4">WordPress Website</option>
+            </select> */}
+
+            {/* <div className="dropdown">
+              <button
+                className="btn btn-primary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false">
+                  Category
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+              </ul>
+              <div className="form-outline">
+                <input type="number" id="typeNumber" className="form-control" />
+                <label className="form-label" for="typeNumber">Amount</label>
+              </div>
+            </div> */}
           </div>
-        </div>}
+        </div>
+      }
     </div>
   );
 }
