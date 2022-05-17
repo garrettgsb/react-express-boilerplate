fe0dd1676f2892b20544e41fce95f5b1ab231f31
Alvin <alvin.tol@hotmail.com>
2022-05-17 13:56:25 -0600

    adds classnames dependancy

--- react-front-end/src/component/mainView/SingleExpense.js
+++ react-front-end/src/component/mainView/SingleExpense.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import classNames from 'classnames';
 export default function SingleExpense(props) {
 
+
+  const tableClass = classNames('d-flex justify-content-around', {
+    'table-warning': props.savings,
+    'table-danger': props.expense,
+    'table-success': props.success
+  });
+
   return (
-    <tr className="table-success d-flex justify-content-around">
-          <td>{props.created_at}</td>
-          <td>{props.category_id}</td>
-          <td>{'$' + (props.amount / 100).toFixed(2)}</td>
-        </tr>
+    <tr className={tableClass}>
+      <td>{props.created_at}</td>
+      <td>{props.category_id}</td>
+      <td>{'$' + (props.amount / 100).toFixed(2)}</td>
+    </tr>
   )
 };
