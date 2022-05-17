39939e9aea5138377887870da7dfd26b317e6714
Alvin <alvin.tol@hotmail.com>
2022-05-17 17:12:41 -0600

    adds socket.io-client

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -4,17 +4,32 @@ const BodyParser = require('body-parser');
 const PORT = 8081;
 const sassMiddleware = require('./lib/sass-middleware');
 const cors = require('cors');
+const http = require('http');
+const socketio = require('socket.io');
 
 const { Pool } = require('pg');
 require('dotenv').config();
 const dbParams = require("./lib/db");
 const db = new Pool(dbParams);
+const server = http.createServer(App);
+const io = socketio(server);
 db.connect();
 
 const users = require('./src/routes/users');
 const expenses = require('./src/routes/expenses');
 const goals = require('./src/routes/goals');
 const categories = require('./src/routes/categories');
+
+io.on("connection", socket => {
+  console.log('Socket connected.')
+  socket.onmessage = event => {
+    console.log(`Message Received: ${event.data}`);
+
+    if (event.data === "ping") {
+      socket.send(JSON.stringify("pong"));
+    }
+  };
+});
 
 App.use(cors());
 // Express Configuration
