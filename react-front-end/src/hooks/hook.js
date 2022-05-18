084efb01ab51e4f8e448cdcd0f526b5eeed1fabd
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-05-17 21:39:28 -0700

    replace hard coded date with props

--- react-front-end/src/hooks/hook.js
+++ react-front-end/src/hooks/hook.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, /*useRef*/ } from "react";
 import axios from "axios"
 
 export default function useApplicationData() {
