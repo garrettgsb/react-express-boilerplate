2a3fa706f40355b5203b38d42bbfec64248b4ada
Kevin Keonhee Lee <blackseeds10@hotmail.com>
2022-05-18 20:35:34 -0700

    make variables to get money till goal per day/week/month/year

--- react-front-end/src/component/mainView/Savings.js
+++ react-front-end/src/component/mainView/Savings.js
@@ -3,13 +3,22 @@ import ProgressCircle from './ProgressCircle';
 import { getTotalAmount, getDaysTillGoal } from '../../helpers/helper_functions';
 
 export default function Savings(props) {
+  // gets total amount of saved money
   const savingsbyID = props.savings.filter((savings) => savings.user_id === props.userId);
   const savingsByCatId = savingsbyID.filter((categories) => categories.category_id === 8);
   const totalSaved = getTotalAmount(savingsByCatId);
   
+  // gets days until end date for goal
   const goalByID = props.goals.filter((goal) => goal.user_id === props.userId);
   const totalGoal = getTotalAmount(goalByID);
   const totalDaysTillGoal = getDaysTillGoal(goalByID);
+
+  // gets money per day/week/month/year
+  const moneyTillGoal = totalGoal - totalSaved;
+  const moneyPerDayToGoal = '$' + (moneyTillGoal / totalDaysTillGoal / 100).toFixed(2);
+  const moneyPerWeekToGoal = '$' + (moneyTillGoal / (totalDaysTillGoal / 7) / 100).toFixed(2);
+  const moneyPerMonthToGoal = '$' + (moneyTillGoal / (totalDaysTillGoal / 31) / 100).toFixed(2);
+  const moneyPerYearToGoal = '$' + (moneyTillGoal / (totalDaysTillGoal / 365) / 100).toFixed(2);
 
   return (
     <div>
@@ -36,6 +45,26 @@ export default function Savings(props) {
             <tr>
               <td>
                 {totalDaysTillGoal} days until {goalByID[0].goal_name}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                {moneyPerDayToGoal} / day until you reach your goal by {goalByID[0].end_date}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                {moneyPerWeekToGoal} / day until you reach your goal by {goalByID[0].end_date}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                {moneyPerMonthToGoal} / day until you reach your goal by {goalByID[0].end_date}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                {moneyPerYearToGoal} / day until you reach your goal by {goalByID[0].end_date}
               </td>
             </tr>
             </thead>
