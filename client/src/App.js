1f6800c53ffc9b2c563039efe979471584879f70
MattLawson98 <matthewlawson50@gmail.com>
2022-05-19 10:39:55 -0700

    Changed search api code to send results to components for showing

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,16 @@ import './App.scss';
 import Header from './components/Header';
 import { CssBaseline } from '@mui/material';
 import Crawls from './components/Crawls';
-// import Venue from './components/Venue';
+import Venue from './components/Venue';
+
 
 export default function App() {
 
   const [message ] = useState('Click the button to load data!');
-  
+  const [searchResults, setSearchResults] = useState([]);
+  const [bar,  setBar] = useState({});
+  const [photo, setPhoto] = useState({});
+ 
   // const fetchData = () => { 
   //     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
   //     .then((response) => {
@@ -19,9 +23,12 @@ export default function App() {
   //       setMessage(response.data.message);
   //   })
   // }
+  
+  
+  let result = [];
 
   const Search = () => {
-    
+
     const options = {
       method: 'GET',
       url: 'https://google-maps28.p.rapidapi.com/maps/api/place/nearbysearch/json',
@@ -36,22 +43,81 @@ export default function App() {
         'X-RapidAPI-Key': `${process.env.REACT_APP_RapidAPI_Key}`
       }
     };
+
     
     axios.request(options).then(function (response) {
-      console.log(response.data.results);
+      setSearchResults(response.data.results);
+            
     }).catch(function (error) {
       console.error(error);
     });
+    
+    return result;
   }
   
+
+
+  const RandNum= () => {
+    let max = searchResults.length ;
+    // console.log(max);
+    let venueNum = Math.floor((Math.random() * max));
+    // console.log(venueNum);
+    console.log(searchResults[venueNum])
+    return venueNum;
+  }
+
+
+
+  function click() {
+  // console.log(Search()[0].name)
+  // console.log(bar);
+  // console.log(bar2);
+  setBar(searchResults[RandNum()]);
+  console.log(bar);
+  console.log(grabPhoto());
+  console.log(photo);
+  console.log(bar.name);
+  }
+
+  function grabPhoto() {
+    const options = {
+      method: 'GET',
+      url: 'https://google-maps28.p.rapidapi.com/maps/api/place/photo',
+      params: {
+        photo_reference: "",
+        maxwidth: '400',
+        maxheight: '400'
+      },
+      headers: {
+        'X-RapidAPI-Host': 'google-maps28.p.rapidapi.com',
+        'X-RapidAPI-Key': '2f8033e889mshb4c8ca74d55c336p1769fcjsna7c55af16b77'
+      }
+    };
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      setPhoto(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+    
+  }
+    
     return (
-      
         <div className="App">
         <CssBaseline />
          <Header />
-         {/* <Venue /> */}
+         <Venue 
+          bar={bar}
+         />
          <h1>{ message }</h1>
+         <button onClick={click} >
+           Check
+         </button> 
          <button onClick={Search} >
+           Where are you from?
+         </button>  
+         <button onClick={RandNum} >
            Hop
          </button>    
          <Crawls />
--- client/src/components/Venue.jsx
+++ client/src/components/Venue.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import "./Venue.scss";
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 
-export default function Venue(props) {
+
+export default function Venue({bar}) {
+  // console.log(bar);
   return (
     <main className="venue">
       <div className="venue--img">
-        IMAGE
-      </div>
+        {bar.image}
+      </div> 
+      
       <div className="venue--name">
-        Brassneck Brewery
+        {bar.name}
       </div>
       <div className="data">
         <div className="data--values">
-          <div className="data--values--item">Rating</div>
-          <div className="data--values--item">Rtng-Amt</div>
-          <div className="data--values--item">Price</div>
-          <div className="data--values--item">My-Rtng</div>
+          <div className="data--values--item">{bar.rating}</div>
+          <div className="data--values--item">{bar.user_ratings_total}</div>
+          <div className="data--values--item">{bar.price_level}</div>
+          {/* <div className="data--values--item">My-Rtng</div> */}
         </div>
-        <div className="data--description"> Insert description here!
+        <div className="data--description"> 
+        <a href={`https://www.google.com/maps/search/?api=1&query=<address>&query_place_id=${bar.place_id}`} target="_blank" rel="noreferrer noopener" >
+          <MapOutlinedIcon >
+          </MapOutlinedIcon>
+        </a>
         </div>
       </div>
     </main>
