7349d0db2c29cf1cb7e389f3e94352763788f3de
Alvin <alvin.tol@hotmail.com>
2022-05-19 23:23:38 -0600

    adds dark background while on new input card

--- react-front-end/src/component/mainView/Expenses.js
+++ react-front-end/src/component/mainView/Expenses.js
@@ -17,9 +17,17 @@ export default function Expenses(props) {
 
 	const expenseInput = classNames('vw-50  align-items-center', {
 		'disappear': state.input === 'disappear',
-		'card': state.input !== 'disappear',
+		'card': state.input !== 'disappear'
 	});
+	
 
+
+
+
+	const blur = classNames('', {
+		'blur': state.input !== 'disappear'
+	});
+	
 	const LINE = 'LINE';
 	const EXPENSES = 'EXPENSES';
 
@@ -37,7 +45,7 @@ export default function Expenses(props) {
 	};
 
 	return (
-		<div>
+		<div id='expenses-page'>
 			{mode === LINE && (
 				<LineGraph
 					key={props.userId}
@@ -52,109 +60,112 @@ export default function Expenses(props) {
 				<div id="user-expense-input">
 					<ExpenseTable key={props.expenses.length} expenses={props.expenses} />
 					<div id='input-card' className={expenseInput}>
-								<form className="d-flex justify-content-around row row-cols-lg-auto g-3 align-items-center p-3">
-									<div className="col-lg-3 col-sm-6">
-										<label htmlFor="date" className="visually-hidden">
-											date
-										</label>
-										<input
-											id="date"
-											className="form-control"
-											type="date"
-											value={state.date}
-											onChange={event =>
-												setState({ ...state, date: event.target.value })
-											}
-										/>
+						<form className="d-flex justify-content-around row row-cols-lg-auto g-3 align-items-center p-3">
+							<div className="col-lg-3 col-sm-6">
+								<label htmlFor="date" className="visually-hidden">
+									date
+								</label>
+								<input
+									id="date"
+									className="form-control"
+									type="date"
+									value={state.date}
+									onChange={event =>
+										setState({ ...state, date: event.target.value })
+									}
+								/>
 
-										<span id="dateSelected"></span>
-									</div>
-									<div className="col-12">
-										<label
-											className="visually-hidden"
-											htmlFor="inlineFormInputGroupUsername"
-										>
-											Amount
-										</label>
-										<div className="input-group">
-											<div className="input-group-text">$</div>
-											<input
-												type="number"
-												imputmode="decimal"
-												min="0.01"
-												step="0.01"
-												className="form-control"
-												id="inlineFormInputGroupUsername"
-												placeholder="Amount"
-												onChange={event =>
-													setState({
-														...state,
-														amount: event.target.value * 100,
-													})
-												}
-											/>
-										</div>
-									</div>
-									<div className="col-12">
-										<label
-											className="visually-hidden"
-											htmlFor="inlineFormSelectPref"
-										>
-											Category
-										</label>
-										<select
-											className="select"
-											value={state.category_id}
-											onChange={event => {
-												setState({
-													...state,
-													category_name: getCategoryName(event.target.value),
-													category_id: parseInt(event.target.value),
-												});
-											}}
-										>
-											<option value="0" disabled>
-												Category
-											</option>
-											<option value="1">Eating Out</option>
-											<option value="2">Entertainment</option>
-											<option value="3">Fuel</option>
-											<option value="4">Groceries</option>
-											<option value="5">Income</option>
-											<option value="6">Insurance</option>
-											<option value="7">Rent</option>
-											<option value="8">Savings</option>
-											<option value="9">Shopping</option>
-											<option value="10">Other</option>
-										</select>
-									</div>
-									<div className="col-12 d-flex align-items-center">
-										<button
-											type="submit"
-											className="btn btn-primary m-1"
-											onClick={e => {
-												e.preventDefault();
-												submit(
-													props.userId,
-													state.date,
-													state.amount,
-													state.category_id,
-													state.category_name
-												);
-											}}
-										>
-											Submit
-										</button>
-                    <button 
-                    onClick={(e) => {
-                      e.preventDefault()
-                      setState({...state, input: 'disappear'})} }
-                    className='btn btn-danger m-1'>
-                  Cancel
-                </button>
-									</div>
-								</form>
+								<span id="dateSelected"></span>
 							</div>
+							<div className="col-12">
+								<label
+									className="visually-hidden"
+									htmlFor="inlineFormInputGroupUsername"
+								>
+									Amount
+								</label>
+								<div className="input-group">
+									<div className="input-group-text">$</div>
+									<input
+										type="number"
+										imputmode="decimal"
+										min="0.01"
+										step="0.01"
+										className="form-control"
+										id="inlineFormInputGroupUsername"
+										placeholder="Amount"
+										onChange={event =>
+											setState({
+												...state,
+												amount: event.target.value * 100,
+											})
+										}
+									/>
+								</div>
+							</div>
+							<div className="col-12">
+								<label
+									className="visually-hidden"
+									htmlFor="inlineFormSelectPref"
+								>
+									Category
+								</label>
+								<select
+									className="select"
+									value={state.category_id}
+									onChange={event => {
+										setState({
+											...state,
+											category_name: getCategoryName(event.target.value),
+											category_id: parseInt(event.target.value),
+										});
+									}}
+								>
+									<option value="0" disabled>
+										Category
+									</option>
+									<option value="1">Eating Out</option>
+									<option value="2">Entertainment</option>
+									<option value="3">Fuel</option>
+									<option value="4">Groceries</option>
+									<option value="5">Income</option>
+									<option value="6">Insurance</option>
+									<option value="7">Rent</option>
+									<option value="8">Savings</option>
+									<option value="9">Shopping</option>
+									<option value="10">Other</option>
+								</select>
+							</div>
+							<div className="col-12 d-flex align-items-center">
+								<button
+									type="submit"
+									className="btn btn-primary m-1"
+									onClick={e => {
+										e.preventDefault();
+										submit(
+											props.userId,
+											state.date,
+											state.amount,
+											state.category_id,
+											state.category_name
+										);
+									}}
+								>
+									Submit
+								</button>
+								<button
+									onClick={(e) => {
+										e.preventDefault()
+										setState({ ...state, input: 'disappear' })
+									}}
+									className='btn btn-danger m-1'>
+									Cancel
+								</button>
+							</div>
+						</form>
+					</div>
+								<div className={blur}>
+								</div>
 					<div className="col-12">
 						<div className="d-flex column align-items-center justify-content-center text-center">
 							<div>
