abae27e799d6b8a66d72fb6582bbc0f240b4ef88
Dave <dave.w.mckinnon@gmail.com>
2022-05-20 18:08:30 -0700

    change styling

--- client/src/components/Venue.jsx
+++ client/src/components/Venue.jsx
@@ -5,24 +5,42 @@ import { BarChartSharp } from "@mui/icons-material";
 
 
 export default function Venue({bar}) {
-  console.log("hello")
-  console.log(bar);
+
+  const priceLevel = (bar) => {
+    if (bar === 1) {
+      return "$"
+    }
+    else if (bar === 2) {
+      return "$$"
+    }
+    else if (bar === 3) {
+      return "$$$"
+    }
+    else if (bar === 4) {
+      return "$$$$"
+    }
+    else {
+      return "N/A"
+    }
+  }
+
   return (
     
     <main className="venue">
       {console.log(bar)}
       <div className="venue--img">
-      <img src={`https://maps.googleapis.com/maps/api/place/photo?photoreference=${bar.photos[0].photo_reference}&sensor=false&maxheight=250&maxwidth=250&key=${process.env.REACT_APP_GOOGLE_PHOTO_KEY}`} alt="" />
+      <img src={`https://maps.googleapis.com/maps/api/place/photo?photoreference=${bar.photos[0].photo_reference}&sensor=false&maxheight=1000&maxwidth=1000&key=${process.env.REACT_APP_GOOGLE_PHOTO_KEY}`} alt="" />
       </div> 
       
       <div className="venue--name">
       {bar.name}
       </div>
-      <div className="data">
-        <div className="data--left"> 
+      <div className="data"> 
+        <div className="data--left">
+        <div className="data--values"><strong>Price:</strong>&nbsp;&nbsp;{priceLevel(bar.price_level)}</div> 
         <div className="data--values"><strong>Rating:</strong>&nbsp;&nbsp;{bar.rating}/5</div>
-        <div className="data--values"><strong>Total Reviews:</strong>&nbsp;&nbsp;{bar.user_ratings_total}</div>
-        <div className="data--values"><strong>Price:</strong>&nbsp;&nbsp;{bar.price_level}/4</div>
+        <div className="data--values--reviews"><strong>Total Reviews:</strong>&nbsp;&nbsp;{bar.user_ratings_total}</div>
+       
         </div>
         <div className="data--right"> 
         <a href={`https://www.google.com/maps/search/?api=1&query=<address>&query_place_id=${bar.place_id}`} target="_blank" rel="noreferrer noopener" >
