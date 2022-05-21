d4d4de0c7185b2f28139c755dd50c741dad0c42e
Alvin <alvin.tol@hotmail.com>
2022-05-20 22:33:51 -0600

    passes vacation data down to line graph component

--- react-front-end/src/component/mainView/Expenses.js
+++ react-front-end/src/component/mainView/Expenses.js
@@ -64,6 +64,7 @@ export default function Expenses(props) {
 					expenses={props.expenses}
 					back={back}
 					vacationMode={props.vacationMode}
+					vacationData={props.vacationData}
 				/>
 			)}
 			{mode === EXPENSES && (
