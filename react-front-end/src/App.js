9361088a58a29bbdc503c4839df4b7fd8a7d11f6
Dkell88 <DarrenKelly1188@gmail.com>
2022-06-09 22:53:56 -0700

    Set up basic socket. Convert class to function based react

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,45 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import './AudioPlayer'
+import AudioPLayer from './AudioPlayer';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!',
-      src: ''
-    }
-  }
+//Socket io client
+import socketIOClient from 'socket.io-client';
+const ENDPOINT = '/';
 
-  fetchData = () => {
+const App = () => {
+
+  const [state, setState] = useState({
+    message: 'Click the button to load data!',
+    src: 'https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3'
+  });
+
+  useEffect(() => {
+  console.log("TEST")
+  const socket = socketIOClient(ENDPOINT);
+  socket.on('connect', () => console.log("we have connected!"))
+  }, []);
+  
+  const fetchData = () => {
     axios.get('https://api.deezer.com/track/3135554') // You can simply make your requests to "/api/whatever you want"
     .then((response) => {
       // handle success
       console.log(response.data) // The entire response from the Rails API
 
       console.log(response.data.title) // Just the message
-      this.setState({
+      setState({
         message: response.data.title,
         src: response.data.preview
       });
     }) 
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-        <audio autoPlay name="media">
-          <source src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3" type="audio/mpeg" />
-          Your browser does not support MP3
-        </audio>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>{ state.message }</h1>
+      <button onClick={fetchData} >
+        Fetch Data
+      </button>        
+      <AudioPLayer src = {state.src}/>
+    </div>
+  );
 }
+
+
 
 export default App;
