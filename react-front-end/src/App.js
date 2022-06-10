8204543945b38a34599ab6e26acbc3aa129cd783
Charles Ngo <charlesngo37@gmail.com>
2022-06-09 20:37:25 -0700

    Adds audio player and deezer api query

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import './AudioPlayer'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: 'Click the button to load data!'
+      message: 'Click the button to load data!',
+      src: ''
     }
   }
 
   fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+    axios.get('https://api.deezer.com/track/3135554') // You can simply make your requests to "/api/whatever you want"
     .then((response) => {
       // handle success
       console.log(response.data) // The entire response from the Rails API
 
-      console.log(response.data.message) // Just the message
+      console.log(response.data.title) // Just the message
       this.setState({
-        message: response.data.message
+        message: response.data.title,
+        src: response.data.preview
       });
     }) 
   }
@@ -30,6 +33,10 @@ class App extends Component {
         <button onClick={this.fetchData} >
           Fetch Data
         </button>        
+        <audio autoPlay name="media">
+          <source src="https://cdns-preview-b.dzcdn.net/stream/c-b2e0166bba75a78251d6dca9c9c3b41a-7.mp3" type="audio/mpeg" />
+          Your browser does not support MP3
+        </audio>
       </div>
     );
   }
