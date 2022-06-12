9540ddb8a8c0facec32331d278ec04b6818568e0
Amy Martin <amymariemart@gmail.com>
2022-06-12 14:44:38 -0700

    Change wireframe names, add comments to server.js, remove ikea-name-generator

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,53 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const axios = require("axios");
+const bodyParser = require("body-parser");
 const PORT = 8080;
-const axios = require('axios')
-const { getToken, getPlaylist } = require('./helpers/spotify')
-const ikea = require('ikea-name-generator');
-require('dotenv').config()
+require("dotenv").config();
+
+// helper functions
+const { getToken, getPlaylist } = require("./helpers/spotify");
 
 // socket IO
-const socketio = require('socket.io');
-const http = require('http');
+const socketio = require("socket.io");
+const http = require("http");
 const server = http.createServer(app);
 const io = socketio(server);
 
-// Express Configuration
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-let token = ''
-getToken().then((res) => token = res.data.access_token)
-
-// sample GET route
-app.get('/api/data', (req, res) => {
-  getPlaylist(token)
-    .then(result => res.json({ tracks: result.data.tracks }))
-})
-
-server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
-});
-
-
+// global varibales
+let token = "";
 let users = [];
 
-//Socket listeners
-io.on('connection', socket => {
+// express configuration
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// retrieves authentication token from spotify
+getToken().then((res) => (token = res.data.access_token));
+
+app.get("/api/data", (req, res) => {
+  // retrieves playlist data from spotify
+  getPlaylist(token).then((result) => res.json({ tracks: result.data.tracks }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Express is listening on port ${PORT} 👍`);
+});
+
+// established socket connection
+io.on("connection", (socket) => {
   const user = socket.handshake.query.username;
   const roomId = socket.handshake.query.roomId;
-  console.log("User has connected ", user )
-  users.push(user)
-  console.log("users: ", users)
+
+  users.push(user);
   socket.join(roomId);
 
-  socket.on('Guess', guess => {
-    socket.to(roomId).emit('chat-messages', `${user}: ${guess}`)
-  })
+  console.log(`${user} has connected!`);
+  console.log("All Users: ", users);
 
-});  
+  socket.on("Guess", (guess) => {
+    socket.to(roomId).emit("chat-messages", `${user}: ${guess}`);
+  });
 
+  // disconnect user and removes them from users array
+  socket.on("disconnect", () => {
+    users = users.filter((name) => name !== user);
 
+    console.log(`${user} has diconnected!`);
+    console.log("All Users: ", users);
+  });
+});
