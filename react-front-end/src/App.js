5098b27350ffff57d96a2f36b332ad9aaed760e5
josemourinho333 <yoo.phil92@gmail.com>
2022-08-04 21:25:03 -0700

    Add json to browser

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -3,26 +3,23 @@ import axios from 'axios';
 import './App.css';
 
 const App = () => {
-  const [state, setState] = useState({
-    message: 'Click the button to load data!'
-  });
+  const [state, setState] = useState([]);
 
 
   const fetchData = () => {
     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
     .then((response) => {
       // handle success
-      console.log("response", response.data) // The entire response from the Rails API
+      console.log("response", response) // The entire response from the Rails API
 
-      console.log('msg', response.data.message) // Just the message
-      const message = response.data.message
-      setState({...state, message})
-    }) 
+      console.log('msg', response.data) // Just the message
+      const message = response.data
+      setState([...message])
+    }, [])
   }
 
   return (
     <div className="App">
-      <h1>{ state.message }</h1>
       <button onClick={fetchData} >
         Fetch Data
       </button>        
