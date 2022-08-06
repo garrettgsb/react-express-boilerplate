abb80f85d9e82722eda11c4da3f454f339cde555
josemourinho333 <yoo.phil92@gmail.com>
2022-08-06 08:50:50 -0700

    Update initial API call done in each render inside useEffect hook, [] dependency for now

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 const App = () => {
   const [state, setState] = useState({});
-
+  
   // promise chain for setting initial states
-  // needs to be inside useEffect() leter 
-  const fetchData = () => {
+  // Depency: Will likely depend on swiping state
+  useEffect(() => {
     Promise.all([
       axios.get('/api/users/1/all'),
       axios.get('/api/users/1'),
@@ -19,7 +19,7 @@ const App = () => {
       console.log('all', all);
       setState({...state, users: all[0].data, user: all[1].data, messages: all[2].data, likedBy: all[3].data, matches: all[4].data});
     }) 
-  }
+  }, []);
 
   const swipeUser = (toId, like) => {
     axios.post('/api/users/1/matchings', {toId, like})
@@ -33,8 +33,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <button onClick={fetchData} >
-        Fetch Data
+      <button>
+        unused button
       </button>        
       <button onClick={() => swipeUser(3, true)}> 
         Post Data       
