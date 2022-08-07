b4c6075605248f4e36cd1ed9fd86fcb33ee5017e
AVacAdok <avakuang8@gmail.com>
2022-08-07 18:28:25 +0000

    install tailwind to app

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -103,6 +103,10 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello World!
+      </h1>
+
       <button>
         unused button
       </button>        
