5c8794172c720ed5e48d83c60620eca26e95e0e6
AVacAdok <avakuang8@gmail.com>
2022-08-08 18:24:58 +0000

    add set state when input login info and set the cookie

--- react-front-end/src/components/login/login-form.js
+++ react-front-end/src/components/login/login-form.js
@@ -24,48 +24,69 @@ function LoginForm(props) {
       })
       .catch((error) => console.log('err:', error));
   };
-  /// End of login and signout stuff
 
   return (
-    <div className="flex justify-center items-center mt-20">
+    <div className="h-screen">
+      <div className="flex justify-center items-center mt-10 mb-10 ">
+        <img src="https://media.istockphoto.com/photos/loving-couple-on-the-seashore-happy-valentines-day-picture-id1290253065?k=20&m=1290253065&s=612x612&w=0&h=zz7C8qIsECPkmF0Tgo3EIOdQ91tbERXaOGuaSli3YLM=" className=" max-w-full h-auto rounded drop-shadow-xl" />
 
+        <form className="bg-white drop-shadow px-8 pt-6 pb-8 mb-4 mix-blend-normal overflow-visible">
+          <div className="form-inner">
+            <h1 className="text-center mb-5 font-bold text-xl mt-10">Keen</h1>
+            {/* error msg */}
+            <div className="mb-3 pt-3">
+              <input
+                onChange={(event) => setUsername(event.target.value)}
+                value={username}
+                type="text"
+                placeholder="Username/Email"
+                className="px-1 py-1 w-full placeholder-slate-700 bg-gray-50 rounded-sm border border-gray-300 sm:text-xs focus:ring-fuchsia-800 focus:border-fuchsia-800 dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fuchsia-800 dark:focus:border-fuchsia-800" />
 
-      <img src="https://media.istockphoto.com/photos/loving-couple-on-the-seashore-happy-valentines-day-picture-id1290253065?k=20&m=1290253065&s=612x612&w=0&h=zz7C8qIsECPkmF0Tgo3EIOdQ91tbERXaOGuaSli3YLM=" className=" max-w-full h-auto rounded-lg drop-shadow-2xl " />
+              <input
+                onChange={(event) => setPassword(event.target.value)}
+                value={password}
+                type="password"
+                placeholder="Password"
+                className="px-1 py-1 mt-3 w-full placeholder-slate-700 bg-gray-50 rounded-sm border border-gray-300 sm:text-xs focus:ring-fuchsia-800 focus:border-fuchsia-800 dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fuchsia-800 dark:focus:border-fuchsia-800" />
+            </div>
 
-      <form className="bg-white drop-shadow px-8 pt-6 pb-8 mb-4 mix-blend-normal overflow-visible">
-        <div className="form-inner">
-          <h1 className="text-center mb-5 font-bold text-xl mt-10">Keen</h1>
-          {/* error msg */}
-          <div className="mb-3 pt-3">
-            <input
-              onChange={(event) => setUsername(event.target.value)}
-              type="text"
-              placeholder="Username/Email"
-              className="px-1 py-1 w-full placeholder-slate-700 bg-gray-50 rounded-sm border border-gray-300 sm:text-xs focus:ring-fuchsia-800 focus:border-fuchsia-800 dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fuchsia-800 dark:focus:border-fuchsia-800" />
+            <div className="mb-3 pt-3">
+              <button onClick={handleClickLogIn} type="submit" className="px-1 py-1 w-full bg-fuchsia-800 hover:bg-fuchsia-900 text-white font-bold rounded-sm">
+                Log In
+              </button>
 
-            <input
-              onChange={(event) => setPassword(event.target.value)}
-              type="text"
-              placeholder="Password"
-              className="px-1 py-1 mt-3 w-full placeholder-slate-700 bg-gray-50 rounded-sm border border-gray-300 sm:text-xs focus:ring-fuchsia-800 focus:border-fuchsia-800 dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-fuchsia-800 dark:focus:border-fuchsia-800" />
-          </div>
+              <p className="sm:text-xs text-gray-600 text-right mt-3 border-b-2 pb-6">Forgot Password?</p>
 
-          <div className="mb-3 pt-3">
-            <button onClick={() => handleClickLogIn(username, password)} className="px-1 py-1 w-full bg-fuchsia-800 hover:bg-fuchsia-900 text-white font-bold rounded-sm">
-              Log In
-            </button>
+              <div className="flex mt-7 justify-center">
 
-            <p className="sm:text-xs text-gray-600 text-right mt-3 border-b-2 pb-6">Forgot Password?</p>
-
-            <div className="flex mt-7 justify-center">
-
-              <p className="sm:text-xs text-slate-900 pr-1">Don't have an account?</p>
-              <p className="sm:text-xs text-fuchsia-800">Sign up</p>
+                <p className="sm:text-xs text-slate-900 pr-1">Don't have an account?</p>
+                <p className="sm:text-xs text-fuchsia-800">Sign up</p>
+              </div>
             </div>
           </div>
+        </form>
+      </div>
+      <div >
+        <p className="text-l font-bold flex justify-center">
+          Created By
+        </p>
+
+        <div className="grid grid-rows-2 grid-flow-col">
+          <p>Ava K</p>
+          <div className="flex ">
+            <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="20" height="20"></img>
+            <a href="https://github.com/avacadok">avacadok</a>
+          </div>
+          
+          <p>Denis K</p>
+          <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="20" height="20"></img>
+          <p>Phil Y</p>
+          <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="20" height="20"></img>
         </div>
-      </form>
+      </div>
+
     </div>
+
   )
 }
 
