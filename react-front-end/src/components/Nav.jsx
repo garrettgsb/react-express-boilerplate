9d7bc1def0e52e22155e51d0d359a38557d63594
ukflava <ukflava@gmail.com>
2022-08-07 20:20:44 -0700

    dropdown for  navbar

--- react-front-end/src/components/Nav.jsx
+++ react-front-end/src/components/Nav.jsx
@@ -9,6 +9,7 @@ import {
 import Axios from "axios";
 
 export default function Nav() {
+  const [open, setOpen] = useState(false)
   
   return (
     <div className="grid content-center shadow-xl h-20 border-b big-white sticky top-0 z-50 ">
@@ -20,7 +21,33 @@ export default function Nav() {
         <BellIcon className="h-10 cursor-pointer rounded-full hover:text-fuchsia-700" />
         <UserCircleIcon className="h-10 cursor-pointer rounded-full hover:text-fuchsia-700" />
 
-        <button type="button" className="flex text-sm rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+
+
+        <button type="button" onClick={() =>{setOpen(!open)}}>
+        <img className="w-10 h-10 rounded-full" src="https://www.w3schools.com/howto/img_avatar.png" alt="user photo" />
+        </button>
+<div>{open ? (<div className="my-40 list-none shadow dark:bg-gray-700 dark:divide-gray-600" data-dropdown-placement="bottom">
+        <div className="py-3 px-4">
+          <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+          <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+        </div>
+        <ul className="py-1" aria-labelledby="user-menu-button">
+          <li>
+            <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+          </li>
+          <li>
+            <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+          </li>
+          <li>
+            <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+          </li>
+          <li>
+            <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+          </li>
+        </ul>
+      </div>) : (<p></p>)}</div>
+
+        {/* <button type="button" className="flex text-sm rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
         <span className="sr-only">Open user menu</span>
         <img className="w-10 h-10 rounded-full" src="https://www.w3schools.com/howto/img_avatar.png" alt="user photo" />
       </button>
@@ -44,7 +71,7 @@ export default function Nav() {
             <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
           </li>
         </ul>
-      </div>
+      </div> */}
 
 
 
