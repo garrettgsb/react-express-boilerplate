48a29891c2db7c6f14b3ad016955223cdee56f4d
ukflava <ukflava@gmail.com>
2022-08-07 22:31:14 -0700

    implement onBlur for menu

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -113,7 +113,7 @@ const App = () => {
 <>
     <header> <Nav state={state}/></header>
     
-    {/* <body>
+    <body>
     <div className="App">
       <h4 className="text-3xl font-bold underline">
         Hello World!
@@ -148,7 +148,7 @@ const App = () => {
       </div>
 
     </div>
-    </body> */}
+    </body>
     </>
   );
 }
--- react-front-end/src/components/Nav.jsx
+++ react-front-end/src/components/Nav.jsx
@@ -7,28 +7,34 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/outline";
 
+
 // color ------------------------- #8A00A0
+// Kept dropdown in this 
 
 export default function Nav(props) {
   const [open, setOpen] = useState(false)
-  const user = props.state.user[0]
+  const user = props.state.user[0] 
+  // const user = {}
   console.log(user)
   return (
-    <div className="grid content-center shadow-xl h-20 border-b big-white sticky top-0 z-50 ">
+  
+    <div onBlur={
+      console.log("dddddd")} className="grid content-center shadow-xl h-20 border-b big-white sticky top-0 z-50 ">
               
         <div className="flex justify-between content-center space-x-4">
         <div className="Vwhite-text text-4xl ml-10 myfont text-left cursor-pointer hover:text-[#8A00A0]">KEEN</div>
         <div className="flex space-x-6 mr-10">
         <HeartIcon className="h-10 cursor-pointer rounded-full shadow-sm hover:text-[#8A00A0]" />
         <BellIcon className="h-10 cursor-pointer rounded-full hover:text-[#8A00A0]" />
-        <UserCircleIcon className="h-10 cursor-pointer rounded-full hover:text-[#8A00A0]" />
+        <UserCircleIcon className="h-10 cursor-pointer rounded-full hover:text-[#8A00A0] " />
 
         <button className="dropdown-relative" type="button" onClick={() =>{setOpen(!open)}}>
         <img className="w-10 h-10 rounded-full" src={user.profile_photo} alt="user photo" />
         </button>
-<div>{open ? (<div className="fixed top-20 right-5  dropdown-toggle min-w-max items-right bg-white text-base z-50 float-right
-                        py-2 list-none text-down rounded-lg shadow-lg mt-1 mr-50 bg-clip-padding border-none">
-        <div className="py-3 px-4">
+
+<div>{open ? (<div  className="fixed top-20 right-5  dropdown-toggle min-w-max items-right bg-white text-base z-50 float-right
+                        py-2 list-none text-down rounded-lg shadow-lg mt-1 mr-50 bg-clip-padding border-none ">
+        <div className="py-3 px-4 ">
           <span className="block text-sm text-gray-900 dark:text-white">{user.name}</span>
           <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">{user.email}</span>
         </div>
