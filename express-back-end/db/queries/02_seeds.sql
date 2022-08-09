651dbb73de5a8055e02f4b7b08ab2b68ef16ec63
josemourinho333 <yoo.phil92@gmail.com>
2022-08-09 09:01:47 -0700

    Add update to drinks, exercises, dating goals table for values

--- express-back-end/db/queries/02_seeds.sql
+++ express-back-end/db/queries/02_seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (name, email, password, bio, age, gender_id, location, height_in_cm, education,
  occupation, drink_id, exercise_id, dating_goal_id, is_active)
 VALUES
-  ('Brennan Padilla','ectus.quis@icloud.com', '1111', 'lorem bio', 25, 1, 'Vancouver', 180 , 'high school', 'student', 1, 2, 1, true),
+  ('Brennan Padilla','ectus.quis@icloud.com', '1111', 'I have never seen a straight banana.', 25, 1, 'Vancouver', 180 , 'high school', 'student', 3, 3, 2, true),
   ('Belle Hendrick','quis@hotmail.couk', '1111', 'lorem bio', 30, 0, 'Toronto', 155 , 'University', 'Barista', 1, 2, 1, true),
   ('dfasdf Hendrick','fddfsdf@hotmail.couk', '1111', 'lorem bio', 22, 1, 'Vancouver', 145 , 'high school', 'student', 1, 2, 1, true),
   ('new person','new@hotmail.couk', '1111', 'lorem bio', 50, 1, 'Vancouver', 199 , 'high school', 'Plumber', 1, 2, 1, true),
@@ -44,15 +44,15 @@ VALUES
   
 INSERT INTO drinks (value) 
 VALUES 
-  ('Do Not Care'), ('No'), ('Sometimes'), ('Yes');
+  ('Never'), ('Sometimes'), ('Socially'), ('Yes');
 
 INSERT INTO exercises (value) 
 VALUES 
-  ('Do Not Care'), ('No'), ('Sometimes'), ('Yes');
+  ('Never'), ('Sometimes'), ('Often'), ('Everyday');
 
 INSERT INTO dating_goals (value) 
 VALUES 
-  ('Dont Know Yet'), ('Short Term'), ('Long Term'), ('Life Partner');
+  ('Not sure yet'), ('Short-term / Casual'), ('Long-term'), ('Life partner');
 
 
   INSERT INTO preferences 
