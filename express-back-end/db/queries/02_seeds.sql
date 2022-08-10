c699544ddc95765099ef4372ab5fa7b7a6b620b4
josemourinho333 <yoo.phil92@gmail.com>
2022-08-09 22:43:43 -0700

    Add updates to seeds

--- express-back-end/db/queries/02_seeds.sql
+++ express-back-end/db/queries/02_seeds.sql
@@ -26,9 +26,16 @@ VALUES
 
 INSERT INTO messages (from_user_id, to_user_id, message, message_seen, date_sent) 
 VALUES 
-(1, 2, 'hey from id.1 to id.2', false, CURRENT_TIMESTAMP),
-(2, 1, 'hey from id.2 to id.1', false, CURRENT_TIMESTAMP),
-(1, 2, 'hey again from id.1 to id.2', false, CURRENT_TIMESTAMP);
+(1, 2, 'Hey Id 2', false, CURRENT_TIMESTAMP),
+(2, 1, 'Hello id 1', false, CURRENT_TIMESTAMP),
+(1, 2, 'hey again from id.1 to id.2', false, CURRENT_TIMESTAMP),
+(2, 1, 'Hello again', false, CURRENT_TIMESTAMP),
+(1, 2, 'Hey Id 2', false, CURRENT_TIMESTAMP),
+(2, 1, 'Hello id 1', false, CURRENT_TIMESTAMP),
+(1, 2, 'hey again from id.1 to id.2', false, CURRENT_TIMESTAMP),
+(2, 1, 'Hello again', false, CURRENT_TIMESTAMP),
+(2, 3, 'Test msg to see fetch works', false, CURRENT_TIMESTAMP);
+
 
 INSERT INTO matchings (from_user_id, to_user_id, like_value, seen, matched_date) 
 VALUES 
