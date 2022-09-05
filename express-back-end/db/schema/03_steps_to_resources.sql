549f1fde5387f992ca9972f2f9248a63f2859ce5
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-09-05 06:18:14 +0000

    Remove the single files for the schema since there is one file containing all the tables needed

--- express-back-end/db/schema/03_steps_to_resources.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS steps_to_resources CASCADE;
-
-CREATE TABLE steps_to_resources (
-  id SERIAL PRIMARY KEY NOT NULL,
-  subjects_id INTEGER REFERENCES subjects(id) ON DELETE CASCADE,
-  resources_id INTEGER REFERENCES resources(id) ON DELETE CASCADE
-);
--- express-back-end/db/schema/05_progress_tracker.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS progress_tracker CASCADE;
-
-CREATE TABLE progress_tracker (
-  id SERIAL PRIMARY KEY NOT NULL,
-  users_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  resources_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
-  steps_complete BOOLEAN NOT NULL
-);
