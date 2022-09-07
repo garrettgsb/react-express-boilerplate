440647223255c6fa966be63c4937ef5c7d53132a
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-09-07 03:34:54 +0000

    Successfully connected to the database

---
+++ express-back-end/routes/homepage.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+  router.get('/', (req, res) =>{
+    console.log("Hello Home")
+
+    db.query(
+      'SELECT * FROM resources;'
+    ).then(result => {
+      res.send(result.rows)
+    })
+
+  
+  })
+
+  return router;
+}
