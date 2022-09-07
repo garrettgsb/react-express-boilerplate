867de72c7ea9a236b1b64d85e3929b73854597f7
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-09-07 00:56:03 +0000

    Create db.js file and update server.js for database client connection. Include a few routes as well in server.js

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,7 +1,14 @@
 const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+// PG database client/connection setup
+const { Pool } = require('pg');
+const dbParams = require('./db');
+const db = new Pool(dbParams);
+db.connect();
+
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
@@ -17,3 +24,13 @@ App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
+
+//Separated Routes for Each Resources
+const usersRoutes = require('./routes/users');
+const homepageRoutes = require('./routes/homepage');
+const categoriesRoutes = require('./routes/categories');
+
+//Mount all resource routes
+App.use('/home', homepageRoutes(db));
+App.use('./users/', usersRoutes(db));
+App.use('./categories/', categoriesRoutes(db));
