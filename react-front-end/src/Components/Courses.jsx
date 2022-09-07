4a19fa1deeaaa5007a8fe64a82e45f58363073fb
vagrant <eng.saad.najm@gmail.com>
2022-09-07 17:44:26 +0000

    adding homepage discriptions and side boxes

--- react-front-end/src/Components/Courses.jsx
+++ react-front-end/src/Components/Courses.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/courses.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import PageDescription from './PageDescription';
+import 'react-bootstrap'
+import data from '../data';
 
 export default function Courses(props) {
-  const newData = props.data.map((category) => {
+  const newDataLimit = props.data.map((category) => {
     return (
       <div>
-        <Link to={`/course/${category.name.toLowerCase()}`}>{ category.name }</Link>
+        <Link className='text-newDataLimit' to={`/course/${category.name.toLowerCase()}`}>{category.name}</Link>
 
       </div>
     );
   })
-  const newDataLimit = props.data.map((category) => {
+  const newData = props.data.map((category) => {
     return (
       <div>
-        <Link to={`/course/${category.name.toLowerCase()}`}>{ category.name }</Link>
+        <Link className='text-newData' to={`/course/${category.name.toLowerCase()}`}>{category.name}</Link>
       </div>
     );
   })
@@ -23,13 +29,21 @@ export default function Courses(props) {
   return (
     <div className='container'>
       <div className='row'>
-      <div className='col-1'>
-        {newDataLimit}
+
+        <Container className='container'>
+          <Row className='all-categories' >
+            <Col xs={2} className='border border-3 side-categories'>
+              {newDataLimit}
+            </Col>
+            <Col xs={10}>
+              <div className='row px-0 page-description'>
+                <PageDescription />
+              </div>
+              <div className='fav-categories'>{newData}</div>
+            </Col>
+          </Row>
+        </Container>
       </div>
-      </div>
-      <ul className='col'>
-        {/* {newData} */}
-      </ul>
     </div>
   );
 }
---
+++ react-front-end/src/Components/PageDescription.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageDescription() {
+  return (
+    <div>
+      <h2>Page Description Page Description Page Description Page Description Page Description </h2>
+    </div>
+  )
+}
+
+export default PageDescription
