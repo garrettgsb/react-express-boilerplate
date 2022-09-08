a7141d7bfc16317ef1bfbf149afab548a484acdc
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-09-08 00:10:50 +0000

    Create route for tracking a users progress

--- express-back-end/routes/progresstracker.js
+++ express-back-end/routes/progresstracker.js
@@ -8,5 +8,16 @@ module.exports = (db) => {
     });
   });
 
+  router.post("/progress", (req, res) => {
+    const params = [req.body['steps_complete']];
+
+    db.query(`UPDATE progress_tracker
+    SET steps_complete = $1`, params)
+    .then((data) => {
+      
+    })
+    .catch(error => res.status(500).json({error: error.message}));
+  })
+
   return router;
 };
