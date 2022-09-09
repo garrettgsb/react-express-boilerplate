3a05d7745d083d8d3fd99bd59335607ccb7d8414
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-09-09 01:34:25 +0000

    Increase amount for varchar

--- express-back-end/db/schema.sql
+++ express-back-end/db/schema.sql
@@ -6,26 +6,26 @@ DROP TABLE IF EXISTS steps_to_resources;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  first_name VARCHAR(1000) NOT NULL,
+  last_name VARCHAR(1000) NOT NULL,
+  email VARCHAR(1000) NOT NULL,
+  password VARCHAR(1000) NOT NULL,
   admin BOOLEAN NOT NULL
 );
 
 CREATE TABLE subjects (
   id SERIAL PRIMARY KEY NOT NULL,
-  subject_name VARCHAR(55) NOT NULL
+  subject_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE resources (
   id SERIAL PRIMARY KEY NOT NULL,
   subjects_id INTEGER REFERENCES subjects(id) ON DELETE CASCADE,
   step_number INTEGER NOT NULL,
-  step_description VARCHAR(255) NOT NULL,
-  article_url VARCHAR(255),
-  photo_url VARCHAR(255),
-  video_url VARCHAR(255)
+  step_description VARCHAR(1000) NOT NULL,
+  article_url VARCHAR(1000),
+  photo_url VARCHAR(1000),
+  video_url VARCHAR(1000)
 );
 
 CREATE TABLE progress_tracker (
