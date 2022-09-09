b79484045e8f6a2cc970b245e84f83de22647d5c
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-09-09 19:20:38 +0000

    Include a new query based off the changes in the schema and seeds

--- express-back-end/routes/progresstracker.js
+++ express-back-end/routes/progresstracker.js
@@ -16,15 +16,22 @@ module.exports = (db) => {
   //Once they finish every step they get a completion badge
   router.post("/progress", (req, res) => {
     console.log("server")
-    const params = [req.body.highest_steps]; //req.body from front-end
+    const params = [req.body.highest_steps];
+    //grab id from session cookie
+    const userId = [req.body.users_id];
+    const resourcesId = [req.body.resources_id];
 
-    db.query(`UPDATE progress_tracker
-    SET highest_steps = highest_steps + $1
-    WHERE users_id = 2`, params)
-    .then((data) => {
-      console.log(data);
-      res.status(200);
-    });
+    // db.query(`UPDATE progress_tracker
+    // SET highest_steps = highest_steps + $1
+    // WHERE users_id = 2`, params)
+    // .then((data) => {
+    //   console.log(data);
+    //   res.status(200);
+    // });
+    db.query(`
+    INSERT INTO progress_tracker (users_id, resources_id)
+    VALUES ($1, $2)
+    `, [])
   })
 
   return router;
