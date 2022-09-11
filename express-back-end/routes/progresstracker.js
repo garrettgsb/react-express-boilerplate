987d4b3542ab687615e4bd06d7262c18303018c3
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-09-11 00:25:11 +0000

    Tracking steps completed and returns alerts

--- express-back-end/routes/progresstracker.js
+++ express-back-end/routes/progresstracker.js
@@ -8,30 +8,26 @@ module.exports = (db) => {
     });
   });
 
-
-
-  //Ability for students to say I'm done that step
-  //Once students start learning path, highest step is 0
-  //Once they complete the step it will add to highest step
-  //Once they finish every step they get a completion badge
   router.post("/progress", (req, res) => {
-    console.log("server")
-    const params = [req.body.highest_steps];
-    //grab id from session cookie
-    const userId = [req.body.users_id];
-    const resourcesId = [req.body.resources_id];
 
-    // db.query(`UPDATE progress_tracker
-    // SET highest_steps = highest_steps + $1
-    // WHERE users_id = 2`, params)
-    // .then((data) => {
-    //   console.log(data);
-    //   res.status(200);
-    // });
+    const userId = 2;
+    const resourceId = req.body.resource_id;
+
     db.query(`
     INSERT INTO progress_tracker (users_id, resources_id)
     VALUES ($1, $2)
-    `, [])
+    `, [userId, resourceId])
+    .then((data) => {
+      res.status(200);
+    });
+
+    db.query(`
+    SELECT COUNT(resources_id) FROM progress_tracker;
+    `)
+    .then((data) => {
+      console.log("data.rows", data.rows)
+      res.send(data.rows);
+    });
   })
 
   return router;
