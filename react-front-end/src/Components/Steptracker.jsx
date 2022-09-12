7b816c422dfc7f4c4520426e91563634dfd2041c
vagrant <eng.saad.najm@gmail.com>
2022-09-12 21:10:31 +0000

    fixing step tracker message

--- react-front-end/src/Components/Steptracker.jsx
+++ react-front-end/src/Components/Steptracker.jsx
@@ -4,9 +4,9 @@ import axios from "axios";
 import { message, Alert } from "antd";
 import 'antd/dist/antd.css';
 import '../styles/button.css';
-export default function Steptracker(props) {
-  const [showAlert, setShowAlert] = useState(false)
 
+export default function Steptracker(props) {
+  // const [showAlert, setShowAlert] = useState(false)
   const handleProgress = () => {
     axios
       .post('/progresstracker/progress', {
@@ -16,7 +16,8 @@ export default function Steptracker(props) {
         if (res.data[0].count === '3') {
           message.success("YOU'RE ALMOST THERE. KEEP GOING!")
         } else if (res.data[0].count === '7'){
-          setShowAlert(true)
+          // setShowAlert(true)
+          message.success("HURRAY! YOU HAVE COMPLETED THIS LEARNING PATH!");
         }
 
         console.log('FROM BE', res.data[0].count)
@@ -26,7 +27,7 @@ export default function Steptracker(props) {
         console.log('err', err);
       })
   };
-  const [buttonText, setButtonText] = useState('Click Me');
+  const [buttonText, setButtonText] = useState('Complete Step');
   const updateText = (text) => setButtonText(text);
   return (
     <div>
@@ -34,16 +35,6 @@ export default function Steptracker(props) {
       updateText('Step Completed');
       handleProgress();
     }}>{buttonText}</button>
-
-     {showAlert &&
-      <Alert
-      message="HURRAY! 😄"
-      description="CONGRATS! YOU HAVE COMPLETED THIS LEARNING PATH!"
-      type="success"
-      showIcon
-      closable
-      />
-    }
     </div>
   )
 }
