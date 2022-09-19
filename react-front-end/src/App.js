c7ed35c3b93aec58f730c76c0d1a7591c414cdfb
mariannebourcier <marianne.bourcier@gmail.com>
2022-09-19 16:47:27 -0600

    Reorder css files

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
+import './index.css';
 import Profile from  './components/Profile.js'
 
 class App extends Component {
