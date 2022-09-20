6bede8f4b601bf9582feca2f1441fa99db1ee30d
Gloria <gloriacheung812@gmail.com>
2022-09-20 23:18:15 +0000

    remove axios fetch for now

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -12,23 +12,9 @@ import { Route, Link, Routes } from "react-router-dom";
 function App(props) {
   const [message, setMessage] = useState("");
 
-  const fetchData = () => {
-    axios
-      .get("/api/data") // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // handle success
-        console.log(response.data); // The entire response from the Rails API
-        console.log(response.data.message); // Just the message
-        setMessage(response.data.message);
-      });
-  };
-
   return (
     <div className="App">
       <TopBar />
-      // can take this out later
-      <h1>{message}</h1>
-      <button onClick={fetchData}>Fetch Data</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/homes_rent" element={<Homes_Rent />} />
