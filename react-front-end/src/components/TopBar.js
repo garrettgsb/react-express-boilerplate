1b44c45f446cd98f9936528e84584eb21b8bde09
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-09-19 20:26:47 -0600

    Update Navbar to render different components

--- react-front-end/src/components/TopBar.js
+++ react-front-end/src/components/TopBar.js
@@ -12,8 +12,10 @@ const TopBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/homes_sale">Buy</Nav.Link>
+            <Nav.Link href="/homes_rent">Rent</Nav.Link>
+            <Nav.Link href="/sell">Sell</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
