522efa51fd9ab4e69239000bfbc63d37427363f4
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-09-20 15:58:29 -0600

    Rename to PascalCase

--- react-front-end/src/components/Homes_Rent.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Homes_Rent() {
-  return <div>THIS IS THE HOMES_RENT ROUTE</div>;
-}
--- react-front-end/src/components/Homes_Sale.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Homes_Sale() {
-  return <div>THIS IS THE HOMES_SALE ROUTE</div>;
-}
