469b0302515a7aa6ef8cf19e78487c38925dc194
mariannebourcier <marianne.bourcier@gmail.com>
2022-09-21 10:51:30 -0600

    Create hook for run data

---
+++ react-front-end/src/hooks/useAppData.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+//hook for getting runs
+//hook for logged in user
+//hook for runner only runs
+//hook for planner runs
+//axios route change based on user id
+
+
+
+export default function useAppData() {
+
+  const [runs, setRuns] = useState({});
+
+  const [users, setUsers] = useState({});
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/runs')
+    ]).then((response) => {
+      //console.log(response);
+      const runs = response[0].data;
+      console.log(runs);
+       setRuns({runs});
+    })
+    .catch((error) => {
+      console.log(error.response.status);
+    })
+  }, [])
+
+
+
+return {
+  runs
+}
+
+}
+
+
+
+  
