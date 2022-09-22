474904c75211537f90a667d258075db10a025a14
Gloria <gloriacheung812@gmail.com>
2022-09-22 23:12:54 +0000

    move functions from searchbar to homerent so that data can be passed down to listing container

--- react-front-end/src/components/HomesRent.js
+++ react-front-end/src/components/HomesRent.js
@@ -1,15 +1,61 @@
 import React from "react";
+import { useState } from "react";
 import SearchBar from "./SearchBar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import ListingContainer from "./ListingContainer";
 import "./Homes_Rent.scss";
+import axios from "axios";
 
 export default function Homes_Rent() {
+  const [location, setLocation] = useState("");
+  const [beds, setBeds] = useState("0");
+  const [baths, setBaths] = useState("0");
+  const [maxPrice, setMaxPrice] = useState("10000");
+  const [data, setData] = useState({});
+
+  const fetchData = () => {
+    const options = {
+      method: "GET",
+      url: "https://zillow56.p.rapidapi.com/search",
+      params: {
+        location: location,
+        beds_min: beds,
+        baths_min: baths,
+        price_max: maxPrice,
+        status: "forRent",
+      },
+      headers: {
+        "X-RapidAPI-Key": process.env.API_KEY,
+        "X-RapidAPI-Host": "zillow56.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then((res) => {
+        setData(res.data.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
   return (
     <div className="rent">
-      <SearchBar></SearchBar>
+      <SearchBar
+        submitHandler={submitHandler}
+        setBaths={setBaths}
+        setBeds={setBeds}
+        setLocation={setLocation}
+        setMaxPrice={setMaxPrice}
+      />
       <Row className="p-0 m-0 border-top">
         <Col className="mapContainer">
           <Image
@@ -17,7 +63,7 @@ export default function Homes_Rent() {
             className="w-100"
           />
         </Col>
-        <ListingContainer />
+        <ListingContainer data={data} />
       </Row>
     </div>
   );
