86a9f217b0c2b02e946c0db16c829be4d2a08e91
mariannebourcier <marianne.bourcier@gmail.com>
2022-09-22 15:39:07 -0600

    Display logo to homepage

--- react-front-end/src/components/Homepage.jsx
+++ react-front-end/src/components/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../components/Homepage.css";
+import logo from "./logo.png";
 
 export default function Homepage() {
   
   return (
     <div>
       <div className="home-intro">
+      <img src={logo} className="logo" alt="logo" />
         <p>Welcome to WeRun</p>
         <p>We help you get connected to runs near you.</p>
       </div>
