b46412b4b17e763a3ab5cae67584238d15b60186
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-09-21 23:55:15 -0600

    Create Placeholder Login Page

---
+++ react-front-end/src/components/LoginPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+export default function LoginPage() {
+  return <div>THIS IS THE LOGIN PAGE</div>;
+}
