fe23982ba71ec97a3dd8e065b1c7f619e0d9cc93
mariannebourcier <marianne.bourcier@gmail.com>
2022-09-22 15:49:39 -0600

    Add maps icons to homepage

--- react-front-end/src/components/Homepage.jsx
+++ react-front-end/src/components/Homepage.jsx
@@ -13,13 +13,18 @@ export default function Homepage() {
         <p>We help you get connected to runs near you.</p>
       </div>
 
-      <div className="home-join">
-        <p>Find a run near you </p>
+      <div className="home-runs">
+        <div className="home-join">
+          <img className="map-logo" src="https://cdn-icons-png.flaticon.com/512/1865/1865153.png" alt="map-icon" />
+          <p>Find a run near you </p>
+        </div>
+        <div className="home-plan">
+          <img className="map-logo" src="https://cdn-icons-png.flaticon.com/512/2953/2953336.png" alt="run-icon" />
+          <p>Create a run</p>
+        </div>
       </div>
 
-      <div className="home-plan">
-        <p>Create a run</p>
-      </div>
+     
     </div>
   
   )
