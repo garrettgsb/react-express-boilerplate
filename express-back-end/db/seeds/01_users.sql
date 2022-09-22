7a0edb58e268069e084ea4153648868775c748ce
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-09-21 21:48:46 -0600

    create seed file for users

---
+++ express-back-end/db/seeds/01_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (email, password) VALUES ('billieeilish@gmail.com', 'blohsh');
+INSERT INTO users (email, password) VALUES ('bob@gmail.com', 'password1');
+INSERT INTO users (email, password) VALUES ('sara@gmail.com', 'password2');
+INSERT INTO users (email, password) VALUES ('brian@gmail.com', 'password3');
+INSERT INTO users (email, password) VALUES ('gloria@gmail.com', 'password4');
