63e63e7dddfa6f0dc361ac39a76f0398f625a6ed
Gloria <gloriacheung812@gmail.com>
2022-09-24 19:53:18 +0000

    import and use listgroup component to render each listing item

--- react-front-end/src/components/ListingContainer.js
+++ react-front-end/src/components/ListingContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import ListingItem from "./ListingItem";
+import ListGroup from "react-bootstrap/ListGroup";
 import "./ListingContainer.scss";
 
 export default function ListingContainer(props) {
@@ -15,8 +16,8 @@ export default function ListingContainer(props) {
   console.log(data);
 
   return (
-    <Row lg={2} md={1} className="listingContainer p-0 m-0 d-flex">
-      {dataArray}
+    <Row className="listingContainer p-0 m-0 d-flex">
+      <ListGroup variant="flush">{dataArray}</ListGroup>
     </Row>
   );
 }
