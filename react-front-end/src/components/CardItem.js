8ade79e786e7054fb193314523a138d8ee9eccdf
Gloria <gloriacheung812@gmail.com>
2022-09-25 20:27:30 +0000

    add link to each card when click button and unique key

--- react-front-end/src/components/CardItem.js
+++ react-front-end/src/components/CardItem.js
@@ -15,6 +15,7 @@ const CardItem = () => {
       button: "Browse homes",
       image:
         "https://thumbs.dreamstime.com/b/vintage-clipart-house-fenced-yard-hi-quality-image-house-home-fenced-yard-as-vintage-retro-clipart-great-177066401.jpg",
+      link: "/homes_sale",
     },
     {
       title: "Sell a home",
@@ -23,6 +24,7 @@ const CardItem = () => {
       button: "See your options",
       image:
         "https://thumbs.dreamstime.com/b/vintage-clipart-house-home-hi-quality-image-house-home-yard-trees-as-vintage-retro-clipart-great-posters-177065927.jpg",
+      link: "/sell",
     },
     {
       title: "Rent a home",
@@ -31,16 +33,19 @@ const CardItem = () => {
       button: "Find rentals",
       image:
         "https://thumbs.dreamstime.com/b/vintage-clipart-house-hi-quality-image-house-neighbohood-as-vintage-retro-clipart-great-posters-packaging-brochures-177059492.jpg",
+      link: "/homes_rent",
     },
   ];
   const data = items.map((item) => (
-    <Col>
+    <Col key={items.indexOf(item)}>
       <Card>
         <Card.Img className="card_img" variant="top" src={item.image} />
         <Card.Body>
           <Card.Title>{item.title}</Card.Title>
           <Card.Text>{item.description}</Card.Text>
-          <Button variant="primary">{item.button}</Button>
+          <Button as="a" variant="primary" href={item.link}>
+            {item.button}
+          </Button>
         </Card.Body>
       </Card>
     </Col>
