4380714079a3bb2bc01d857f5c4dc26af4568780
Gloria <gloriacheung812@gmail.com>
2022-09-25 00:55:09 +0000

    use bootstrap react modal component to display info from api

---
+++ react-front-end/src/components/ModalContent.js
@@ -0,0 +1,90 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Modal from "react-bootstrap/Modal";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/esm/Image";
+import Fact from "./Fact";
+import "./ModalContent.scss";
+
+export default function ModalContent(props) {
+  const { handleClose, data, show } = props;
+  return (
+    <Modal show={show} onHide={handleClose} size="xl">
+      <Modal.Header closeButton>
+        <Modal.Title>
+          ${data.price}/month
+          <span className="small ps-3">
+            {data.bedrooms} bd | {data.bathrooms} ba | {data.livingArea}{" "}
+            {data.livingAreaUnitsShort}
+          </span>
+          <div className="small">
+            {data.address && data.address.streetAddress}
+            {", "}
+            {data.address && data.address.city}
+            {", "}
+            {data.address && data.address.state}{" "}
+            {data.address && data.address.zipcode}
+          </div>
+          <span className="small d-block">
+            <strong>For Rent</strong>
+          </span>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Container>
+          <Row>
+            <Col lg={7}>
+              {data.photos &&
+                data.photos.map((item) => {
+                  return (
+                    <Image
+                      className="m-1"
+                      key={data.photos.indexOf(item)}
+                      src={item.mixedSources.jpeg[0].url}
+                    ></Image>
+                  );
+                })}
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Overview</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    Listed on {data.datePostedString}
+                  </Card.Subtitle>
+                  <Card.Text>{data.description}</Card.Text>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Rental facts and features</Card.Title>
+                  <div>
+                    {data.resoFacts &&
+                      data.resoFacts.atAGlanceFacts.map((fact) => (
+                        <Fact
+                          idx={data.resoFacts.atAGlanceFacts.indexOf(fact)}
+                          key={data.resoFacts.atAGlanceFacts.indexOf(fact)}
+                          label={fact.factLabel}
+                          value={fact.factValue}
+                        />
+                      ))}
+                  </div>
+                </Card.Body>
+              </Card>
+
+              {data.appliances}
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
