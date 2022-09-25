727bdccc7515fde28d7a2b2dda3bf34b73dec186
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-09-25 01:52:44 -0600

    remove fake login logic from app.js

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -10,43 +10,16 @@ import { Route, Routes } from "react-router-dom";
 import RegistrationPage from "./components/RegistrationPage";
 
 function App(props) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
-
-    if (storedUserLoggedInInformation === "1") {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const loginHandler = (email, password) => {
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
-
-return (
+  return (
     <div className="App">
-      <TopBar isLoggedIn={isLoggedIn} onLogout={logoutHandler} />
+      <TopBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/homes_rent" element={<HomesRent />} />
         <Route path="/homes_sale" element={<HomesSale />} />
         <Route path="/sell" element={<Sell />} />
-        <Route
-          path="/login"
-          element={<LoginPage onLogin={loginHandler} isLoggedIn={isLoggedIn} />}
-        />
-        <Route
-          path="/register"
-          element={<RegistrationPage onLogin={loginHandler} />}
-          isLoggedIn={isLoggedIn}
-        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
       </Routes>
     </div>
   );
