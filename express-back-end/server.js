30c635b160519a1db440da14080e388be80455df
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-09-25 02:15:16 -0600

    Update server.js
    - remove bcrypt
    - use cookie-session
    - implement login logic

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -3,16 +3,34 @@ const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
 const pool = require("./db");
-const bcrypt = require("bcryptjs");
 
 //middleware
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["secretKey"],
+  })
+);
 //routes
 app.post("/login", async (req, res) => {
   const { email, pw } = req.body;
+  const query = {
+    name: "fetch-user",
+    text: "SELECT * FROM users WHERE email = $1 and pw = $2",
+    values: [email, pw],
+  };
+  pool.query(query, (error, response) => {
+    if (!response.rows[0]) {
+      console.log("Invalid credentials");
+    } else {
+      console.log("Succesful.");
+      req.session.userId = response.rows[0].userid;
+    }
+  });
 });
 
 app.post("/register", async (req, res) => {
