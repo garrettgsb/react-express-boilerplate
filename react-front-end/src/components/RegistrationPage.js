89643e6a8996da9f92f69b13bd2e1036a1b94d32
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-09-25 05:17:05 -0600

    re-create registration page with firebase

--- react-front-end/src/components/RegistrationPage.js
+++ react-front-end/src/components/RegistrationPage.js
@@ -1,142 +1,74 @@
-import React, { useState, useReducer, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useRef, useState, useEffect } from "react";
+import { Form, Button, Card, Alert, Container } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Card from "./UI/Card/Card";
-import classes from "./LoginPage.module.css";
-import Button from "./UI/Button/Button";
-import Axios from "axios";
+export default function RegistrationPage() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup, currentUser } = useAuth();
+  const navigate = useNavigate();
 
-const emailReducer = (state, action) => {
-  if (action.type === "USER_INPUT") {
-    return { value: action.val, isValid: action.val.includes("@") };
-  }
-  if (action.type === "INPUT_BLUR") {
-    return { value: state.value, isValid: state.value.includes("@") };
-  }
-  return { value: "", isValid: false };
-};
-
-const passwordReducer = (state, action) => {
-  if (action.type === "USER_INPUT") {
-    return { value: action.val, isValid: action.val.trim().length > 0 };
-  }
-  if (action.type === "INPUT_BLUR") {
-    return { value: state.value, isValid: state.value.trim().length > 0 };
-  }
-  return { value: "", isValid: false };
-};
-
-function RegistrationPage(props) {
-  let navigate = useNavigate();
-
-  const [formIsValid, setFormIsValid] = useState(false);
-
-  const [emailState, dispatchEmail] = useReducer(emailReducer, {
-    value: "",
-    isValid: null,
-  });
-
-  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
-    value: "",
-    isValid: null,
-  });
-
-  const { isValid: emailIsValid } = emailState;
-  const { isValid: passwordIsValid } = passwordState;
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const identifier = setTimeout(() => {
-      setFormIsValid(emailIsValid && passwordIsValid);
-    }, 500);
+    if (currentUser) {
+      navigate("/");
+    }
+  }, []);
 
-    return () => {
-      clearTimeout(identifier);
-    };
-  }, [emailIsValid, passwordIsValid]);
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-  const emailChangeHandler = (event) => {
-    dispatchEmail({
-      type: "USER_INPUT",
-      val: event.target.value,
-    });
-  };
+    // If password and confirmation password don't match, set an error
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
 
-  const passwordChangeHandler = (event) => {
-    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+      navigate("/login");
+    } catch (e) {
+      console.log(e);
+      setError("Failed to create an account");
+    }
 
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 0);
-  };
-
-  const validateEmailHandler = () => {
-    dispatchEmail({ type: "INPUT_BLUR" });
-  };
-
-  const validatePasswordHandler = () => {
-    dispatchEmail({ type: "INPUT_BLUR" });
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    Axios.post("http://localhost:8080/register", {
-      email: emailState.value,
-      pw: passwordState.value,
-    }).then((response) => {
-      console.log(response);
-    });
-    navigate("/login");
-  };
+    setLoading(false);
+  }
 
   return (
-    <React.Fragment>
-      {!props.isLoggedIn && (
-        <Card className={classes.login}>
-          <form onSubmit={submitHandler}>
-            <div
-              className={`${classes.control} ${
-                emailState.isValid === false ? classes.invalid : ""
-              }`}
-            >
-              <label htmlFor="email">E-Mail</label>
-              <input
-                type="text"
-                id="email"
-                value={emailState.value}
-                onChange={emailChangeHandler}
-                onBlur={validateEmailHandler}
-                name="email"
-              />
-            </div>
-            <div
-              className={`${classes.control} ${
-                passwordState.isValid === false ? classes.invalid : ""
-              }`}
-            >
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                value={passwordState.value}
-                onChange={passwordChangeHandler}
-                onBlur={validatePasswordHandler}
-                name="password"
-              />
-            </div>
-            <div className={classes.actions}>
-              <Button
-                type="submit"
-                className={classes.btn}
-                disabled={!formIsValid}
-                onSubmit={submitHandler}
-              >
-                Sign Up
-              </Button>
-            </div>
-          </form>
-        </Card>
-      )}
-      {props.isLoggedIn && <h1>Welcome Back</h1>}
-    </React.Fragment>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? <Link to="/login">Log In</Link>
+      </div>
+    </>
   );
 }
-
-export default RegistrationPage;
