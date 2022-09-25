3088e66f19bf8f063e29174bfc5d9a75de753241
Gloria <gloriacheung812@gmail.com>
2022-09-25 20:28:52 +0000

    use bootstrap container and change layout to display map before data is shown

--- react-front-end/src/components/HomesRent.js
+++ react-front-end/src/components/HomesRent.js
@@ -2,10 +2,9 @@ import React from "react";
 import { useState } from "react";
 import SearchBar from "./SearchBar";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/esm/Container";
 import ListingContainer from "./ListingContainer";
-import "./Homes_Rent.scss";
 import axios from "axios";
 
 export default function Homes_Rent() {
@@ -54,7 +53,7 @@ export default function Homes_Rent() {
   };
 
   return (
-    <div className="rent">
+    <Container>
       <SearchBar
         submitHandler={submitHandler}
         setBaths={setBaths}
@@ -63,14 +62,15 @@ export default function Homes_Rent() {
         setHomeType={setHomeType}
       />
       <Row className="p-0 m-0 border-top">
-        <Col className="mapContainer">
+        {!Object.values(data).length && (
           <Image
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL8lLj1o8mI7lLHKpC4I5qQUJrf9qLZNhDRA&usqp=CAU"
             className="w-100"
           />
-        </Col>
+        )}
+
         <ListingContainer data={data} />
       </Row>
-    </div>
+    </Container>
   );
 }
