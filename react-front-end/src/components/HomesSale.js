8ba3c3511777814cecebcffc4994667f22b32e1d
Gloria <gloriacheung812@gmail.com>
2022-09-25 15:40:57 +0000

    set default beds to 1 so api doesnt just show new constructions initially

--- react-front-end/src/components/HomesSale.js
+++ react-front-end/src/components/HomesSale.js
@@ -1,5 +1,76 @@
 import React from "react";
+import { useState } from "react";
+import SearchBar from "./SearchBar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import ListingContainer from "./ListingContainer";
+import axios from "axios";
 
 export default function Homes_Sale() {
-  return <div>THIS IS THE HOMES_SALE ROUTE</div>;
+  const [location, setLocation] = useState("");
+  const [beds, setBeds] = useState("1");
+  const [baths, setBaths] = useState("0");
+  const [data, setData] = useState({});
+  const [homeType, setHomeType] = useState({
+    isSingleFamily: true,
+    isMultiFamily: true,
+    isApartment: true,
+    isCondo: true,
+    isTownhouse: true,
+  });
+
+  const fetchData = () => {
+    const options = {
+      method: "GET",
+      url: "https://zillow56.p.rapidapi.com/search",
+      params: {
+        location: location,
+        beds_min: beds,
+        baths_min: baths,
+        ...homeType,
+        status: "forSale",
+      },
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+        "X-RapidAPI-Host": "zillow56.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then((res) => {
+        console.log(res.data.results);
+        setData(res.data.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  return (
+    <div className="sale">
+      <SearchBar
+        submitHandler={submitHandler}
+        setBaths={setBaths}
+        setBeds={setBeds}
+        setLocation={setLocation}
+        setHomeType={setHomeType}
+      />
+      <Row className="p-0 m-0 border-top">
+        <Col className="mapContainer">
+          <Image
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL8lLj1o8mI7lLHKpC4I5qQUJrf9qLZNhDRA&usqp=CAU"
+            className="w-100"
+          />
+        </Col>
+        <ListingContainer data={data} />
+      </Row>
+    </div>
+  );
 }
