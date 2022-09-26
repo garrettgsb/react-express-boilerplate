2f34c106c5ee7555c57260105f66e59f887b1d75
Sneha Mahajan <sneh.km@gmail.com>
2022-09-26 14:38:28 -0400

    Add more seed data to runs table.

--- express-back-end/db/seeds.sql
+++ express-back-end/db/seeds.sql
@@ -11,7 +11,17 @@ VALUES ('Aberfoyle Park','Great running space full of trees.','212, Aberfoyle St
 ('Seaside Park','Beautiful view of the sea.','213, Seaside St., Seaside, BC, K8L 8Y5',5,'10:00:00','2022-12-23',1),
 ('Mountain Park','See mountains while you run.','412, Mountain St., Mountain, AB, O5Y 8F7',5,'09:00:00','2022-08-12',3),
 ('Hidden Park','Quaint area far from the city hustle and bustle.','432, Hidden St., Hidden, QC, W4R 5T8',5,'08:00:00','2022-11-10',3),
-('New Park','New surroundings','432, New St., New, QC, W4R 5T8',5,'08:00:00','2022-8-10',3);
+('New Park','New surroundings','432, New St., New, QC, W4R 5T8',5,'08:00:00','2022-8-10',3),
+('Gargoyle Park','Great running space full of trees.','212, Gargoyle St., Gargoyle, ON, L8T 8T5',5,'10:00:00','2023-11-23',1),
+('Bench Park','Beautiful view of the sea.','213, Bench St., Bench, BC, K8L 8Y5',5,'10:00:00','2023-12-23',1),
+('Orange Park','See mountains while you run.','412, Orange St., Orange, AB, O5Y 8F7',5,'09:00:00','2023-08-12',3),
+('Greens Park','Quaint area far from the city hustle and bustle.','432, Greens St., Greens, QC, W4R 5T8',5,'08:00:00','2023-11-10',3),
+('Blue Park','New surroundings','432, Blue St., Blue, QC, W4R 5T8',5,'08:00:00','2023-8-10',3),
+('Smile Park','Great running space full of trees.','212, Smile St., Smile, ON, L8T 8T5',5,'10:00:00','2023-05-23',1),
+('Wild Park','Beautiful view of the sea.','213, Wild St., Wild, BC, K8L 8Y5',5,'10:00:00','2023-12-23',1),
+('Dog Park','See mountains while you run.','412, Dog St., Dog, AB, O5Y 8F7',5,'09:00:00','2023-08-12',3),
+('High Park','Quaint area far from the city hustle and bustle.','432, High St., High, QC, W4R 5T8',5,'08:00:00','2023-11-10',3),
+('Drowsy Park','New surroundings','432, Drowsy St., Drowsy, QC, W4R 5T8',5,'08:00:00','2023-12-10',3);
 
 
 -- users_runs
