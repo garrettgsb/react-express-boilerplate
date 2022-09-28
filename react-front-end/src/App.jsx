72608938de52737757934bbe5aefb3ab72e2c870
Sneha Mahajan <sneh.km@gmail.com>
2022-09-28 15:10:35 -0400

    Move map & register run component to router file.

--- react-front-end/src/App.jsx
+++ react-front-end/src/App.jsx
@@ -1,25 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-// import { router } from "./AppRouter";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { getRuns, getUsersRuns, login, logout } from "./hooks/useAppData";
 import Navigation from "./components/Navigation.js";
 import { Outlet } from "react-router-dom";
-import { atom, useSetRecoilState, useRecoilValue } from "recoil";
-
-import RegisterRun from "./components/RegisterRun";
-
-import Map from "./components/Map";
-
-
-
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-import SignIn from "./components/SignIn";
-
 require("dotenv").config();
-
 
 export default function App() {
   return (
--- react-front-end/src/AppRouter.jsx
+++ react-front-end/src/AppRouter.jsx
@@ -5,6 +5,8 @@ import FindRun from "./components/FindRun";
 import RegisterUser from "./components/RegisterUser";
 import Homepage from "./components/Homepage.jsx";
 import SignIn from "./components/SignIn";
+import RegisterRun from "./components/RegisterRun";
+import Map from "./components/Map";
 import App from "./App";
 
 import {
@@ -17,14 +19,12 @@ export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Homepage />} />
-      <Route
-        path="/profile"
-        element={<Profile />}
-      
-      />
-      <Route path="/runs" element={<FindRun />}  />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/runs" element={<FindRun />} />
       <Route path="/register" element={<RegisterUser />} />
+      <Route path="/create" element={<RegisterRun />}/>
       <Route path="/signin" element={<SignIn />} />
+      <Route path="/map" element={<Map />}/>
     </Route>
   )
 );
