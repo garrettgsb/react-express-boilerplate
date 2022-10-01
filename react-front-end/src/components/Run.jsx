1ddd0e71f58f66628275893e18656756ceaf6715
Sneha Mahajan <sneh.km@gmail.com>
2022-10-01 12:53:45 -0400

    Simplify run component by removing bootstrap card.

--- react-front-end/src/components/Run.jsx
+++ react-front-end/src/components/Run.jsx
@@ -30,36 +30,10 @@ export default function Run(props) {
               <ListGroup.Item>Time: {run.time}</ListGroup.Item>
               <ListGroup.Item>Date: {run.date}</ListGroup.Item>
             </ListGroup>
-            {/* {type === "available" && <Button variant="primary" onClick={join}>Join</Button>} */}
             <JoinButton runType={type} joinStatus={joinStatus} join={join} />
           </div>
         </div>
       </section>
-
-      {/* <Card className="run">
-        <Card.Img className="run-image" src={`/api/runs/image/${run.id}`} />
-        <Card.Body>
-          <div className="run-heading">
-            <Card.Title>{run.name}</Card.Title>
-            {run.future_run && (
-              <span className="material-symbols-rounded">schedule</span>
-            )}
-          </div>
-          <Card.Text>{run.description}</Card.Text>
-          <div className="run-desc">
-            <ListGroup variant="flush">
-              <ListGroup.Item>Distance: {run.distance} km</ListGroup.Item>
-              {type !== "attended" && (
-                <ListGroup.Item>Address: {run.location}</ListGroup.Item>
-              )}
-              <ListGroup.Item>Time: {run.time}</ListGroup.Item>
-              <ListGroup.Item>Date: {run.date}</ListGroup.Item>
-            </ListGroup>
-           
-            <JoinButton runType={type} joinStatus={joinStatus} join={join}/>
-          </div>
-        </Card.Body>
-      </Card> */}
     </>
   );
 }
