5e74c954a413d520c475f690fc5961a8ca574551
Sneha Mahajan <sneh.km@gmail.com>
2022-10-04 17:54:07 -0400

    Add AutoComplete component in RegisterRun form.

--- react-front-end/src/components/AutoComplete.jsx
+++ react-front-end/src/components/AutoComplete.jsx
@@ -1,112 +1,39 @@
-import Autocomplete from "react-google-autocomplete";
-import React from "react";
+import { usePlacesWidget } from "react-google-autocomplete";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 
-export default function AutoComplete() {
+export default function AutoComplete(props) {
   const myKey = process.env.REACT_APP_MAP_API_KEY;
+  const { setPlaceName, setAddress, setCoords } = props;
+  // const [placeName, setPlaceName] = useState("");
+  // const [address, setAddress] = useState("");
+  // const [coords, setCoords] = useState({ lat: "", lng: "" });
+  const { ref: locationRef } = usePlacesWidget({
+    apiKey: myKey,
+    onPlaceSelected: (place) => {
+      setAddress(place.formatted_address);
+      setPlaceName(place.name);
+      setCoords({
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng(),
+      });
+    },
+    options: {
+      types: ["park"],
+      componentRestrictions: { country: "ca" },
+      fields: ["name", "formatted_address", "geometry"],
+    },
+    inputAutocompleteValue: "text",
+  });
   return (
     <>
-      <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Enter an address</Form.Label>
-          <Form.Control type="text" placeholder="Address" />
-        </Form.Group>
-      </Form>
-      <Autocomplete
-        apiKey={myKey}
-        onPlaceSelected={(place) => {
-          console.log(place);
-        }}
+      <Form.Control
+        required
+        type="text"
+        ref={locationRef}
+        // placeholder="Which park will this run be at?"
+        autoComplete="text"
       />
     </>
   );
 }
-
-// import usePlacesAutocomplete, {
-//     getGeocode,
-//     getLatLng,
-// } from "use-places-autocomplete";
-// import useOnclickOutside from "react-cool-onclickoutside";
-
-//   const PlacesAutocomplete = () => {
-//     const {
-//       ready,
-//       value,
-//       suggestions: { status, data },
-//       setValue,
-//       clearSuggestions,
-//     } = usePlacesAutocomplete({
-//       requestOptions: {
-//         /* Define search scope here */
-//       },
-//       debounce: 300,
-//     });
-//     const ref = useOnclickOutside(() => {
-//       // When user clicks outside of the component, we can dismiss
-//       // the searched suggestions by calling this method
-//       clearSuggestions();
-//     });
-
-//     const handleInput = (e) => {
-//       // Update the keyword of the input element
-//       setValue(e.target.value);
-//     };
-
-//     const handleSelect =
-//       ({ description }) =>
-//       () => {
-//         // When user selects a place, we can replace the keyword without request data from API
-//         // by setting the second parameter to "false"
-//         setValue(description, false);
-//         clearSuggestions();
-
-//         // Get latitude and longitude via utility functions
-//         getGeocode({ address: description }).then((results) => {
-//           const { lat, lng } = getLatLng(results[0]);
-//           console.log("📍 Coordinates: ", { lat, lng });
-//         });
-//       };
-
-//     const renderSuggestions = () =>
-//       data.map((suggestion) => {
-//         const {
-//           place_id,
-//           structured_formatting: { main_text, secondary_text },
-//         } = suggestion;
-
-//         return (
-//           <li key={place_id} onClick={handleSelect(suggestion)}>
-//             <strong>{main_text}</strong> <small>{secondary_text}</small>
-//           </li>
-//         );
-//       });
-
-//     return (
-//       <div ref={ref}>
-//         <input
-//           value={value}
-//           onChange={handleInput}
-//           disabled={!ready}
-//           placeholder="Where are you going?"
-//         />
-//         {/* We can use the "status" to decide whether we should display the dropdown or not */}
-//         {status === "OK" && <ul>{renderSuggestions()}</ul>}
-//       </div>
-//     );
-//   };
-
-// export default PlacesAutocomplete;
-//   // return (
-//   //   <AutoComplete
-//   //     apiKey={myKey}
-//   //     style={{ width:"90%" }}
-//   //     onPlaceSelected={(place) => {
-//   //       console.log(place);
-//   //     }}
-//   //     options={{
-//   //       types: ["(regions)"],
-//   //       componentRestrictions: { country: "ca" },
-//   //     }}
-//   //     defaultValue="Toronto"
-//   //     />
-//   // )
