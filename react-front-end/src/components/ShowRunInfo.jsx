221b64f54426b586a31bfe2c90c7116cdd7c57ab
Sneha Mahajan <sneh.km@gmail.com>
2022-10-07 17:35:57 -0400

    Add route for ShowRunInfo component in app router.

--- react-front-end/src/components/ShowRunInfo.jsx
+++ react-front-end/src/components/ShowRunInfo.jsx
@@ -13,41 +13,94 @@ export default function ShowRunInfo({ runName = "Gargoyle Park" }) {
   // const handleClose = () => setShow(false);
   // const handleShow = () => setShow(true);
 
-  useEffect(() => {
-    if (!pageRefresh) {
-      // window.location.reload(true);
-      setPageRefresh(true);
+  // useEffect(() => {
+  //   if (!pageRefresh) {
+  //     // window.location.reload(true);
+  //     setPageRefresh(true);
+  //   }
+  // }, []);
+
+  useRecoilState(userCoordinatesAtom);
+  const myKey = process.env.REACT_APP_MAP_API_KEY;
+
+  const handleApiLoaded = (mapp, maps) => {
+    let map = mapp;
+
+    function initialize() {
+      map = new maps.Map(document.getElementById("run-path"), {
+        mapId: "4493db09864aa939",
+        center: new maps.LatLng(43.6532, -79.3832),
+        zoom: 13,
+        mapTypeId: maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+      });
+      console.log("Loaded!!!!!!!!");
+
+      let directionsDisplay = new maps.DirectionsRenderer();
+      let directionsService = new maps.DirectionsService();
+      calculateAndDisplayRoute(
+        new maps.LatLng(from.lat, from.lng),
+        new maps.LatLng(to.lat, to.lng),
+        directionsService,
+        directionsDisplay
+      );
+      directionsDisplay.setMap(map);
+      directionsDisplay.setOptions({
+        polylineOptions: {
+          strokeColor: "#80B918CC",
+          strokeWeight: "8",
+        },
+      });
+      directionsDisplay.setPanel(document.getElementById("run-path"));
     }
-  }, []);
+    maps.event.addDomListener(window, "load", initialize);
+    function calculateAndDisplayRoute(
+      start,
+      end,
+      directionsService,
+      directionsDisplay
+    ) {
+      directionsService.route(
+        {
+          origin: start,
+          destination: end,
+          travelMode: maps.TravelMode.WALKING,
+        },
+        function (response, status) {
+          if (status === maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert("Directions request failed due to " + status);
+          }
+        }
+      );
+    }
+  };
+
 
   return (
     <>
-      <RouteMap zoom={zoom} from={from} to={to} />
+      {/* <RouteMap zoom={zoom} from={from} to={to} /> */}
 
-      {/* <Button variant="primary" onClick={handleShow}>
-        Show route
-      </Button>
-
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-        size="lg"
+      <div
+        id="run-path"
+        style={{
+          height: "600px",
+          width: "600px",
+          borderRadius: "2rem",
+          border: "10px solid lightgray",
+        }}
       >
-        <Modal.Header closeButton className="route-modal-header">
-          <Modal.Title className="modal-title">{runName}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="route-modal-message">
-          <p>Here is your running route</p>
-          <RouteMap zoom={zoom} from={from} to={to} />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal> */}
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: myKey,
+          }}
+          defaultCenter={{ lat: 43.6532, lng: -79.3832 }}
+          defaultZoom={zoom}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+        />
+      </div>
     </>
   );
 }
