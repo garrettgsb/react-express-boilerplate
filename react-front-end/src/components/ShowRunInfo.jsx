5c2f0615f6ce8ab8655878e03f635b8b9583f47b
Sneha Mahajan <sneh.km@gmail.com>
2022-10-07 16:11:12 -0400

    Add ShowRunInfo & RouteMap components from test branch.

---
+++ react-front-end/src/components/ShowRunInfo.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import RouteMap from "./RouteMap";
+import "./ShowRunInfo.css";
+
+export default function ShowRunInfo({ runName = "Gargoyle Park" }) {
+  const zoom = 10;
+  const [show, setShow] = useState(false);
+  const from = { lat: 43.4952921, lng: -79.9715306 };
+  const to = { lat: 43.5058059, lng: -79.964735 };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      {/* <RouteMap center={center} zoom={zoom}  from={from} to={to}/> */}
+
+      <Button variant="primary" onClick={handleShow}>
+        Show route
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+      >
+        <Modal.Header closeButton className="route-modal-header">
+          <Modal.Title className="modal-title">{runName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="route-modal-message">
+          <p>Here is your running route</p>
+          <RouteMap zoom={zoom} from={from} to={to} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
