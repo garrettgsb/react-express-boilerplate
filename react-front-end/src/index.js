9076610e66fe4d34a257b737fe43d3a14a26ed29
minha0817 <minhakk24@gmail.com>
2022-11-25 14:49:27 -0800

    add routes for login

--- react-front-end/src/index.js
+++ react-front-end/src/index.js
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Login from './pages/Login';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    }
+])
+
+ReactDOM.render(<RouterProvider router={router} />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ react-front-end/src/pages/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function Login () {
+    return (
+        <h1>This is Login page</h1>
+    )
+}
