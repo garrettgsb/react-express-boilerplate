f7fc62ffe0f0bb19fa1728f55e20af1b24a4985e
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-11-26 21:57:20 -0800

    Fix responsive behaviour Fix menu button not showing in mobile

--- react-front-end/src/pages/components/ResponsiveDrawer.js
+++ react-front-end/src/pages/components/ResponsiveDrawer.js
@@ -1,12 +1,29 @@
-import * as React from 'react';
+import * as React from "react";
 
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 
-import { Box, Drawer, AppBar, CssBaseline, Toolbar, List, IconButton, Typography, Divider, ListItemButton, ListSubheader, ListItemIcon, ListItemText, Collapse } from '@mui/material'
-import { ExpandLess, ExpandMore, StarBorder } from '@mui/icons-material'
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import EqualizerIcon from '@mui/icons-material/Equalizer';
-import Header from './Header';
+import {
+  Avatar,
+  Button,
+  Box,
+  Drawer,
+  AppBar,
+  CssBaseline,
+  Toolbar,
+  List,
+  IconButton,
+  Typography,
+  Divider,
+  ListItemButton,
+  ListSubheader,
+  ListItemIcon,
+  ListItemText,
+  Collapse,
+} from "@mui/material";
+import { ExpandLess, ExpandMore, StarBorder } from "@mui/icons-material";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import EqualizerIcon from "@mui/icons-material/Equalizer";
+import Header from "./Header";
 
 const drawerWidth = 240;
 
@@ -15,32 +32,29 @@ const NavbarData = [
   {
     title: "Progress",
     // icon: <InsightsIcon/>,
-    link: "/dashboard"
+    link: "/dashboard",
   },
   {
     title: "Program",
     // icon: <InsightsIcon/>,
-    link: "/program"
+    link: "/program",
   },
-
 ];
 
 const Programs = [
   {
     key: 1,
-    name: 'Full Body'
+    name: "Full Body",
   },
   {
     key: 2,
-    name: 'Bro Split'
+    name: "Bro Split",
   },
   {
     key: 3,
-    name: 'Upper Lower'
+    name: "Upper Lower",
   },
-]
-
-
+];
 
 function ResponsiveDrawer(props) {
   const { window } = props;
@@ -49,79 +63,92 @@ function ResponsiveDrawer(props) {
   // Expanding list item state and click handler
   const [open, setOpen] = React.useState(true);
   const handleClick = () => {
-      setOpen(!open);
+    setOpen(!open);
   };
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
-  const drawer = (
-    <div>
-
-      <Divider />
-        <List
-            sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-            component="nav"
-            aria-labelledby="nested-list-subheader"
+  const header = (
+    <AppBar
+      position="fixed"
+      sx={{
+        width: { sm: `calc(100% - ${drawerWidth}px)` },
+        ml: { sm: `${drawerWidth}px` },
+      }}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: "none" } }}
         >
-          <ListItemButton>
-              <ListItemIcon>
-                  <EqualizerIcon />
-              </ListItemIcon>
-              <ListItemText primary={NavbarData[0].title} />
-          </ListItemButton>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 100 }}>
+          Workout Tracker
+        </Typography>
+        <Avatar alt="Jason Ling" src="images/pikachu.png" />
+        <Button color="inherit">Logout</Button>
+      </Toolbar>
+    </AppBar>
+  );
 
-          <ListItemButton onClick={handleClick}>
-              <ListItemIcon>
-              <FitnessCenterIcon />
-              </ListItemIcon>
-              <ListItemText primary={NavbarData[1].title} />
-              {open ? <ExpandLess /> : <ExpandMore />}
-          </ListItemButton>
+  const drawerItems = (
+    <div>
+      <Divider />
+      <List
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <EqualizerIcon />
+          </ListItemIcon>
+          <ListItemText primary={NavbarData[0].title} />
+        </ListItemButton>
 
-            <Collapse in={open} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
+        <ListItemButton onClick={handleClick}>
+          <ListItemIcon>
+            <FitnessCenterIcon />
+          </ListItemIcon>
+          <ListItemText primary={NavbarData[1].title} />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
 
-                    {Programs.map((program) => (
-
-                        <ListItemButton key={program.key} sx={{ pl: 4 }}>
-                            <ListItemIcon>
-                            <StarBorder />
-                            </ListItemIcon>
-                            <ListItemText primary={program.name} />
-                        </ListItemButton>
-
-                    ))}
-                </List>
-            </Collapse>
-
-            </List>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            {Programs.map((program) => (
+              <ListItemButton key={program.key} sx={{ pl: 4 }}>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary={program.name} />
+              </ListItemButton>
+            ))}
+          </List>
+        </Collapse>
+      </List>
     </div>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Header/>
-      </AppBar>
+      {header}
 
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-
         <Drawer
           container={container}
           variant="temporary"
@@ -131,33 +158,42 @@ function ResponsiveDrawer(props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
-          {drawer}
+          {drawerItems}
         </Drawer>
 
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
           open
         >
-          {drawer}
+          {drawerItems}
         </Drawer>
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
       >
         {props.children}
       </Box>
     </Box>
   );
 }
-
 
 export default ResponsiveDrawer;
