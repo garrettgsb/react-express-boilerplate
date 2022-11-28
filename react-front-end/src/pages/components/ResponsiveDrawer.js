c78c4a87f8841eb4c9077a1e165b6257bb740990
minha0817 <minhakk24@gmail.com>
2022-11-27 21:06:19 -0800

    change the main container size and overflow

--- react-front-end/src/pages/components/ResponsiveDrawer.js
+++ react-front-end/src/pages/components/ResponsiveDrawer.js
@@ -155,7 +155,7 @@ function ResponsiveDrawer(props) {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: "flex" }}
+    <Box sx={{ display: "flex", height: "100vh"}}
     >
       <CssBaseline />
       {header}
@@ -204,6 +204,9 @@ function ResponsiveDrawer(props) {
           flexGrow: 1,
           p: 3,
           width: { sm: `calc(100% - ${drawerWidth}px)` },
+          overflow: 'auto',
+          // flex: 1,
+          // maxheight: '100%'
         }}
 
       >
