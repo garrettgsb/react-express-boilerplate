0b75192f98a11929b10217eb9fc10011610a5222
stufromnorthvan <stumitchell@irsss.ca>
2022-12-01 15:19:15 -0800

    add conditional rendering to canvas page so user can draw picture if not logged in

--- react-front-end/src/pages/HomePage.jsx
+++ react-front-end/src/pages/HomePage.jsx
@@ -14,8 +14,9 @@ export default function HomePage(props) {
       </article>
       <section className="main_section">
         <label className="title">CurtainDraw</label>
+        {!user && <Link to="/login" className="new_canvas" style={{ color: 'rgb(50,50,50)', textDecoration: 'none' }}>Login</Link>}
         {user && <div>Welcome back, {user.name}!</div>}
-        <Link to="/canvas" className="new_canvas" style={{ color: 'rgb(50,50,50)', textDecoration: 'none' }}>Create New</Link>
+        {user && <Link to="/canvas" className="new_canvas" style={{ color: 'rgb(50,50,50)', textDecoration: 'none' }}>Create New</Link>}
 
         <div className="examples">
           <div className="ex1"></div>
