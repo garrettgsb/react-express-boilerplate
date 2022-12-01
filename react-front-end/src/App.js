2456774edfcc51e82c55cc1bdced0b9661519d48
minha0817 <minhakk24@gmail.com>
2022-11-30 23:42:44 -0800

    refactored axios calls into functions

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,40 +1,45 @@
 import React, { Component, useEffect, useState } from "react";
 import axios from "axios";
-import { Outlet, useOutletContext} from "react-router-dom";
+import { Outlet, useOutletContext } from "react-router-dom";
 import "./App.css";
 import ResponsiveDrawer from "./pages/components/ResponsiveDrawer";
-
 
 export default function App() {
   const [programs, setPrograms] = useState([]);
   const [workouts, setWorkouts] = useState([]);
-  // const [exerciseSelections, setExerciseSelections] = useState([]);
-  // const [exercises, setExercises] = useState([]);
 
-
-  // When App initially loads, fetch data and store in state
-
-  useEffect(() => {
+  const getAndSetPrograms = () => {
+    console.log("getAndSetPrograms was called");
     axios
       .get("http://localhost:8080/api/programs")
       .then((result) => {
-        console.log("programs:", result.data)
+        console.log("programs:", result.data);
         setPrograms(result.data);
       })
       .catch((e) => {
         console.log(e);
       });
+  };
 
+  const getAndSetWorkouts = () => {
     axios
       .get("http://localhost:8080/api/workouts")
       .then((result) => {
-        console.log("workouts:", result.data)
+        console.log("workouts:", result.data);
         setWorkouts(result.data);
       })
       .catch((e) => {
         console.log(e);
       });
+  };
+  // const [exerciseSelections, setExerciseSelections] = useState([]);
+  // const [exercises, setExercises] = useState([]);
 
+  // When App initially loads, fetch data and store in state
+
+  useEffect(() => {
+    getAndSetPrograms();
+    getAndSetWorkouts();
     // axios
     //   .get("http://localhost:8080/api/exerciseselections")
     //   .then((result) => {
@@ -45,7 +50,6 @@ export default function App() {
     //     console.log(e);
     //   });
 
-
     // axios
     //   .get("http://localhost:8080/api/program/exercise/:id")
     //   .then((result) => {
@@ -54,16 +58,22 @@ export default function App() {
     //   });
   }, []);
 
-
   return (
     <>
       <ResponsiveDrawer programs={programs} setPrograms={setPrograms}>
-        <Outlet context={{programs, setPrograms}}/>
+        <Outlet
+          context={{
+            programs,
+            setPrograms,
+            getAndSetPrograms,
+            getAndSetWorkouts,
+          }}
+        />
       </ResponsiveDrawer>
     </>
   );
 }
 
 export function usePrograms() {
-  return useOutletContext()
+  return useOutletContext();
 }
