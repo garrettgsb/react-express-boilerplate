7c17e54f34568935adcb0adec5ca02de491af73f
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-12-01 12:42:49 -0800

    Connect workout and exercises data fetching Display ExerciseCards dynamically

--- react-front-end/src/pages/components/ExerciseCard/index.js
+++ react-front-end/src/pages/components/ExerciseCard/index.js
@@ -24,14 +24,16 @@ const exerciseAttributes = {
 };
 
 export default function ExerciseCard(props) {
+  // console.log("this is the prop in ECard", props);
+
   const [edit, setEdit] = useState(false);
   const toggleEdit = () => {
     setEdit(!edit);
   };
 
   return edit ? (
-    <Edit edit={edit} toggleEdit={toggleEdit} />
+    <Edit edit={edit} toggleEdit={toggleEdit} {...props} />
   ) : (
-    <Show edit={edit} toggleEdit={toggleEdit} />
+    <Show edit={edit} toggleEdit={toggleEdit} {...props} />
   );
 }
