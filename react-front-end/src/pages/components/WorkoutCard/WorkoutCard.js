d7f90eadb7243e3cb1aedb2f546df54bfb06d994
minha0817 <minhakk24@gmail.com>
2022-12-02 12:48:23 -0800

    Style workout card

--- react-front-end/src/pages/components/WorkoutCard/WorkoutCard.js
+++ react-front-end/src/pages/components/WorkoutCard/WorkoutCard.js
@@ -1,41 +1,36 @@
 import React from "react";
 
-import {
-  Card,
-  CardMedia,
-  Typography,
-} from "@mui/material";
+import { Card, CardMedia, Typography, CardContent, CardActionArea  } from "@mui/material";
 import { useParams, useNavigate, Link } from "react-router-dom";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 
-export default function WorkoutCard (props) {
-
-  //   // Program navigation click handler
-  //   // const onClickProgram = (id) => {
-  //   //   navigate(`/program/${id}`);
-  //   // };
-
-  // const onClickWorkout = (id) => {
-  //   navigate(``)
-  // }
+export default function WorkoutCard(props) {
 
 
   return (
     <Link to={`/workout/${props.workout.id}`} className={"workoutListItem"}>
-      <Card variant="outlined">
-            <Typography variant="h4">{props.workout.name}</Typography>
-            <Typography variant="p">{props.workout.description}</Typography>
-            <Typography variant="p">{props.workout.duration}</Typography>
-            {/* {props.edit ? <DeleteIcon variant="h4" onClick={props.delete}/> : ""} */}
-            <CardMedia
-              component="img"
-              height="140"
-              image={props.workout.image}
-              alt={props.workout.name}
-            />
-
+      
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={props.workout.image}
+            alt={props.workout.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {props.workout.name}
+            </Typography>
+            <Typography gutterBottom variant="body2" component="div">
+              Duration : {props.workout.duration}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {props.workout.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
       </Card>
     </Link>
-  )
+  );
 }
-
--- react-front-end/src/pages/components/WorkoutCard/WorkoutForm.js
+++ react-front-end/src/pages/components/WorkoutCard/WorkoutForm.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 export default function WorkoutForm() {
   return (
-    <h1>Workout Form</h1>
+    <>
+      <h1>Workout Form</h1>
+    
+    </>
+
   )
 };
