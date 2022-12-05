98678127a6d6b04167cefb8038be38e9ac2f514c
minha0817 <minhakk24@gmail.com>
2022-12-05 11:17:49 -0800

    Add styling

--- react-front-end/src/pages/components/WorkoutCard/WorkoutCard.js
+++ react-front-end/src/pages/components/WorkoutCard/WorkoutCard.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import {
+  Box,
   Card,
   CardMedia,
   Typography,
@@ -8,6 +9,7 @@ import {
   CardActionArea,
   CardActions,
   Button,
+  Divider,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import EditIcon from "@mui/icons-material/Edit";
@@ -41,43 +43,52 @@ export default function WorkoutCard(props) {
         />
       ) : (
         <>
-          <div className="workoutListItem" onClick={handleNavigate}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
+          <Card className="workoutListItem" onClick={handleNavigate}>
+            <Box display="flex">
                 <CardMedia
                   component="img"
                   height="140"
+                  sx={{ width: "40%", height: "auto" }}
                   image={props.workout.image}
                   alt={props.workout.name}
                 />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {props.workout.name}
-                  </Typography>
-                  <Typography gutterBottom variant="body2" component="div">
-                    Duration : {props.workout.duration}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {props.workout.description}
-                  </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                  <Button
-                    variant="outlined"
-                    startIcon={<EditIcon />}
-                    size="small"
-                    sx={{ ml: "auto" }}
-                    onClick={(event) => {
-                      event.stopPropagation();
-                      setEditWorkoutMode(true);
-                    }}
-                  >
-                    Edit
-                  </Button>
-                </CardActions>
-              </CardActionArea>
-            </Card>
-          </div>
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="space-between"
+                  width="100%"
+                >
+                  <CardContent>
+                    <Typography gutterBottom variant="h5">
+                      {props.workout.name}
+                    </Typography>
+                    <Typography gutterBottom variant="body2">
+                      Duration : {props.workout.duration}
+                    </Typography>
+                    <Typography variant="h5" pt={"0.5em"}>
+                      Description
+                    </Typography>
+                    <Typography variant="p">
+                      {props.workout.description}
+                    </Typography>
+                  </CardContent>
+                  <CardActions disableSpacing>
+                    <Button
+                      variant="outlined"
+                      startIcon={<EditIcon />}
+                      size="small"
+                      sx={{ ml: "auto" }}
+                      onClick={(event) => {
+                        event.stopPropagation();
+                        setEditWorkoutMode(true);
+                      }}
+                    >
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Box>
+            </Box>
+          </Card>
         </>
       )}
     </>
