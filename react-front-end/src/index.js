eb504e6013ceb1a7483cc677032b55315b31c318
Ozzy Tasci <oguzcantasci@gmail.com>
2023-06-07 04:44:22 -0400

    Implement auth0 for login & test

--- react-front-end/src/index.js
+++ react-front-end/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Auth0Provider
+  domain="dev-2jggyfjpths0o6ur.us.auth0.com"
+  clientId="kERHuWD6o8BOtyhWE2xSQCEPa9KPhnGq"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+>
+  <App />
+</Auth0Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
