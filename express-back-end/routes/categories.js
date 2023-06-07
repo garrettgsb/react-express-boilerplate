fb11103784fe0b598c9a81f602b6174fcd02551c
Ozzy Tasci <oguzcantasci@gmail.com>
2023-06-07 06:05:13 -0400

    Implement endpoints for categories

--- express-back-end/routes/categories.js
+++ express-back-end/routes/categories.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { Categories } = require('../models')
 
 // GET /api/categories
 router.get('/', (req, res) => {
-  res.json({ message: 'You\'ve sent a GET request to /api/categories' });
+  Categories.findAll()
+  .then(categories => res.send(categories))
+  .catch((err) => console.log('err:', err))
+  // res.json({ message: 'You\'ve sent a GET request to /api/categories' });
+});
+
+//GET /api/categories/:id
+router.get('/:id', (req, res) => {
+  const categoryId = req.params.id;
+  Categories.findById(categoryId)
+  .then(categories => res.send(categories))
+  .catch((err) => console.log('err:', err))
+  // res.json({ message: `You\'ve sent a GET request to /api/categories/${categoryId}` });
 });
 
 module.exports = router;
