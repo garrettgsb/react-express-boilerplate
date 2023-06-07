52d85bd31c2f25fa8404fad7a3554d3bb907c1ff
Ozzy Tasci <oguzcantasci@gmail.com>
2023-06-07 19:11:03 -0400

    Implement sign up

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -3,16 +3,16 @@ import axios from "axios";
 import "./App.css";
 import { withAuth0 } from "@auth0/auth0-react";
 import Login from "./components/login";
+import Signup from "./components/signup";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       message: "Click the button to load data!",
+      user: {},
     };
   }
-
-  
 
   fetchTest = () => {
     axios
@@ -22,9 +22,6 @@ class App extends Component {
         console.log(response.data); // The entire response from the Rails API
 
         console.log(response.data.message); // Just the message
-        this.setState({
-          message: response.data.message,
-        });
       });
   };
 
@@ -45,7 +42,10 @@ class App extends Component {
     if (isAuthenticated !== prevProps.auth0.isAuthenticated) {
       if (isAuthenticated) {
         console.log("User is authenticated");
-        this.fetchData();
+        this.fetchData().then((response) => {
+          this.setState({ user: response[0] });
+          console.log(this.state.user_id);
+        });
       } else {
         console.log("User is not authenticated");
       }
@@ -55,16 +55,13 @@ class App extends Component {
   fetchData = async () => {
     const { user } = this.props.auth0;
     try {
-      const response = await axios.get(`/api/test?email=${user.email}`);
+      const response = await axios.get(`/api/users/login/${user.email}`);
       const userData = response.data;
-      console.log(userData);
+      return userData;
     } catch (error) {
       console.error(error);
     }
   };
-
-  
-  
 
   render() {
     const { isAuthenticated, logout } = this.props.auth0;
@@ -74,7 +71,6 @@ class App extends Component {
         <button onClick={this.fetchTest}>Fetch Data</button>
         {isAuthenticated ? (
           <div>
-            <h1>You are logged in!</h1>
             <button
               onClick={() => logout({ returnTo: window.location.origin })}
             >
@@ -82,7 +78,10 @@ class App extends Component {
             </button>
           </div>
         ) : (
-          <Login />
+          <div>
+            <Login />
+            <Signup />
+          </div>
         )}
       </div>
     );
