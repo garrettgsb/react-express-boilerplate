433d507f10af47af8f2c350bb9659ac79396bb89
Ozzy Tasci <oguzcantasci@gmail.com>
2023-06-08 13:58:47 -0400

    Create route to get all entries for a category from story_categories

--- express-back-end/routes/story_categories.js
+++ express-back-end/routes/story_categories.js
@@ -1,18 +1,29 @@
-const Express = require('express');
+const Express = require("express");
 const router = Express.Router();
-const { StoryCategories} = require('../models')
+const { StoryCategories } = require("../models");
 
 // GET /api/story-categories
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   StoryCategories.findAll()
-    .then(user => {
+    .then((user) => {
       const data = {
         user,
-        message: 'Get all user'
-      }
-      res.send(data)
+        message: "Get all user",
+      };
+      res.send(data);
     })
-    .catch((err) => console.log('err:', err))
+    .catch((err) => console.log("err:", err));
+});
+
+
+// GET all entries for a category from story_categories ---- /api/story-categories/:id
+router.get("/:id", (req, res) => {
+  const category_id = req.params.id;
+  StoryCategories.find({ category_id })
+    .then((entries) => {
+      res.send(entries);
+    })
+    .catch((err) => console.log("err:", err));
 });
 
 module.exports = router;
