a27ff18a50b18c5d66a6c040fc34a99d6df599fc
Sethu <sethunair2102@gmail.com>
2023-09-19 15:44:10 -0600

    Added database schema and seeded test user

---
+++ express-back-end/seeds/01_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (username, email, password)
+VALUES ('test', 'test@tmail.com', 'password');
+
+INSERT INTO watchlists (movie_id, date_added)
+VALUES ('123', '2019-01-04');
+
+INSERT INTO user_watchlist (user_id, watchlist_id)
+VALUES ('1', '1');
