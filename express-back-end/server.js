e5282b7c9ffd3e0a59896c30dc5f15a2708feffd
myfootsasleep <richardoda@gmail.com>
2023-09-21 00:38:53 -0700

    npm install EJS and created a basic registration page

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -3,6 +3,12 @@ const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
 
+//Middleware
+App.use(BodyParser.urlencoded({ extended: false }));
+App.use(BodyParser.json());
+App.use(Express.static('public'));
+App.set('view engine', 'ejs'); // Set EJS as the view engine
+
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
@@ -13,6 +19,14 @@ App.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
 }));
 
+//Getting the registration page
+App.get('/register', (req, res) =>{
+  res.render('register');
+});
+//Making a registration page
+App.post('/register', (req, res) => {
+  const{ username, email, password} = req.body
+});
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
