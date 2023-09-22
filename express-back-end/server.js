37db55387165bad85bc019730f2bdc1a30b04ede
myfootsasleep <richardoda@gmail.com>
2023-09-21 18:39:34 -0700

    Commiting my work so far so I can pull main

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -3,31 +3,39 @@ const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
 
-//Middleware
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static('public'));
-App.set('view engine', 'ejs'); // Set EJS as the view engine
+// Middleware
+App.set("view engine", "ejs");
+App.use(Express.urlencoded({extended: true}));
 
-// Express Configuration
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static('public'));
 
-// Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
-
-//Getting the registration page
-App.get('/register', (req, res) =>{
-  res.render('register');
+// Getting the registration page (Specific route should be defined first)
+App.get('/register', (req, res) => {
+  try {
+    res.render('registration');
+  } catch (error) {
+    console.error('Error rendering template:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
-//Making a registration page
+
+App.get('/api/data', (req, res) => {
+  res.json({
+    message: "Seems to work!",
+  });
+});
+
+// Handling registration POST request
 App.post('/register', (req, res) => {
-  const{ username, email, password} = req.body
+  const { username, email, password } = req.body;
+  
+  // Implement your registration logic here
+  // For example, you can store the user data in a database
+
+  // Send a response to the user
+  res.send('Registration Successful');
 });
+
 App.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+  console.log(`Express is listening on port ${PORT}`);
+  
 });
