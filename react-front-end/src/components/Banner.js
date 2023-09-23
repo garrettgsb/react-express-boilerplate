37a0e42ce0307b7445d06a45ba24a5368dc87477
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-09-23 08:18:26 -0400

    dynamically fetching banner info

--- react-front-end/src/components/Banner.js
+++ react-front-end/src/components/Banner.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
+import axios from "axios";
+import requests from "../TMDB_API_Requests";
 
 function Banner() {
+  const [bannerMovie, setBannerMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.top_rated_url);
+      setBannerMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="banner">
+    <div
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${bannerMovie.backdrop_path}")`,
+      }}
+    >
       <div className="banner_contents">
-        <h1 className="banner_title">The Dark Knight</h1>
+        <h1 className="banner_title">
+          {bannerMovie.title || bannerMovie.name || bannerMovie.orignal_name}
+        </h1>
         <div className="banner_buttons">
           <button className="banner_button">More Info</button>
-          <button className="banner_button">Add To Watchlist</button>
+          <button className="banner_button">Watch Trailer</button>
         </div>
-        <h1 className="banner_description">
-          Batman raises the stakes in his war on crime. With the help of Lt. Jim
-          Gordon and District Attorney Harvey Dent, Batman sets out to dismantle
-          the remaining criminal organizations that plague the streets. The
-          partnership proves to be effective, but they soon find themselves prey
-          to a reign of chaos unleashed by a rising criminal mastermind known to
-          the terrified citizens of Gotham as the Joker.
-        </h1>
+        <h1 className="banner_description">{bannerMovie.overview}</h1>
       </div>
       <div className="banner_fadeBottom"></div>
     </div>
