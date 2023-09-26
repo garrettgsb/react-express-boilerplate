ac073a8004ded38934553d6433fd402a248a1ffd
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-09-26 06:14:41 -0400

    added movie details structure

--- react-front-end/src/components/MovieDetails.js
+++ react-front-end/src/components/MovieDetails.js
@@ -19,7 +19,78 @@ function MovieDetails() {
     console.log(request.data);
     return request;
   }
-  return <div className="movie"></div>;
+  return (
+    <div className="movie">
+      <div className="movie__intro">
+        <img
+          className="movie__backdrop"
+          src={`https://image.tmdb.org/t/p/original${
+            currentMovieDetails ? currentMovieDetails.backdrop_path : ""
+          }`}
+          alt={currentMovieDetails ? currentMovieDetails.original_title : ""}
+        />
+      </div>
+      <div className="movie__detail">
+        <div className="movie__detailLeft">
+          <div className="movie__posterBox">
+            <img
+              className="movie__poster"
+              src={`https://image.tmdb.org/t/p/original${
+                currentMovieDetails ? currentMovieDetails.poster_path : ""
+              }`}
+              alt={
+                currentMovieDetails ? currentMovieDetails.original_title : ""
+              }
+            />
+          </div>
+        </div>
+        <div className="movie__detailRight">
+          <div className="movie__detailRightTop">
+            <div className="movie__name">
+              {currentMovieDetails ? currentMovieDetails.original_title : ""}
+            </div>
+            <div className="movie__tagline">
+              {currentMovieDetails ? currentMovieDetails.tagline : ""}
+            </div>
+            <div className="movie__rating">
+              <span className="movie__voteCount">
+                {currentMovieDetails
+                  ? +currentMovieDetails.vote_count + " votes"
+                  : ""}
+              </span>
+            </div>
+            <div className="movie__runtime">
+              {currentMovieDetails ? currentMovieDetails.runtime + " mins" : ""}
+            </div>
+            <div className="movie__releaseDate">
+              {currentMovieDetails
+                ? "Release date: " + currentMovieDetails.release_date
+                : ""}
+            </div>
+            <div className="movie__genres">
+              {currentMovieDetails && currentMovieDetails.genres
+                ? currentMovieDetails.genres.map((genre) => (
+                    <>
+                      <span className="movie__genre" id={genre.id}>
+                        {genre.name}
+                      </span>
+                    </>
+                  ))
+                : ""}
+            </div>
+          </div>
+          <div className="movie__detailRightBottom">
+            <div className="synopsisText">Plot</div>
+            <div>{currentMovieDetails ? currentMovieDetails.overview : ""}</div>
+          </div>
+          <div className="movie__buttons">
+            <button className="movie__button">Add to Favourites</button>
+            <button className="movie__button">Watch Trailer</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MovieDetails;
