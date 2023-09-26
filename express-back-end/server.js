db389e01aa238169865b8af6892129e62e730f45
myfootsasleep <richardoda@gmail.com>
2023-09-25 17:36:15 -0700

    Deleted the parent folder node_module, readded BodyParser in server.js

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,13 +1,13 @@
-const Express = require('express');
+const Express = require("express");
 const App = Express();
-const BodyParser = require('body-parser');
+const BodyParser = require("body-parser");
 const PORT = 8080;
 
+App.use(BodyParser.urlencoded({ extended: false }));
+App.use(BodyParser.json());
+App.use(Express.static("public"));
 
-
-
-
-App.get('/api/data', (req, res) => {
+App.get("/api/data", (req, res) => {
   res.json({
     message: "Seems to work!",
   });
@@ -17,5 +17,4 @@ App.get('/api/data', (req, res) => {
 
 App.listen(PORT, () => {
   console.log(`Express is listening on port ${PORT}`);
-  
 });
