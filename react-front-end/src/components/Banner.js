2ba992f5aa4d77c4068ace136abf4a5f6320c0a4
Sethu Nair <sethunair2102@gmail.com>
2023-10-03 15:43:51 -0600

    Watch trailer feature complete

--- react-front-end/src/components/Banner.js
+++ react-front-end/src/components/Banner.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
+import {useNavigate} from 'react-router-dom'
 import "./Banner.css";
 import axios from "axios";
 import requests from "../TMDB_API_Requests";
+import movieTrailer from "movie-trailer";
+import MovieDetails from "./MovieDetails";
+import tmdb_api_requests from "../TMDB_API_Requests";
 
 function Banner() {
   const [bannerMovie, setBannerMovie] = useState([]);
+  const navigate = useNavigate();
+  
+  const handleBannerWatchTrailerClick = function(movie) {
+    navigate(`/movie/${bannerMovie.id}`, {
+      state: { genre_url: `${tmdb_api_requests.trending_url}`, fireFunction: true }
+    })
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -32,7 +43,7 @@ function Banner() {
         </h1>
         <div className="banner_buttons">
           <button className="banner_button">More Info</button>
-          <button className="banner_button">Watch Trailer</button>
+          <button onClick={() => handleBannerWatchTrailerClick(bannerMovie.name)} className="banner_button">Watch Trailer</button>
         </div>
         <h1 className="banner_description">{bannerMovie.overview}</h1>
       </div>
