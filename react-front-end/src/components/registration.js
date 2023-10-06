537580bf5a6789a092eb3693662d90e26d9d3e77
myfootsasleep <richardoda@gmail.com>
2023-10-05 17:27:54 -0700

    Commiting so i can merge with master

--- react-front-end/src/components/registration.js
+++ react-front-end/src/components/registration.js
@@ -1,11 +1,12 @@
-// RegistrationForm.js
 import React, { useState } from 'react';
-
+import './Registration.css';
+import axios from 'axios'
 const RegistrationForm = () => {
   const [formData, setFormData] = useState({
     username: '',
     email: '',
     password: '',
+    passwordConfirm: ''
   });
 
   const handleChange = (e) => {
@@ -18,47 +19,78 @@ const RegistrationForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // You can add your registration logic here
-    console.log(formData);
-  };
+    alert("Form Working?")
+
+    // Check if the passwords match
+    if (formData.password !== formData.passwordConfirm) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    // Now you can proceed with registration logic
+    console.log('Registration data:', formData);
+    axios.post('/register', {
+      username: formData.username,
+      email: formData.email,
+      password: formData.password,
+      passwordConfirm: formData.passwordConfirm,
+    })
+    .then((response) => {
+      // Handle the registration success (e.g., redirect to login page)
+      console.log('User registered successfully');
+      // You can add redirection logic here
+    })
+    .catch((error) => {
+      // Handle registration error (e.g., display an error message)
+      console.error('Registration error:', error);
+    });  };
 
   return (
-    <div>
-      <h2>Registration</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username</label>
+    <div className='registration'>
+      <h3>Create Account</h3>
+      <form onSubmit={handleSubmit} action='/register' method='post'>
+        <div className="form-div">
+          <label htmlFor="email">Email: </label>
           <input
+            className="form-control"
+            type="email"
+            name="email"
+            placeholder="example@email.com"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <br />
+          <label htmlFor="username">Username: </label>
+          <input
+            className="form-control"
             type="text"
-            id="username"
             name="username"
             value={formData.username}
             onChange={handleChange}
           />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
+          <br />
+          <label htmlFor="password">Password: </label>
           <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
+            className="form-control"
             type="password"
-            id="password"
             name="password"
             value={formData.password}
             onChange={handleChange}
           />
+          <br />
+          <label htmlFor="passwordConfirm">Confirm Password: </label>
+          <input
+            className="form-control"
+            type="password"
+            name="passwordConfirm"
+            value={formData.passwordConfirm}
+            onChange={handleChange}
+          />
         </div>
-        <div>
-          <button type="submit">Register</button>
-        </div>
+        <br />
+        <button type="submit" className="login-button">
+          Signup
+        </button>
       </form>
     </div>
   );
