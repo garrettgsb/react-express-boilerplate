6112bf198a3f12a904995db58542a9f5419a0bb5
Hossein <hossein.saafi@gmail.com>
2023-10-17 16:32:36 -0700

    move function to home.js component

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,37 +1,14 @@
 import "./style/App.css";
 import React from "react";
-import data from "./data.json";
-import Quiz from "./asset/THELOGO.png";
+import Home from "./components/home.js"
+// import data from "./data.json";
+// import Quiz from "./asset/THELOGO.png";
 
 
 function App() {
-
-  function handleStartClick() { }
-  function handleInstructionsClick() { }
   return (
-    <div className="div-style">
-      <img src={Quiz} alt="quizjs"/> 
-      <button className="rectangle-button" onClick={handleStartClick}>
-        START
-      </button>
-      <button className="rectangle-button" onClick={handleInstructionsClick}>
-        INSTRUCTIONS
-      </button>
-      <div className="score-box">
-        <h2>High Score</h2>
-        <div className="score-columns">
-          {data.highScores.map((score, index) => (
-            <div key={index} className="score-row">
-              <span className="player-name">{score.name}</span>
-              <span className="player-score">{score.score}</span>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    <Home />
+  )
 }
-
-
 
 export default App;
