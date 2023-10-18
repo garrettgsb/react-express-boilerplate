81346365e9419208b75b2393dadbe9d0c0b41159
Ryan Stewart <ryanstew17@gmail.com>
2023-10-18 20:40:27 -0300

    started seeds for question table

--- express-back-end/db/02_seeds.sql
+++ express-back-end/db/02_seeds.sql
@@ -1,4 +1,6 @@
 INSERT INTO Question (Question, RoundNumber, Hint, OptionA, OptionB, OptionC, OptionD, Correct_Option) VALUES
 -- example of a INSERT statement
 -- ('What is 2 + 2?', 1, 'Hint for question 2', '3', '4', '5', '6', 'B', '2023-10-12 08:10:00'); createdAt will be auto added 
+
 ('What is the square root of a fish?', 1, 'Fish come in many shapes and sizes, but they are known for something specific.', '2', 'Fish', '42', '√(-1)', 'B')
+('If you have 3 apples and you take away 2, how many apples do you have left?', 1, 'This question plays with the concept of ownership.', '0', '1', '2', '3', 'B')
