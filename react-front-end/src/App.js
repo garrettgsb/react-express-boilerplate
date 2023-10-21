d4452e02fc078991c185db1afc5f82d3dc77df94
ychento <yiyaochen@gmail.com>
2023-10-21 18:59:49 -0400

    add route

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,6 +2,7 @@ import "./style/App.css";
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/home';
+import Game from "./components/game";
 import Instruction from './components/instruction';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/instructions" element={<Instruction />} />
+        <Route path="/game" element={<Game />} />
       </Routes>
     </BrowserRouter>
   );
