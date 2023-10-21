138e1d65b627aafa3bd86f14731a91a2b8a6c6e5
Ryan Stewart <ryanstew17@gmail.com>
2023-10-21 10:08:40 -0300

    front end for last page

--- react-front-end/src/components/congrads.js
+++ react-front-end/src/components/congrads.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "../style/congrads.css";
 import { useNavigate } from 'react-router-dom';
+import Quiz from "../asset/THELOGO.png";
 
 const Congrats = () => {
   const navigate = useNavigate();
@@ -8,11 +10,32 @@ const Congrats = () => {
     navigate('/');
   };
 
-  return (
-    <div>
-      <h1>Congratulations!</h1>
-      <p>You've completed the quiz!</p>
+    // State variables for form fields
+    const [name, setName] = useState('');
   
+    // Function to handle form submission
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      // Perform actions with form data (e.g., send to server)
+      console.log('Submitted:', { name });
+    };
+  return (
+    <div className='container'>
+      <h1 className='title'>Congratulations!</h1>
+      <h2 className='on'>ON</h2>
+      <h2 className='completing'>COMPLETING</h2>
+      <img src={Quiz} alt="quizjs" />
+      <form className='myForm' onSubmit={handleSubmit}>
+          <label className='name'>
+          Enter your nickname here!!
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <button className='submit' type="submit">Submit</button>
+        </form>
       <button onClick={handleBackToHome}>Back to Home</button>
     </div>
   );
