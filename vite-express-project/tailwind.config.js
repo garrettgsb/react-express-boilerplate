44164d591a6212f62cc586d5bfac01ad5745a590
Rina Inada <indrn@icloud.com>
2023-11-15 18:20:04 -0800

    Update tailwind config

--- vite-express-project/tailwind.config.js
+++ vite-express-project/tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['Oswald', 'sans-serif'],
+        subHeading: ['Roboto', 'sans-serif'],
+        bodyFont: ['Open Sans', 'sans-serif'],
+      },
+      colors: {
+        // Background Color
+        background: '#f2f2f0',
+
+        // Primary Text Color
+        textPrimary: '#333333',
+
+        // Secondary Text Color
+        textSecondary: '#666666',
+
+        // Accent (Link) Color
+        accent: '#F27F3D',
+
+        // Hover/Active Link Color
+        accentHover: '#C3672A',
+
+        // Button Color
+        button: '#F27F3D',
+
+        // Hover/Active Button Color
+        buttonHover: '#C3672A',
+      },
+    },
   },
   plugins: [],
-}
-
+};
