75abce7098311210074bab811fc6b09f9d3158e4
Sunny Hundal <amandip.h@gmail.com>
2023-11-17 10:42:18 -0800

    Add ability to view users by id numbers

--- vite-express-project/src/client/App.jsx
+++ vite-express-project/src/client/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import LandingRoute from "./routes/LandingRoute";
 import MyProfile from "./routes/MyProfileRoute";
+import UserProfile from "./routes/UserProfileRoute";
 import SignupModal from "./components/SignupModal";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -24,6 +25,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<LandingRoute openModal={openModal} />}/>
         <Route exact path="/myprofile" element={<MyProfile />} />
+        <Route exact path="/users/:id" element={<UserProfile />} />
       </Routes>
       <Footer />
       {isModalOpen && <SignupModal isOpen={isModalOpen} onClose={closeModal} />}
