a9d692462c0930e3e6afd5f3609b0884f8ee38ae
Sunny Hundal <amandip.h@gmail.com>
2023-11-17 09:53:10 -0800

    Applied routing for Landing and MyProfileRoute

--- vite-express-project/src/client/App.jsx
+++ vite-express-project/src/client/App.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import Landing from "./routes/LandingRoute";
+import LandingRoute from "./routes/LandingRoute";
+import MyProfile from "./routes/MyProfileRoute";
 import SignupModal from "./components/SignupModal";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
@@ -17,15 +18,22 @@ function App() {
     setIsModalOpen(false);
   };
 
-
   return (
-
     <>
-      <NavBar openModal={openModal} />
-      <Landing openModal={openModal} />
-      <Footer />
-      {isModalOpen && <SignupModal isOpen={isModalOpen} onClose={closeModal} />}
-    </>
+  <NavBar openModal={openModal} />
+      <Routes>
+        <Route exact path="/" element={<LandingRoute />} />
+        <Route exact path="/myprofile" element={<MyProfile />} />
+      </Routes>
+  <Footer />
+  </>
+
+    // <>
+    //   <NavBar openModal={openModal} />
+    //   <Landing openModal={openModal} />
+    //   <Footer />
+    //   {isModalOpen && <SignupModal isOpen={isModalOpen} onClose={closeModal} />}
+    // </>
   );
 }
 
