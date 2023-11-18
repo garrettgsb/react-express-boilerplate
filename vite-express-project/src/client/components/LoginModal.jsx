97f5a17bfef3b3d433109477bd1d41829782d942
Rina Inada <indrn@icloud.com>
2023-11-18 00:26:52 -0800

    Move login modal into a separate component

--- vite-express-project/src/client/components/LoginModal.jsx
+++ vite-express-project/src/client/components/LoginModal.jsx
@@ -1,4 +1,31 @@
-export default LoginModal = () => {
+import { useAuth } from "../hooks/AuthContext";
 
-  
+const LoginModal = () => {
+  const { isLoggedIn, login, logout } = useAuth();
+
+  return (
+    /* Login Modal */
+    <dialog id="login_modal" className="modal">
+      <div className="modal-box flex flex-col items-center justify-center">
+        <form method="dialog">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <h3 className="font-bold text-lg font-heading">Welcome Back!</h3>
+        <div className="form-control w-full max-w-xs m-4">
+          <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xs" />
+        </div>
+        <div className="form-control w-full max-w-xs m-4">
+          <input type="text" placeholder="Password" className="input input-bordered w-full max-w-xs" />
+        </div>
+        <button
+          onClick={() => login()}
+          className="font-subHeading bg-button hover:bg-buttonHover text-white font-bold py-2 px-4 rounded-sm"
+        >
+          Log in
+        </button>
+      </div>
+    </dialog>
+  );
 };
+
+export default LoginModal;
--- vite-express-project/src/client/components/NavBar.jsx
+++ vite-express-project/src/client/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../hooks/AuthContext";
+import LoginModal from "./LoginModal";
 
 export default function NavBar({ openModal }) {
 const { isLoggedIn, login, logout } = useAuth();
@@ -11,8 +12,6 @@ const { isLoggedIn, login, logout } = useAuth();
 
   return (
     <nav className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 pb-1.5 h-10">
-      
-      
       <span className="font-heading text-4xl whitespace-nowrap">LOGO</span>
       <div>
         <ul className="flex space-x-6">
@@ -49,28 +48,8 @@ const { isLoggedIn, login, logout } = useAuth();
             >
               Log in
             </button>
-
-            {/* Login Modal */}
-            <dialog id="login_modal" className="modal">
-              <div className="modal-box flex flex-col items-center justify-center">
-                <form method="dialog">
-                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                </form>
-                <h3 className="font-bold text-lg font-heading">Welcome Back!</h3>
-                <div className="form-control w-full max-w-xs m-4">
-                  <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xs" />
-                </div>
-                <div className="form-control w-full max-w-xs m-4">
-                  <input type="text" placeholder="Password" className="input input-bordered w-full max-w-xs" />
-                </div>
-                <button
-                  onClick={() => login()}
-                  className="font-subHeading bg-button hover:bg-buttonHover text-white font-bold py-2 px-4 rounded-sm"
-                >
-                  Log in
-                </button>
-              </div>
-            </dialog>
+            
+            <LoginModal />
           
             <button
               className="font-subHeading bg-button hover:bg-buttonHover text-white font-bold py-2 px-4 rounded"
