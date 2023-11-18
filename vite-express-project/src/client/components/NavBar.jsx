1f30bd1e6cf8f8745a879315f3e7bedd388aabad
Rina Inada <indrn@icloud.com>
2023-11-18 00:15:00 -0800

    Implement login modal

--- vite-express-project/src/client/components/NavBar.jsx
+++ vite-express-project/src/client/components/NavBar.jsx
@@ -44,11 +44,34 @@ const { isLoggedIn, login, logout } = useAuth();
           <>
             {/* When the user is NOT logged in */}
             <button
-              onClick={() => login()}
+              onClick={()=>document.getElementById('login_modal').showModal()}
               className="font-subHeading bg-transparent hover:bg-buttonHover text-button font-semibold hover:text-white py-2 px-4 border border-button hover:border-transparent rounded"
             >
               Log in
             </button>
+
+            {/* Login Modal */}
+            <dialog id="login_modal" className="modal">
+              <div className="modal-box flex flex-col items-center justify-center">
+                <form method="dialog">
+                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                </form>
+                <h3 className="font-bold text-lg font-heading">Welcome Back!</h3>
+                <div className="form-control w-full max-w-xs m-4">
+                  <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xs" />
+                </div>
+                <div className="form-control w-full max-w-xs m-4">
+                  <input type="text" placeholder="Password" className="input input-bordered w-full max-w-xs" />
+                </div>
+                <button
+                  onClick={() => login()}
+                  className="font-subHeading bg-button hover:bg-buttonHover text-white font-bold py-2 px-4 rounded-sm"
+                >
+                  Log in
+                </button>
+              </div>
+            </dialog>
+          
             <button
               className="font-subHeading bg-button hover:bg-buttonHover text-white font-bold py-2 px-4 rounded"
               onClick={openModal}
