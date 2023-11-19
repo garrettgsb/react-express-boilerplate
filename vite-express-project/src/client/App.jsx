71a70267950096ee06c44ee17173abd187ed7ca3
Rina Inada <indrn@icloud.com>
2023-11-18 19:26:03 -0800

    Implement new project frontend

--- vite-express-project/src/client/App.jsx
+++ vite-express-project/src/client/App.jsx
@@ -9,6 +9,7 @@ import SignupModal from "./components/SignupModal";
 import ProjectProfile from "./routes/ProjectProfileRoute";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./hooks/AuthContext";
+import NewProjectForm from "./components/NewProjectForm";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -34,6 +35,7 @@ function App() {
         <Route exact path="/myprofile" element={<MyProfile />} />
         <Route exact path="/users/:id" element={<UserProfile />} />
         <Route exact path="/project/:id" element={<ProjectProfile />} />
+        <Route exact path="/project/new" element={<NewProjectForm />}/>
       </Routes>
       <Footer />
       {isModalOpen && <SignupModal isOpen={isModalOpen} onClose={closeModal} />}
