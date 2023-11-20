30664a72f718f48be83154f8c98bac375daa648b
Rina Inada <indrn@icloud.com>
2023-11-19 22:41:10 -0800

    Change remove img button

--- vite-express-project/src/client/components/NewProjectForm.jsx
+++ vite-express-project/src/client/components/NewProjectForm.jsx
@@ -47,7 +47,7 @@ export default function NewProjectForm() {
           {imagePreview && (
             <button
               type="button"
-              className="absolute top-2 right-2 text-red-500"
+              className="absolute top-1 right-1 text-pink-900"
               onClick={() => {
                 setImagePreview(null);
                 setFormData({
@@ -56,7 +56,10 @@ export default function NewProjectForm() {
                 });
               }}
             >
-              Remove
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clip-rule="evenodd" />
+              </svg>
+
             </button>
           )}
         </div>
