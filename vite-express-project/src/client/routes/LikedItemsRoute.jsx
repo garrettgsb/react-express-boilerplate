6c006ad0caf335bddffc7c0a76716253fa0df20f
Sunny Hundal <amandip.h@gmail.com>
2023-11-21 16:35:37 -0800

    Styled liked items as cards -needs additional styling

--- vite-express-project/src/client/routes/LikedItemsRoute.jsx
+++ vite-express-project/src/client/routes/LikedItemsRoute.jsx
@@ -1,41 +1,89 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../hooks/AuthContext";
+import { useParams } from "react-router-dom";
 
 const ItemList = () => {
+  const { id } = useParams();
   const [items, setItems] = useState([]);
+  const [projects, setProjects] = useState({});
   const { isLoggedIn, user, setUser } = useAuth;
   const userID = localStorage.getItem("user_id");
 
   useEffect(() => {
-    // Fetching data from a JSON output
-    fetch(`/api/likes/${userID}`, console.log(user))
-      .then((response) => response.json())
-      .then((data) => {
+    const fetchProject = async () => {
+      try {
+        const response = await fetch(`/api/projects`);
+        const data = await response.json();
+        setProjects(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    const fetchLike = async () => {
+      try {
+        const response = await fetch(`/api/likes/3`);
+        const data = await response.json();
         if (Array.isArray(data)) {
           setItems(data);
+          console.log(data);
         } else {
-          // transform object into an array
           const transformedData = Object.keys(data).map((key) => data[key]);
           setItems(transformedData);
         }
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+      } catch (error) {
+        console.error("Error fetching likes:", error);
+      }
+    };
+
+ 
+
+    fetchProject();
+    fetchLike();
+
+  }, [id]);
+
+
+
+
 
   return (
     <div>
-      <h1>Items List</h1>
+      <h1 className="text-2xl font-bold mb-4 mt-10">Items List</h1>
 
-      <ul>
-        {console.log(items)}
+      <div className="grid grid-cols-3 gap-3">
         {items.map((item) => (
-          <li key={item.id}>
-            <p>
-              <a href={`/project/${item.project_id}`}>Project 1</a>
-            </p>
-          </li>
+          <div
+            key={item.id}
+            className="w-50 h-60 bg-white rounded-lg shadow-md"
+          >
+
+            <a href={`/project/${item.project_id}`}>
+              <img
+                src={`/public${projects[10].images}`} // Replace with the URL or path to your item's image
+                alt={projects.title} // Use alt text relevant to the image
+                className="w-full h-40 object-cover object-center"
+              />
+            </a>
+
+            <div className="p-4">
+              <a
+                href={`/project/${item.project_id}`}
+                className="block text-lg font-semibold mb-2"
+              >
+                {projects.title}
+              </a>
+              
+              <img
+                src="/public/images/art_6.jpg"
+                className="w-10 h-10 rounded-full object-cover border-2 border-gray-300 mb-11"
+              />
+
+              <p className="text-gray-600 border-solid"></p>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
