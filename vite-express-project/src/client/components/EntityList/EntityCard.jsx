ceb123b36a285d9bcfac983c85dc3217c4441a3e
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-11-23 15:54:52 -0800

    Give entity card to have white border and border radius

--- vite-express-project/src/client/components/EntityList/EntityCard.jsx
+++ vite-express-project/src/client/components/EntityList/EntityCard.jsx
@@ -11,7 +11,9 @@ export const EntityCard = ({ style, data, isArtists }) => {
   const navigate = useNavigate();
 
   return (
-    <div style={{ ...style, ...entityCardStyle }}>
+    <div 
+      style={{ ...style, ...entityCardStyle }}
+    >
       <img 
         style={{ height: `${ITEM_SIZE}px`, width: `${ITEM_SIZE}px` }}
         src={isArtists ? data.profile_picture : data.images[0]}
@@ -20,6 +22,7 @@ export const EntityCard = ({ style, data, isArtists }) => {
         onClick={() => {
           navigate(`/${isArtists ? 'users' : 'projects'}/${data.id}`);
         }}
+        className="border rounded-3xl"
       />
     </div>
   )
