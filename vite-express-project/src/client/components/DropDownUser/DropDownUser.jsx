d85aed3a2052c8140abfe98252429b0825d601d8
Sunny Hundal <amandip.h@gmail.com>
2023-11-26 23:25:37 -0800

    Created component to create dropdown menu for user for liked items and profile

---
+++ vite-express-project/src/client/components/DropDownUser/DropDownUser.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useAuth } from "../../hooks/AuthContext";
+import { useState } from "react";
+
+const DropDownUser = () => {
+  const { user } = useAuth();
+  const [isOpen, setIsopen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsopen(!isOpen);
+  };
+
+  return (
+    <div className="relative inline-block text-left">
+      <button
+        onClick={toggleDropdown}
+        type="button"
+        className="inline-flex justify-center w-12 h-12 bg-gray-500 text-white rounded-full focus:outline-none"
+        style={{
+          backgroundImage: `url('http://localhost:3000/images/user_1.jpg')`,
+          backgroundPosition: "50% 25%",
+          backgroundSize: "300%",
+        }}
+      ></button>
+      {isOpen && (
+        <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-40 bg-white border border-gray-200 rounded-md shadow-lg">
+          <ul>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <a href={`/users/${user.id}`}>My Profile</a>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <a href="/likeditems">My Liked Items</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropDownUser;
