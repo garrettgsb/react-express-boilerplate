dacd565fba95e7cb440eeabd85f1b7e6eb3064e2
Sunny Hundal <amandip.h@gmail.com>
2023-12-02 14:27:49 -0800

    Initial data fetch is done and data passed to children components

--- vite-express-project/src/client/components/EntityList/index.jsx
+++ vite-express-project/src/client/components/EntityList/index.jsx
@@ -1,16 +1,22 @@
 import { useLocation } from 'react-router-dom';
 import { EntityContextProvider } from './EntityListContext';
 import { EntityTable } from './EntityTable';
+import { useFetchLikesData } from '../LikeList/likeHooks';
+import { useAuth } from '../../hooks/AuthContext';
 
 export const EntityList = () => {
   const location = useLocation();
   const { pathname } = location;
   const splitPath = pathname.split("/");
   const url = splitPath[splitPath.length - 1];
+  const { loggedInUser } = useAuth();
+  const {likesData, error} = useFetchLikesData(loggedInUser.id);
+
+  console.log("Likes data on index:", likesData);
 
   return (
     <EntityContextProvider url={url}>
-      <EntityTable url={url} />
+      <EntityTable url={url} likesData={likesData} />
     </EntityContextProvider>
   );
 }
