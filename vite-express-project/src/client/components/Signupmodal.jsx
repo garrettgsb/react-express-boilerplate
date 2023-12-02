1cd5be37d73865bfb541b7e3af3eccd008fcb1da
Rina Inada <indrn@icloud.com>
2023-12-01 22:00:20 -0800

    Adjust styling

--- vite-express-project/src/client/components/Signupmodal.jsx
+++ vite-express-project/src/client/components/Signupmodal.jsx
@@ -44,47 +44,48 @@ const SignupModal = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto flex items-center justify-center">
-      <div className="fixed inset-0 backdrop-filter backdrop-blur-sm backdrop-brightness-50 backdrop-saturate-150 "></div>
-      <div className="modal-box relative bg-white p-8 rounded-md">
+      {/* <div className="fixed inset-0 backdrop-filter backdrop-blur-sm backdrop-brightness-50 backdrop-saturate-150 "></div> */}
+      <div className="modal-box relative bg-base-100 p-8 rounded-md">
         <div className="flex justify-end">
           <button onClick={onClose}>Close</button>
         </div>
-        <div className="text-2xl font-bold mb-4">Sign Up</div>
+        <div className="text-2xl font-bold mb-6 font-heading tracking-wide">Sign Up</div>
         <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700">
+          <div className="mb-6">
+            {/* <label className="block text-sm font-bodyFont text-neutral m-2">
               Email
-            </label>
+            </label> */}
             <input
               type="email"
               id="email"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+              // className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+              className="input input-bordered w-full max-w-xs "
               value={email}
               onChange={handleEmailChange}
+              placeholder="Email"
               required
             />
           </div>
-          <div className="mb-4">
-            <label
+          <div className="mb-6">
+            {/* <label
               htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium text-neutral m-2"
             >
               Password
-            </label>
+            </label> */}
             <input
               type="password"
               id="password"
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+              // className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+              className="input input-bordered w-full max-w-xs shadow-sm "
               value={password}
               onChange={handlePasswordChange}
+              placeholder="Password"
               required
             />
           </div>
           <div className="flex justify-center">
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
+            <button type="submit" className="btn btn-primary text-white">
               Sign Up
             </button>
           </div>
