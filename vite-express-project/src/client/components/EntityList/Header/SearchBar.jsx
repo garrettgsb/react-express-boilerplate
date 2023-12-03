b6d089c1d0349b396d23f19ca5322833c1dd9d14
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-12-02 22:12:26 -0800

    Create SearchBar component

---
+++ vite-express-project/src/client/components/EntityList/Header/SearchBar.jsx
@@ -0,0 +1,11 @@
+export const SearchBar = ({}) => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="w-56 h-8 px-4 rounded-md border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+      />
+    </div>
+  )
+}
