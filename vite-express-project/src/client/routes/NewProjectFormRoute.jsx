f4faa74cb913cc14f77ad0caa9103d58d817b73d
Rina Inada <indrn@icloud.com>
2023-12-02 23:29:59 -0800

    Add form data

--- vite-express-project/src/client/routes/NewProjectFormRoute.jsx
+++ vite-express-project/src/client/routes/NewProjectFormRoute.jsx
@@ -32,34 +32,36 @@ export default function NewProjectFormRoute() {
 
   const { loggedInUser, checkAuthentication, isLoading } = useAuth();
   const { projectId } = useParams();
+  
   const location = useLocation();
-  // const { state: { projectData }} = location;
+  const { state } = location;
+  const projectData = state && state.projectData;
 
-  // const initialFormData = projectData
-  //   ? {
-  //       projectName: projectData.title,
-  //       description: projectData.description,
-  //       budget: projectData.budget / 100, 
-  //       location: projectData.location,
-  //       projectType: projectData.type,
-  //       image: null, 
-  //     }
-  //   : {
-  //       projectName: "",
-  //       description: "",
-  //       budget: 10,
-  //       location: "",
-  //       projectType: "",
-  //       image: null,
-  //     };
+  const initialFormData = projectData
+    ? {
+        projectName: projectData.title,
+        description: projectData.description,
+        budget: projectData.budget / 100, 
+        location: projectData.location,
+        projectType: projectData.type,
+        image: null, 
+      }
+    : {
+        projectName: "",
+        description: "",
+        budget: 10,
+        location: "",
+        projectType: "",
+        image: null,
+      };
   
-  // const [initialFom, setInitialForm] = useState(initialFormData);
+  const [initialForm, setInitialForm] = useState(initialFormData);
   
-  // useEffect(() => {
-  //   setFormData(initialForm);
-  // }, [initialForm]);
+  useEffect(() => {
+    setFormData(initialForm);
+  }, [initialForm]);
   
-  // console.log(projectData);
+  console.log(projectData);
 
   useEffect(() => {
     const fetchData = async() => {
@@ -98,7 +100,7 @@ export default function NewProjectFormRoute() {
       </div>
       <form onSubmit={(e) => (isEditMode ? handleEditSubmit(e, projectId, employer_id) : handleSubmit(e, employer_id))}>
         <div className="flex justify-center items-start">
-          <div className="relative">
+          { !isEditMode && <div className="relative">
             <label className="border-solid border rounded-lg w-56 h-56 m-5 flex items-center justify-center text-white cursor-pointer relative">
               {!imagePreview && (
                 <input
@@ -117,7 +119,6 @@ export default function NewProjectFormRoute() {
                 />
               ) : (
                 <svg xmlns="http://www.w3.org/2000/svg" height="7em" viewBox="0 0 512 512" fill="#F27F3D">
-                  {/*<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->*/}
                   <path d="M448 80c8.8 0 16 7.2 16 16V415.8l-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3V96c0-8.8 7.2-16 16-16H448zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"/>
                 </svg>
               )}
@@ -142,7 +143,7 @@ export default function NewProjectFormRoute() {
                 </span> 
               </button>
             )}
-          </div>
+          </div>}
 
           <div className="flex flex-col items-center justify-center m-5 mt-0">
             <InputField
