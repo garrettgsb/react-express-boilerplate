e3a03df9590e0cc4026db6210c5979203320ea70
Feda Muhammadian <fedamuhammadian@Fedas-MacBook-Air.local>
2024-01-25 16:26:15 -0800

    added the seeds

---
+++ express-back-end/src/db/schema/create.sql
@@ -0,0 +1,16 @@
+create table gas_stations (
+	id INT,
+	title VARCHAR(50),
+	reguler DECIMAL(3,2),
+	premium  DECIMAL(3,2),
+	deisel  DECIMAL(3,2),
+	payment_method VARCHAR(50),
+	street VARCHAR(50),
+	city VARCHAR(50),
+	state VARCHAR(50),
+	country VARCHAR(50),
+	phone VARCHAR(50),
+	rating DECIMAL(3,2),
+	lat VARCHAR(50),
+	long VARCHAR(50)
+);
