da16572553ae9da467d5941d5e3b62dc5ac07ff1
Maryan ibrahim <maryanibrahim301@yahoo.com>
2024-01-26 17:05:05 -0600

    add top navigation bar

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import TopNavigationBar from './components/TopNavigationBar';
-import Home from './components/Home';
-import Login from './components/login';
-import Register from './components/Register';
+import React, { Component } from "react";
+import axios from "axios";
+import TopNavigationBar from "./components/TopNavigationBar";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <TopNavigationBar />
-      <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="/login" element={<LoginWithNavigate />} />
-        <Route path="/Register" element={<Register />} />
-      </Routes>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "Click the button to load data!",
+    };
+  }
 
-// Helper component to pass the navigate function to Login
-const LoginWithNavigate = () => {
-  const navigate = useNavigate();
-  return <Login navigate={navigate} />;
+  fetchData = () => {
+    axios
+      .get("/api/data") // You can simply make your requests to "/api/whatever you want"
+      .then((response) => {
+        // handle success
+        console.log(response.data); // The entire response from the Rails API
+
+        console.log(response.data.message); // Just the message
+        this.setState({
+          message: response.data.message,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <TopNavigationBar />
+        <h1>{this.state.message}</h1>
+        <button onClick={this.fetchData}>Fetch Data</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-front-end/src/components/TopNavigationBar.jsx
+++ react-front-end/src/components/TopNavigationBar.jsx
@@ -1,39 +1,16 @@
 import React from "react";
-import { Link, useNavigate } from 'react-router-dom';
-import '../styles/TopNavigationBar.scss';
+import '../styles/TopNavigationBar.scss'
 
-const TopNavigationBar = () => {
-  const navigate = useNavigate();
-  // Check if the user is logged in by checking the localStorage
-  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-  const userName = localStorage.getItem("userName"); // Retrieve the user's name from localStorage
-
-  // Function to handle user logout
-  const handleLogout = () => {
-    // clear the isLoggedIn flag and userName from localStorage to log the user out
-    localStorage.removeItem("isLoggedIn");
-    localStorage.removeItem("userName");
-    // Redirect the user to the home page
-    navigate('/Home');
-  };
-
+const TopNavigationBar = (props) => {
   return (
     <nav className="top-navigation-bar">
-      <Link to="/home"> {/* Link to the home page */}
-        <img src="/logoo.png" alt="Logo" className="logo" />
-      </Link>
+      
+      <img src="/logo.png" alt="Logo" className="logo" />
       <div className="navigation-items">
-        {isLoggedIn ? (
-          <>
-            {/* Display user greeting and logout button if logged in */}
-            <span>Hello, {userName}</span> {/* Display the user's name */}
-            <button onClick={handleLogout}>Logout</button>
-          </>
-        ) : (
-          // Only show the Login link if the user is not logged in
-          <Link to="/login">Login</Link>
-        )}
+       
+        {/* Add other navigation links here */}
       </div>
+      {/* Add any additional markup for the navigation bar */}
     </nav>
   );
 };
