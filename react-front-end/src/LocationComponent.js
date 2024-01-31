0e61ad135c22acb2ab54f4831be4b11aee5a4502
Sophia <chiaki.katsuno@live.ca>
2024-01-30 20:03:04 -0800

    create LocationComponent.js

---
+++ react-front-end/src/LocationComponent.js
@@ -0,0 +1,25 @@
+// CoordinatesComponent.jsx or LocationComponent.js
+
+import React, { useEffect, useState } from "react";
+
+function CoordinatesComponent() {
+  const [coordinates, setCoordinates] = useState({});
+
+  useEffect(() => {
+    // Make a request to the Express.js server
+    fetch("http://localhost:3000/getCoordinates")
+      .then((response) => response.json())
+      .then((data) => setCoordinates(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Coordinates</h1>
+      <p>Latitude: {coordinates.lat}</p>
+      <p>Longitude: {coordinates.lng}</p>
+    </div>
+  );
+}
+
+export default CoordinatesComponent;
