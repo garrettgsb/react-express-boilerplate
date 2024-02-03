f38f7433d4066d6b19f17ccc96af4b558a2559a3
Maryan ibrahim <maryanibrahim301@yahoo.com>
2024-02-03 11:26:00 -0600

    Ignore and remove .DS_Store files

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,33 +1,21 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+
 const app = express();
-const cors = require('cors');
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // Sample GET route
-app.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
+app.get('/api/data', (req, res) => {
+  res.json({
+    message: "Seems to work!",
+  });
+});
 
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
->>>>>>>>> Temporary merge branch 2
 });
-
-// // Sample GET route
-// App.get("/api/data", (req, res) =>
-//   res.json({
-//     message: "Seems to work!",
-//   })
-// );
-
-// App.listen(PORT, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(
-//     `Express seems to be listening on port ${PORT} so that's pretty good 👍`
-//   );
-// });
