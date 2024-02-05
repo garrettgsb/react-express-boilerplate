9d749bbb91ba818fefe33d7612eeb6b2a1165112
Sophia <chiaki.katsuno@live.ca>
2024-02-04 20:18:06 -0800

    create rough draft of all routes needed in server.js

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -6,12 +6,40 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-// Sample GET route
-app.get("/api/data", (req, res) =>
-  res.json({
-    message: "Seems to work!",
-  })
-);
+// Routes
+const gasStationRoute = require("./routes/gasStation");
+const gasStationPriceRoute = require("./routes/gasStationPrice");
+const favoritesRoute = require("./routes/favorites");
+const loginRoute = require("./routes/login");
+const registerRoute = require("./routes/register");
+const userRoute = require("./routes/user");
+const reviewRoute = require("./routes/review");
+const notificationRoute = require("./routes/notification");
+const locationRoute = require("./routes/location");
+const updateRoute = require("./routes/update");
+const accountRoute = require("./routes/account");
+const settingsRoute = require("./routes/settings");
+
+// Use routes
+app.use("/api/gasStation", gasStationRoute);
+app.use("/api/gasStationPrice", gasStationPriceRoute);
+app.use("/api/favorites", favoritesRoute);
+app.use("/api/login", loginRoute);
+app.use("/api/register", registerRoute);
+app.use("/api/user", userRoute);
+app.use("/api/review", reviewRoute);
+app.use("/api/notification", notificationRoute);
+app.use("/api/location", locationRoute);
+app.use("/api/update", updateRoute);
+app.use("/api/account", accountRoute);
+app.use("/api/settings", settingsRoute);
+
+// // Sample GET route
+// app.get("/api/data", (req, res) =>
+//   res.json({
+//     message: "Seems to work!",
+//   })
+// );
 
 // Start the server
 const PORT = process.env.PORT || 8080;
