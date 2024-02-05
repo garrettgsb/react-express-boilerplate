83f4845e5e832a14e80cc70728d918a73a4f00f5
Sophia <chiaki.katsuno@live.ca>
2024-02-04 21:23:37 -0800

    add initial location route

---
+++ express-back-end/routes/location.js
@@ -0,0 +1,10 @@
+// location.js
+
+const router = require("express").Router();
+const pool = require("../db");
+const authorization = require("../middleware/authorization");
+
+// Get location of user from search bar
+// TODO, still need to implement this
+
+module.exports = router;
