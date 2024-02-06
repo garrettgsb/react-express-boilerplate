166f28849cc4ffd51b9d53aabb68a62148aa29cd
fedamuhammadian <f.muhammadian@gmail.com>
2024-02-06 11:39:18 -0800

    moved leaflet map into react front end

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -8,31 +8,31 @@ app.use(cors());
 
 // Routes
 const gasStationRoute = require("./routes/gasStation");
-const gasStationPriceRoute = require("./routes/gasStationPrice");
-const favoritesRoute = require("./routes/favorites");
-const loginRoute = require("./routes/login");
-const registerRoute = require("./routes/register");
-const userRoute = require("./routes/user");
-const reviewRoute = require("./routes/review");
-const notificationRoute = require("./routes/notification");
-const locationRoute = require("./routes/location");
-const updateRoute = require("./routes/update");
-const accountRoute = require("./routes/account");
-const settingsRoute = require("./routes/settings");
+//const gasStationPriceRoute = require("./routes/gasStationPrice");
+//const favoritesRoute = require("./routes/favorites");
+//const loginRoute = require("./routes/login");
+//const registerRoute = require("./routes/register");
+//const userRoute = require("./routes/user");
+//const reviewRoute = require("./routes/review");
+//const notificationRoute = require("./routes/notification");
+//const locationRoute = require("./routes/location");
+//const updateRoute = require("./routes/update");
+//const accountRoute = require("./routes/account");
+//const settingsRoute = require("./routes/settings");
 
 // Use routes
 app.use("/api/gasStation", gasStationRoute);
-app.use("/api/gasStationPrice", gasStationPriceRoute);
-app.use("/api/favorites", favoritesRoute);
-app.use("/api/login", loginRoute);
-app.use("/api/register", registerRoute);
-app.use("/api/user", userRoute);
-app.use("/api/review", reviewRoute);
-app.use("/api/notification", notificationRoute);
-app.use("/api/location", locationRoute);
-app.use("/api/update", updateRoute);
-app.use("/api/account", accountRoute);
-app.use("/api/settings", settingsRoute);
+//app.use("/api/gasStationPrice", gasStationPriceRoute);
+//app.use("/api/favorites", favoritesRoute);
+//app.use("/api/login", loginRoute);
+//app.use("/api/register", registerRoute);
+//app.use("/api/user", userRoute);
+//app.use("/api/review", reviewRoute);
+//app.use("/api/notification", notificationRoute);
+//app.use("/api/location", locationRoute);
+//app.use("/api/update", updateRoute);
+//app.use("/api/account", accountRoute);
+//app.use("/api/settings", settingsRoute);
 
 // // Sample GET route
 // app.get("/api/data", (req, res) =>
