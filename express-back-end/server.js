941fea9982d46d572212fc3e4b6969862762c167
Maryan ibrahim <maryanibrahim301@yahoo.com>
2024-02-06 23:10:37 -0600

    Add routes folder and server.js

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,50 +1,17 @@
-const express = require("express");
+import express from 'express';
+import cors from 'cors';
+import settingsRouter from './routes/settingsRoutes.js';
+import historyRouter from './routes/historyRoutes.js'; 
+import favoritesRouter from './routes/favoritesRoutes.js'; 
+
 const app = express();
-const cors = require("cors");
-
-// Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-// Routes
-const gasStationRoute = require("./routes/gasStation");
-const gasStationPriceRoute = require("./routes/gasStationPrice");
-const favoritesRoute = require("./routes/favorites");
-const loginRoute = require("./routes/login");
-const registerRoute = require("./routes/register");
-const userRoute = require("./routes/user");
-const reviewRoute = require("./routes/review");
-const notificationRoute = require("./routes/notification");
-const locationRoute = require("./routes/location");
-const updateRoute = require("./routes/update");
-const accountRoute = require("./routes/account");
-const settingsRoute = require("./routes/settings");
+// Use routers
+app.use('/api/settings', settingsRouter);
+app.use('/api/history', historyRouter); // Use the history router
+app.use('/api/favorites', favoritesRouter); // Use the favorites router
 
-// Use routes
-app.use("/api/gasStation", gasStationRoute);
-app.use("/api/gasStationPrice", gasStationPriceRoute);
-app.use("/api/favorites", favoritesRoute);
-app.use("/api/login", loginRoute);
-app.use("/api/register", registerRoute);
-app.use("/api/user", userRoute);
-app.use("/api/review", reviewRoute);
-app.use("/api/notification", notificationRoute);
-app.use("/api/location", locationRoute);
-app.use("/api/update", updateRoute);
-app.use("/api/account", accountRoute);
-app.use("/api/settings", settingsRoute);
-
-// // Sample GET route
-// app.get("/api/data", (req, res) =>
-//   res.json({
-//     message: "Seems to work!",
-//   })
-// );
-
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(
-    `Express seems to be listening on port ${PORT} so that's pretty good 👍`
-  );
-});
+const PORT = 8080; 
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
