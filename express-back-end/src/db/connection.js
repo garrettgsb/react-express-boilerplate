01368fc6405d862154e51c87a2e2028b71261d7a
Sophia <chiaki.katsuno@live.ca>
2024-02-07 20:48:25 -0800

    no comment

--- express-back-end/src/db/connection.js
+++ express-back-end/src/db/connection.js
@@ -1,20 +1,18 @@
 // PG database client/connection setup
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const dbParams = {
-  host: 'localhost',
+  host: "localhost",
   port: 5432,
-  user: 'fedamuhammadian',
-  password: 'admin',
-  database: 'gas'
+  user: "postgres",
+  password: "postgres",
+  database: "gas",
 };
 
 const db = new Pool(dbParams);
 
 db.connect(() => {
-  console.log('Connected to database');
+  console.log("Connected to database");
 });
-
-
 
 module.exports = db;
